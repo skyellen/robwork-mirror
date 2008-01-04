f7fa10670a6936183771a36172e78dafc192e85d
Anders Lau Olsen <alauo@mmmi.sdu.dk>
2008-01-04 09:11:42 +0000

    Just so that things compile.
    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/RobWork/trunk@323 494c09c7-a67d-49d4-9938-151fb1965be8

--- src/rw/task/Link.cpp
+++ src/rw/task/Link.cpp
@@ -32,16 +32,20 @@ rw::interpolator::Pose6dStraightSegment LinearToolConstraint::Interpolator(rw::m
 
 
 //Copy of linear interpolator - will be fixed later
-rw::interpolator::Pose6dStraightSegment CircularToolConstraint::Interpolator(rw::math::Transform3D<> &a, rw::math::Transform3D<> &b)
-{
-	return rw::interpolator::Pose6dStraightSegment(a, rw::math::VelocityScrew6D<>(b.P()-a.P(),inverse(a.R())*b.R()));
 
+/*
+rw::interpolator::Pose6dStraightSegment CircularToolConstraint::Interpolator(
+    rw::math::Transform3D<> &a, rw::math::Transform3D<> &b)
+{
+	return rw::interpolator::Pose6dStraightSegment(
+        a, rw::math::VelocityScrew6D<>(b.P()-a.P(),inverse(a.R())*b.R()));
 }
 
-
-
-rw::interpolator::StraightSegment LinearJointConstraint::Interpolator(rw::math::Q &a, rw::math::Q &b)
+rw::interpolator::StraightSegment LinearJointConstraint::Interpolator(
+    rw::math::Q &a, rw::math::Q &b)
 {
 	return rw::interpolator::StraightSegment(a,b,1);
-
 }
+
+*/
+
