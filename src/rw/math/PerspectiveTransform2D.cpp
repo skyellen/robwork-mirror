9253cc054a71075fe2fdd020dcbc16656322d9ee
Jimmy Alison Jørgensen <jimali@mmmi.sdu.dk>
2008-02-14 09:16:15 +0000

    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/RobWork/trunk@408 494c09c7-a67d-49d4-9938-151fb1965be8

--- src/rw/math/PerspectiveTransform2D.cpp
+++ src/rw/math/PerspectiveTransform2D.cpp
@@ -18,29 +18,34 @@ PerspectiveTransform2D<T>
 	ublas::matrix<T> A(rows,cols);
 	ublas::vector<T> y(rows);
 	
-	for(size_t i=0;i<n; i+=2){
+	for(size_t i=0;i<n; i++){
+		double xn = pts1[i](0);
+		double yn = pts1[i](1);
+		
+		double Xn = pts2[i](0);
+		double Yn = pts2[i](1);		
 		// set first row
 		size_t j = i*2;
-		A(j+0, 0) = pts1[i](0); // x1
-		A(j+0, 1) = pts1[i](1); // y1
+		A(j+0, 0) = xn; // x1
+		A(j+0, 1) = yn; // y1
 		A(j+0, 2) = 1;
 		A(j+0, 3) = 0;
 		A(j+0, 4) = 0;
 		A(j+0, 5) = 0;
-		A(j+0, 0) = -pts2[i](0)*pts1[i](0); // -X1x1
-		A(j+0, 0) = -pts2[i](0)*pts1[i](1); // -X1y1
+		A(j+0, 6) = -Xn*xn;// -X1x1
+		A(j+0, 7) = -Xn*yn; // -X1y1
 		// set second row
 		A(j+1, 0) = 0; 
 		A(j+1, 1) = 0; 
 		A(j+1, 2) = 0;
-		A(j+1, 3) = pts1[i](0); // x1
-		A(j+1, 4) = pts1[i](1); // y1
+		A(j+1, 3) = xn; // x1
+		A(j+1, 4) = yn; // y1
 		A(j+1, 5) = 1;
-		A(j+1, 0) = -pts2[i](1)*pts1[i](0); // -X1x1
-		A(j+1, 0) = -pts2[i](1)*pts1[i](1); // -X1y1
+		A(j+1, 6) = -Yn*xn; // -Y1x1
+		A(j+1, 7) = -Yn*yn; // -Y1y1
 		// and insert in y vector
-		y(j+0) = pts2[i](0);
-		y(j+1) = pts2[i](1);
+		y(j+0) = Xn;
+		y(j+1) = Yn;
 	}
 	// now calculate the pseudo inverse to the constructed matrix	
 	ublas::vector<T> x = prod(LinearAlgebra::PseudoInverse(A), y);
