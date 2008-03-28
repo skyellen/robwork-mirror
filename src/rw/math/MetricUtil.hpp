d4573b64e05e354e0441a6534e41ed45fec8fa57
Jimmy Alison Jørgensen <jimali@mmmi.sdu.dk>
2008-03-28 14:33:14 +0000

    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/RobWork/trunk@461 494c09c7-a67d-49d4-9938-151fb1965be8

--- src/rw/math/MetricUtil.hpp
+++ src/rw/math/MetricUtil.hpp
@@ -24,6 +24,7 @@
 
 #include <rw/math/Q.hpp>
 #include <rw/math/Vector3D.hpp>
+#include <rw/math/Math.hpp>
 
 namespace rw { namespace math {
     /** @addtogroup math */
@@ -41,11 +42,13 @@ namespace rw { namespace math {
            @brief The 1-norm of a configuration.
         */
         static double ManhattanLength(const Q& q);
+        static double Norm1(const Q& q);
 
         /**
            @brief The 1-norm of the difference between two configurations.
         */
         static double ManhattanDistance(const Q& a, const Q& b);
+        static double Norm1Distance(const Q& a, const Q& b);
 
         // 2-norm.
 
@@ -53,11 +56,17 @@ namespace rw { namespace math {
            @brief The 2-norm of a configuration.
         */
         static double EuclideanLength(const Q& q);
+        static double Norm2(const Q& q);
 
         /**
            @brief The 2-norm of a vector.
         */
         static double EuclideanLength(const Vector3D<>& vec);
+        
+        template <class T>
+        static T Norm2(const Vector3D<T>& vec){
+        	return sqrt(Norm2Sqr<T>(vec));
+        };
 
         /**
            @brief The 2-norm of a vector.
@@ -68,11 +77,22 @@ namespace rw { namespace math {
            @brief The square of the 2-norm of a configuration.
         */
         static double EuclideanLengthSquared(const math::Q& q);
-
+        static double Norm2Squared(const math::Q& q);
+        
         /**
            @brief The square of the 2-norm of a vector.
         */
         static double EuclideanLengthSquared(const Vector3D<>& vec);
+        
+        /**
+         * @brief The square of the 2-norm of a vector
+         */
+        template <class T>
+        static T Norm2Sqr(const Vector3D<T>& vec){
+            return Math::Sqr(vec[0]) +
+            	   Math::Sqr(vec[1]) +
+            	   Math::Sqr(vec[2]);
+        };
 
         /**
            @brief The square of the 2-norm of a vector.
@@ -86,11 +106,20 @@ namespace rw { namespace math {
            @brief The 2-norm of the difference between two configurations.
         */
         static double EuclideanDistance(const Q& a, const Q& b);
+        static double Norm2Distance(const Q& a, const Q& b);
 
         /**
            @brief The 2-norm of the difference between two vectors.
         */
         static double EuclideanDistance(const Vector3D<>& a, const Vector3D<>& b);
+        
+        /**
+         * @brief The 2-norm of the difference between two vectors.
+         */
+        template <class T>
+        static T Norm2(const Vector3D<T>& a, const Vector3D<T>& b){
+        	return sqrt(Norm2DistSqr<T>(a, b));
+        };
 
         /**
            @brief The 2-norm of the difference between two vectors.
@@ -105,11 +134,23 @@ namespace rw { namespace math {
            configurations.
         */
         static double EuclideanDistanceSquared(const Q& a, const Q& b);
+        static double Norm2DistanceSquared(const Q& a, const Q& b);
 
         /**
            @brief The square of the 2-norm of the difference between two vectors.
         */
         static double EuclideanDistanceSquared(const Vector3D<>& a, const Vector3D<>& b);
+        
+        /**
+           @brief The square of the 2-norm of the difference between two vectors.
+        */
+        template <class T>
+        static T Norm2DistSqr(const Vector3D<T>& a, const Vector3D<T>& b){
+            return
+                Math::Sqr(a[0] - b[0]) +
+                Math::Sqr(a[1] - b[1]) +
+                Math::Sqr(a[2] - b[2]);
+        };
 
         /**
            @brief The square of the 2-norm of the difference between two vectors.
@@ -125,6 +166,7 @@ namespace rw { namespace math {
            @brief The infinity-norm of a configuration.
         */
         static double MaxLength(const Q& q);
+        static double NormInf(const Q& q);
 
         /**
            @brief The scaled infinity-norm of a configuration.
@@ -143,17 +185,20 @@ q_scaled[i] = scale[i] * q[i]
            for all <code>i</code>.
         */
         static double MaxLengthScaled(const Q& q, const Q& scale);
+        static double NormInfScaled(const Q& q, const Q& scale);
 
         /**
            @brief The infinity-norm of a the difference between two configurations.
         */
         static double MaxDistance(const Q& a, const Q& b);
+        static double NormInfDistance(const Q& a, const Q& b);
 
         /**
            @brief The scaled infinity-norm of a the difference between two
            configurations.
         */
         static double MaxDistanceScaled(const Q& a, const Q& b, const Q& scale);
+        static double NormInfDistanceScaled(const Q& a, const Q& b, const Q& scale);
     };
 
     /*@}*/
