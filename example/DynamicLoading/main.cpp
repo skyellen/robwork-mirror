9f0775752ba63d1d41b7dafd87daec771eeda517
Lars-Peter Ellekilde <lpe@mmmi.sdu.dk>
2008-04-07 09:19:24 +0000

    Updated DynamicLoading Example
    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/RobWork/trunk@469 494c09c7-a67d-49d4-9938-151fb1965be8

--- example/DynamicLoading/main.cpp
+++ example/DynamicLoading/main.cpp
@@ -8,18 +8,19 @@
  * This program demonstrates the use of the DynamicLibraryLoader to
  * load in libraries
  */
-int main() {
-
-    //Load PluginA
+int main(int argc, char** argv) {
+    //Load PluginA  
     rw::common::DynamicLibraryLoader<Plugin> factoryA("libPluginA", "factory0");
     Plugin* pluginA = factoryA.get();
-    std::cout<<"PluginA = "<<pluginA->name()<<std::endl;
+	if (pluginA != NULL)
+		std::cout<<"PluginA = "<<pluginA->name()<<std::endl;
 
-    //Load PluginB  
+	//Load PluginB  
     rw::common::DynamicLibraryLoader<Plugin> factoryB("libPluginB", "factory0");
     Plugin* pluginB = factoryB.get();
-    std::cout<<"PluginB = "<<pluginB->name()<<std::endl;
-
+	if (pluginB != NULL)
+		std::cout<<"PluginB = "<<pluginB->name()<<std::endl;
+		
     return 0;
    
 }
--- example/tul/tul.cpp
+++ example/tul/tul.cpp
@@ -1,47 +1,63 @@
-#include <rw/models/WorkCell.hpp>
-#include <rw/models/Device.hpp>
-#include <rw/models/SerialDevice.hpp>
-
-#include <rw/loaders/WorkCellLoader.hpp>
-
-#include <rw/kinematics/Frame.hpp>
-#include <rw/kinematics/State.hpp>
+// QtoPos.cpp : Defines the entry point for the console application.
+//
 
 #include <iostream>
+#include <fstream>
 
+
+#include <rw/math/Q.hpp>
+#include <rw/models/WorkCell.hpp>
+#include <rw/models/Device.hpp>
+#include <rw/kinematics/FKRange.hpp>
+#include <rw/loaders/WorkCellLoader.hpp>
+#include <rw/loaders/rwxml/XMLRWLoader.hpp>
+
+using namespace rw::math;
 using namespace rw::models;
 using namespace rw::kinematics;
+using namespace rw::loaders;
 
-int main(int argc, char** argv)
+int main(int argc, char* argv[])
 {
-    if(argc != 2){
-        std::cout << "Usage: tul <filename>" << std::endl;
+    if (argc <= 2) {
+        std::cout<<"QtoPos <Input> <Output>"<<std::endl;
         return -1;
     }
 
-    std::auto_ptr<WorkCell> workcell;
-
-    try{
-        workcell = rw::loaders::WorkCellLoader::load(argv[1]);
-    }catch(std::string& ex){
-        std::cerr << ex << std::endl;
-        exit(1);
+    std::auto_ptr<WorkCell> workcell = XMLRWLoader::LoadWorkCell("d:/workspace/RobWorkData/XMLScenes/MobileManipulator/MobileManipulator.xml");
+    Device* device;
+    for (std::vector<Device*>::const_iterator it = workcell->getDevices().begin(); it != workcell->getDevices().end(); ++it) {
+        if ((*it)->getName() == "KukaLBR3")  {
+            device = (*it);
+        } 
     }
+    if (device == NULL) {
+        RW_WARN("Error: No KukaLBR3 Device Found");
+        return -1; 
+    }
+    
 
+    Frame* gripper = workcell->findFrame("PG70.TCP");
     State state = workcell->getDefaultState();
-    std::cout
-        << "Success: "
-        << workcell->getDevices().size()
-        << " devices loaded.\n";
 
-    for (size_t i = 0; i < workcell->getDevices().size(); i++) {
-        SerialDevice* device = dynamic_cast<SerialDevice*>(
-            workcell->getDevices()[i]);
 
-        if (device) {
-            std::cout << device->baseTend(state) << "\n";
-        }
+    FKRange fkrange(device->getBase(), gripper, state);
+    std::ifstream in(argv[1]);
+    std::ofstream out(argv[2]);
+    while (!in.eof()) {
+        Q q(7);
+        double t;
+        in>>t;
+        for (int i = 0; i<7; i++)
+            in>>q(i);
+        device->setQ(q, state);
+        Transform3D<> transform = fkrange.get(state);
+        out<<t<<" "<<transform.P()(0)<<" "<<transform.P()(1)<<" "<<transform.P()(2)<<std::endl;
+
     }
+    in.close();
+    out.close();
 
     return 0;
 }
+
