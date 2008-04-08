0edf893c29c230d0d3488e890bbbe5642c628582
Preben Hagh Strunge Holm <prebenh@mmmi.sdu.dk>
2008-04-08 14:35:54 +0000

    Added dummy file
    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/RobWork/trunk@481 494c09c7-a67d-49d4-9938-151fb1965be8

---
+++ src/sandbox/Dummy.cpp
@@ -0,0 +1,3 @@
+#include "Dummy.hpp"
+
+Dummy::Dummy() {}
---
+++ src/sandbox/Dummy.hpp
@@ -0,0 +1,13 @@
+#ifndef DUMMY_HPP_
+#define DUMMY_HPP_
+
+
+class Dummy
+{
+public:
+	Dummy();
+	virtual ~Dummy(){};
+};
+
+
+#endif /*DUMMY_HPP_*/
