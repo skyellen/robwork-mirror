a194c91bc3b6c1d1f65013afd8bfb1eb8e5f80e2
Anders Lau Olsen <alauo@mmmi.sdu.dk>
2008-04-28 13:55:30 +0000

    Test cases for StringUtil.
    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/RobWork/trunk@533 494c09c7-a67d-49d4-9938-151fb1965be8

---
+++ test/common/StringUtilTest.cpp
@@ -0,0 +1,34 @@
+
+#include <rw/common/StringUtil.hpp>
+
+#include <boost/test/unit_test.hpp>
+#include <boost/foreach.hpp>
+
+using namespace rw::common;
+
+void StringUtilTest()
+{
+    BOOST_CHECK(
+        StringUtil::getFileExtension("") == "");
+
+    BOOST_CHECK(
+        StringUtil::getFileExtension("whatever") == "");
+
+    BOOST_CHECK(
+        StringUtil::getFileExtension("whatever.") == ".");
+
+    BOOST_CHECK(
+        StringUtil::getFileExtension("whatever.x") == ".x");
+
+    BOOST_CHECK(
+        StringUtil::getFileExtension("foo.bar/baz") == "");
+
+    BOOST_CHECK(
+        StringUtil::getFileExtension("foo.bar\\baz") == "");
+
+    BOOST_CHECK(
+        StringUtil::getFileExtension("foo.bar/baz.txt") == ".txt");
+
+    BOOST_CHECK(
+        StringUtil::getFileExtension("foo.bar\\baz.txt") == ".txt");
+}
