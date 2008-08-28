0c46c52f58111327af70fcfe2bc4ab041140138c
Lars-Peter Ellekilde <lpe@mmmi.sdu.dk>
2008-08-28 14:24:30 +0000

    Added a possibility of scaling velocity and acceleration limits on the xqpcontroller
    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/RobWork/trunk@746 494c09c7-a67d-49d4-9938-151fb1965be8

--- src/rwlibs/algorithms/xqpcontroller/XQPController.cpp
+++ src/rwlibs/algorithms/xqpcontroller/XQPController.cpp
@@ -14,9 +14,9 @@ using namespace rwlibs::algorithms::qpcontroller;
 using namespace boost::numeric::ublas;
 
 
-XQPController::XQPController(Device* device, 
+XQPController::XQPController(Device* device,
 							 Frame* controlFrame,
-							 const State& state, 
+							 const State& state,
 							 double dt):
 	_device(device),
 	_controlFrame(controlFrame),
@@ -29,7 +29,7 @@ XQPController::XQPController(Device* device,
     _qlower = _device->getBounds().first;
     _qupper = _device->getBounds().second;
 
-    _dqlimit = _device->getVelocityLimits();	
+    _dqlimit = _device->getVelocityLimits();
     _ddqlimit = _device->getAccelerationLimits();
 }
 
@@ -37,11 +37,19 @@ XQPController::~XQPController()
 {
 }
 
+void XQPController::setAccScale(double scale) {
+    _ddqlimit = scale*_device->getAccelerationLimits();
+}
 
-Q XQPController::inequalitySolve(const matrix<double>& G, 
- 				      			 const vector<double>& b, 
- 				      			 const vector<double>& lower, 
- 				      			 const vector<double>& upper, 
+void XQPController::setVelScale(double scale) {
+    _dqlimit = scale*_device->getVelocityLimits();
+}
+
+
+Q XQPController::inequalitySolve(const matrix<double>& G,
+ 				      			 const vector<double>& b,
+ 				      			 const vector<double>& lower,
+ 				      			 const vector<double>& upper,
  				      			 const std::list<Constraint>& constraints) {
     matrix<double> cmat = zero_matrix<double>(2*lower.size() + constraints.size(), lower.size());
     vector<double> limits(2*lower.size() + constraints.size());
@@ -49,11 +57,11 @@ Q XQPController::inequalitySolve(const matrix<double>& G,
         cmat(2*i,i) = 1;
         cmat(2*i+1,i) = -1;
         limits(2*i) = lower(i);
-        limits(2*i+1) = -upper(i);        
+        limits(2*i+1) = -upper(i);
     }
-    
-    
-    size_t index = 2*lower.size(); 
+
+
+    size_t index = 2*lower.size();
     for (std::list<Constraint>::const_iterator it = constraints.begin(); it != constraints.end(); ++it) {
     	const Constraint& c = *it;
     	for (size_t i = 0; i<c._a.size(); i++) {
@@ -62,7 +70,7 @@ Q XQPController::inequalitySolve(const matrix<double>& G,
     	limits(index) = c._b;
     }
 
-    
+
     //vector<double> qstart = (lower+upper)/2.0;
     vector<double> qstart(zero_vector<double>(lower.size()));
     //Try to start it with the minimal velocity
@@ -85,7 +93,7 @@ Q XQPController::inequalitySolve(const matrix<double>& G,
                 if (c._a(i) < 0)
                     qstart(i) = lower(i);
                 else
-                    qstart(i) = upper(i);                    
+                    qstart(i) = upper(i);
             }
             if (inner_prod(c._a.m(), qstart)< c._b) {
                 std::cout<<"Could not find a valid starting velocity"<<std::endl;
@@ -98,18 +106,18 @@ Q XQPController::inequalitySolve(const matrix<double>& G,
           //  std::cout<<"None of the bounds works to start it"<<std::endl;
         //}
     }
-    
-    
-    
-    
-    
+
+
+
+
+
     QPSolver::Status status;
     vector<double> res = QPSolver::inequalitySolve(G, -1*b, cmat, limits, qstart, status);
-    if (status == QPSolver::SUBOPTIMAL) 
+    if (status == QPSolver::SUBOPTIMAL)
         std::cout<<"Solution appears to be suboptimal"<<std::endl;
-    if (status == QPSolver::ERROR) 
+    if (status == QPSolver::ERROR)
         std::cout<<"Solution appears to be invalid"<<std::endl;
-        
+
     return Q(res);
 }
 
@@ -148,16 +156,16 @@ void XQPController::calculateVelocityLimits(vector<double>& lower,
             } else {
                 double j_X = Math::round(sqrt(1.-8*X/(_dt*_dt*(-_ddqlimit)[i]))/2.-1);
                 double q_end_X = (X+_dt*_dt*(-_ddqlimit)[i]*(j_X*(j_X+1))/2)/(_dt*(j_X+1));
-                posmax = q_end_X-j_X*(-_ddqlimit)[i]*_dt; 
+                posmax = q_end_X-j_X*(-_ddqlimit)[i]*_dt;
             }
         }
         x = joint_pos(i)-_qlower[i];
-        if (x<=0)    { 
+        if (x<=0)    {
             posmin = 0;
-        } else {//For qmin      
+        } else {//For qmin
             double j_x = Math::round(sqrt(1+8*x/(_dt*_dt*_ddqlimit[i]))/2-1);
             double q_end_x = (-x+_dt*_dt*_ddqlimit[i]*(j_x*(j_x+1))/2)/(_dt*(j_x+1));
-            double q_min_x = q_end_x-j_x*_ddqlimit[i]*_dt;      
+            double q_min_x = q_end_x-j_x*_ddqlimit[i]*_dt;
             double X = x+_dt*q_min_x;
             if (X<=0) {
                 posmin = 0;
@@ -169,39 +177,39 @@ void XQPController::calculateVelocityLimits(vector<double>& lower,
         }
         upper(i) = std::min(accmax,std::min(velmax, posmax));
         lower(i) = std::max(accmin,std::max(velmin, posmin));
-        
-        
-        //Because of numerical uncertainties we need to test whether upper>lower. 
+
+
+        //Because of numerical uncertainties we need to test whether upper>lower.
         if (upper(i) < lower(i)) {
             lower(i) = upper(i);
         }
     }
 }
 
-Q XQPController::solve(const rw::math::Q& q,		
+Q XQPController::solve(const rw::math::Q& q,
                        const rw::math::Q& dq,
-					   const rw::math::VelocityScrew6D<>& tcpscrew, 
+					   const rw::math::VelocityScrew6D<>& tcpscrew,
 					   const std::list<Constraint>& constraints) {
-	
-    if (q.size() != _dof) 
+
+    if (q.size() != _dof)
         RW_THROW("Length of input configuration does not match the Device");
 
     /*(6);
     Vector3D<> linvel = tcpscrew.linear();
     EAA<> angvel = tcpscrew.angular();
 
-    tcp_vel(0) = linvel(0);      
+    tcp_vel(0) = linvel(0);
     tcp_vel(1) = linvel(1);
     tcp_vel(2) = linvel(2);
     tcp_vel(3) = angvel.angle()*angvel.axis()(0);
     tcp_vel(4) = angvel.angle()*angvel.axis()(1);
     tcp_vel(5) = angvel.angle()*angvel.axis()(2);
     */
-    
+
     _device->setQ(q, _state);
     matrix<double> P;
-    if (_space == ControlFrame) {      
-        Rotation3D<> rot = inverse(_device->baseTframe(_controlFrame, _state).R());        
+    if (_space == ControlFrame) {
+        Rotation3D<> rot = inverse(_device->baseTframe(_controlFrame, _state).R());
         matrix<double> R = zero_matrix<double>(6,6);
         matrix_range<matrix<double> > rot1(R, range(0,3), range(0,3));
         rot1 = rot.m();
@@ -211,19 +219,19 @@ Q XQPController::solve(const rw::math::Q& q,
     } else {
         P = _P;
     }
-    
+
     vector<double> tcp_vel = prod(P, tcpscrew.m());
 	matrix<double> jac = prod(P, _device->baseJframe(_controlFrame,_state).m());
-    
+
     //trim jacobian to remove world z-rotation
     //    matrix_range<matrix<double> > jac(jac6, range(0,6), range(0, jac6.size2()));
 
     matrix<double> A = prod(trans(jac),jac);
     vector<double> b = prod(trans(jac),tcp_vel);
-   
+
     vector<double> lower(_dof);
     vector<double> upper(_dof);
-	
+
     calculateVelocityLimits(lower, upper, q, dq);
     Q sol1 = inequalitySolve(A, b, lower, upper, constraints);
 
--- src/rwlibs/algorithms/xqpcontroller/XQPController.hpp
+++ src/rwlibs/algorithms/xqpcontroller/XQPController.hpp
@@ -14,41 +14,41 @@ namespace algorithms {
 
 /**
  * @brief An extended version of the QPController
- * 
+ *
  * Notice: Still under development
- * 
+ *
  * The XQPController extends the QPController by providing the user
  * with the possibility of adding constraints on the motion. This method
  * follows the algorithm described in [1].
- * 
+ *
  * [1]: Write name of paper when published
  */
 class XQPController
 {
 public:
     /**
-     * @brief Constraint for the XQPController   
-     * 
-     * A constraint is defined as \f$a^T \dot{q} \geq b \f$ 
+     * @brief Constraint for the XQPController
+     *
+     * A constraint is defined as \f$a^T \dot{q} \geq b \f$
      */
     class Constraint {
-    public:       
+    public:
         rw::math::Q _a;
         double _b;
     public:
         /**
          * @brief Constructor for constraint
-         * 
+         *
          * @param a [in] the \b a part
-         * @param b [in] the \b b part        
+         * @param b [in] the \b b part
          */
         Constraint(const rw::math::Q& a, double b):
             _a(a),
-            _b(b) 
-        {            
+            _b(b)
+        {
         }
     };
-    
+
     /**
      * @brief Constructs XQPController
      * @param device [in] Device to control
@@ -56,50 +56,50 @@ public:
      * @param state [in] State specifying how frames are assempled
      * @param dt [in] time step size
      */
-	XQPController(rw::models::Device* device, 
+	XQPController(rw::models::Device* device,
 				  rw::kinematics::Frame* controlFrame,
-				  const rw::kinematics::State& state, 
+				  const rw::kinematics::State& state,
 				  double dt);
 
 	/**
 	 * @brief Destructor
 	 */
 	virtual ~XQPController();
-	
+
 	/**
 	 * @brief Solves the inverse kinematics problem
-	 * 
+	 *
 	 * Notice that if the constraints cannot be fullfilled, no quarantees for the
 	 * return value is given.
-	 * 
+	 *
 	 * @param q [in] Current configuration of the device
 	 * @param dq [in] Current joint velocities for the device
 	 * @param tcpvel [in] Desired tool velocity seen in base frame
 	 * @param constraints [in] List of constraints
-	 */ 
-	rw::math::Q solve(const rw::math::Q& q,				
+	 */
+	rw::math::Q solve(const rw::math::Q& q,
 	                  const rw::math::Q& dq,
-			          const rw::math::VelocityScrew6D<>& tcpvel, 
+			          const rw::math::VelocityScrew6D<>& tcpvel,
 			          const std::list<Constraint>& constraints);
-	
+
 	/**
 	 * @brief Enumeration used to specify frame associated with the projection
 	 */
-	enum ProjectionFrame { BaseFrame = 0, /** Robot Base Frame */ 
+	enum ProjectionFrame { BaseFrame = 0, /** Robot Base Frame */
 	                   ControlFrame  /**The Frame specified as the controlFrame*/
 	                  };
-	
+
 	/**
 	 * @brief Setup the projection
-	 * 
+	 *
 	 * The traditional relationship between device Jacobian, joint velocities and tool velocity
 	 * is given by \f$J\dot{q}=\dot{q}\f$. To ignore certain degrees of freedom or put more
-	 * emphasis (with respect to the least square solution) on some we can multiply with 
-	 * \f$P\f$ to get \f$P J\dot{q}=P \dot{x}\f$. 
-	 * 
+	 * emphasis (with respect to the least square solution) on some we can multiply with
+	 * \f$P\f$ to get \f$P J\dot{q}=P \dot{x}\f$.
+	 *
 	 * \f$P\f$ needs to have exactly 6 columns, however the number of row may be less than 6.
-	 * Use the \b space flag to specify in which space the projection should occur. 
-	 * 
+	 * Use the \b space flag to specify in which space the projection should occur.
+	 *
 	 * Usage: Setup to ignore tool roll
 	 * \code
 	 * XQPController* xqp = new XQPController(device, device->getEnd(), state, dt)
@@ -108,48 +108,63 @@ public:
 	 *     P(i,i) = 1;
 	 * xqp->setProjection(P, XQPController::ControlFrame);
 	 * \endcode
-	 * 
+	 *
 	 * Usage: Increase the weight of the z-coordinate relative to the base
 	 * \code
 	 * XQPController* xqp = new XQPController(device, device->getEnd(), state, dt);
 	 * boost::numeric::ublas::matrix<double> P = boost::numeric::ublas::identity_matrix<double>(6);
-	 * P(2,2) = 100; //Increase the least square weight with a factor of 100 
+	 * P(2,2) = 100; //Increase the least square weight with a factor of 100
 	 * xqp->setProjection(P, XQPController::BaseFrame);
 	 * \endcode
-	 * 
+	 *
 	 * @param P [in] The projection matrix
 	 * @param space [in] The space in which to apply the projection
 	 */
 	void setProjection(const boost::numeric::ublas::matrix<double>& P, ProjectionFrame space);
-	
+
+	/**
+	 * @brief Sets a scale for the acceleration limits
+	 *
+	 * Use this method to scale the acceleration limit e.g. to reduce the
+	 * power with which it can accelerate.
+	 */
+	void setAccScale(double scale);
+
+	/**
+	 * @brief Sets a scale for the velocity limits
+	 *
+	 * Use this method to scale the velocity limits e.g. to reduce the
+	 * speed of the robot
+	 */
+	void setVelScale(double scale);
 private:
 	rw::models::Device* _device;
 	rw::kinematics::Frame* _controlFrame;
 	rw::kinematics::State _state;
 	double _dt;
 	size_t _dof;
-	
+
 	ProjectionFrame _space;
 	boost::numeric::ublas::matrix<double> _P;
-	
+
 	rw::math::Q _qlower;
 	rw::math::Q _qupper;
 	rw::math::Q _dqlimit;
 	rw::math::Q _ddqlimit;
-	
+
 	/**
 	 * Solves the inequality problem 1/2 x^T.G.x+b^T.x subject to lower <= x <= upper
-	 * and the constraints in the constraint list 
+	 * and the constraints in the constraint list
 	 */
-    rw::math::Q inequalitySolve(const boost::numeric::ublas::matrix<double>& G, 
-     				            const boost::numeric::ublas::vector<double>& b, 
-     				            const boost::numeric::ublas::vector<double>& lower, 
-     				            const boost::numeric::ublas::vector<double>& upper, 
+    rw::math::Q inequalitySolve(const boost::numeric::ublas::matrix<double>& G,
+     				            const boost::numeric::ublas::vector<double>& b,
+     				            const boost::numeric::ublas::vector<double>& lower,
+     				            const boost::numeric::ublas::vector<double>& upper,
      				            const std::list<Constraint>& constraints);
 
 public:
     /**
-     * Calculates the velocity limits 
+     * Calculates the velocity limits
      */
     void calculateVelocityLimits(boost::numeric::ublas::vector<double>& lower,
                                  boost::numeric::ublas::vector<double>& upper,
