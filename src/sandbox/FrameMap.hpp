366992ba2b04c258ac8ba690e2e30eef60c9671a
Jimmy Alison Jørgensen <jimali@mmmi.sdu.dk>
2008-09-22 20:55:56 +0000

    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/RobWork/trunk@781 494c09c7-a67d-49d4-9938-151fb1965be8

--- src/sandbox/FrameMap.hpp
+++ src/sandbox/FrameMap.hpp
@@ -1,5 +1,5 @@
-#ifndef FRAMEMAP_HPP_
-#define FRAMEMAP_HPP_
+#ifndef RW_KINEMATICS_FRAMEMAP_HPP_
+#define RW_KINEMATICS_FRAMEMAP_HPP_
 
 #include <rw/kinematics/Frame.hpp>
 
@@ -12,7 +12,7 @@ namespace kinematics {
      * @brief a specialized mapping implementation for frames. It uses the internal
      * structure of Frames to provide fast O(1) lookup for mappings from Frame to anything.
      *
-     * A requirement is that all frames must be registered in the same StateStructure.
+     * @note A requirement is that all frames must be registered in the same StateStructure.
      */
     template <class T>
     class FrameMap {
@@ -20,24 +20,26 @@ namespace kinematics {
 
         /**
          * @brief creates a framemap
+         * @param defaultVal [in] the default value of new instances of T
          */
         FrameMap(T defaultVal):
             _defaultVal(defaultVal)
-            {}
+        {}
 
         /**
          * @brief creates a framemap with an initial size of s
+         * @param s [in] nr of elements of the types T with default value "defaultVal"
+         * @param defaultVal [in] the default value of new instances of T
          */
         FrameMap(int s, T defaultVal):
             _map(s, defaultVal),
             _defaultVal(defaultVal)
-
-        {
-
-        }
+        {}
 
         /**
-         * @brief inserts a value
+         * @brief inserts a value into the frame map
+         * @param frame [in] the frame for which the value is to be associated
+         * @param value [in] the value that is to be associated to the frame
          */
         void insert(const rw::kinematics::Frame& frame, T& value){
             const int idx = frame.getID();
@@ -47,7 +49,9 @@ namespace kinematics {
         }
 
         /**
-         * @brief
+         * @brief return a reference to the value that is associated with the frame "frame"
+         * @param frame [in] the frame for which to find its associated values.
+         * @return reference to the value associated to frame.
          */
         const T& operator[](const rw::kinematics::Frame& frame) const {
             const int idx = frame.getID();
@@ -57,7 +61,9 @@ namespace kinematics {
         }
 
         /**
-         * @brief get the value associated with frame
+         * @brief return a reference to the value that is associated with the frame "frame"
+         * @param frame [in] the frame for which to find its associated values.
+         * @return reference to the value associated to frame.
          */
         T& operator[](const rw::kinematics::Frame& frame) {
             const int idx = frame.getID();
