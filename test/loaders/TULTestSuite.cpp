82e4f0ef1c3a4f5eb74a39382695b01d8c043223
Jimmy Alison Jørgensen <jimali@mmmi.sdu.dk>
2008-10-23 19:56:14 +0000

    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/RobWork/trunk@836 494c09c7-a67d-49d4-9938-151fb1965be8

--- test/loaders/TULTestSuite.cpp
+++ test/loaders/TULTestSuite.cpp
@@ -1,14 +1,40 @@
 #include "TULTestSuite.hpp"
+
+#include "../TestSuiteConfig.h"
 #include <string>
+
+//#include <sandbox/loaders/>
 
 using namespace boost::unit_test;
 
+//using namespace rw::loaders;
+
 void TULLoaderTest();
 void PathLoaderTest();
+
+
+
+void XMLRWLoaderTest(){
+
+    /*BOOST_MESSAGE("XMLRWLoaderTestSuite");
+    BOOST_MESSAGE("- Loading workcell file");
+    std::auto_ptr<WorkCell> workcell =
+        XMLRWLoader::load(testFilePath+"testscene.xml");
+
+    BOOST_REQUIRE(NULL != workcell.get());
+    BOOST_REQUIRE(workcell->getDevices().size() == 1);
+
+    BOOST_MESSAGE("- Testing nr of devices");
+    SerialDevice* device = (SerialDevice*)workcell->getDevices()[0];
+    State state = workcell->getDefaultState();
+*/
+
+}
 
 TULTestSuite::TULTestSuite() :
     boost::unit_test::test_suite("TULTestSuite")
 {
     add( BOOST_TEST_CASE( &TULLoaderTest) );
     add( BOOST_TEST_CASE( &PathLoaderTest) );
+    add( BOOST_TEST_CASE( &XMLRWLoaderTest) );
 }
