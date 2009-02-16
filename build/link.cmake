68988dcf0daf9dca0673b9f8a9623133f167fcac
Jimmy Alison Jørgensen <jimali@mmmi.sdu.dk>
2009-02-16 09:32:02 +0000

    Enabled forcing Xerces not to be included in built
    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/RobWork/trunk@932 494c09c7-a67d-49d4-9938-151fb1965be8

--- build/link.cmake
+++ build/link.cmake
@@ -1,5 +1,5 @@
 # -*- cmake -*-
-message("LibraryOutput = " ${LIBRARY_OUTPUT_PATH})
+#message("LibraryOutput = " ${LIBRARY_OUTPUT_PATH})
 set(d ${LIBRARY_OUTPUT_PATH})
 link_directories(${d})
 list(APPEND CMAKE_LIBRARY_PATH ${d})
@@ -13,14 +13,12 @@ elseif (DEFINED UNIX)
   set(RW_UBLAS_LIBRARY_NAMES lapack)
 endif ()
 
-
 if (RW_HAVE_XERCES)
 	find_library(XERCES_LIB xerces-c)
 	message("XercesLib = "${XERCES_LIB})
 	if (NOT XERCES_LIB)
 	  message("Warning: Could not find Xerces library. Using default name")
-
-	    set(XERCES_LIB xerces-c)
+	  set(XERCES_LIB xerces-c)
 	endif ()
 endif()
 
@@ -33,8 +31,6 @@ if (RW_HAVE_PQP)
 	  set(PQP_LIB pqp)
 	endif ()
 endif()
-
-message( "Find collision libs")
 
 if (RW_HAVE_YAOBI)
 	find_library(YAOBI_LIB yaobi)
@@ -74,18 +70,21 @@ set(RW_PATHPLANNERS_LIBRARY_LIST
   ${RW_LIBRARY_LIST}
   )
 
+message( "-- Looking for collision libs: ")
+
 set(CollisionDetectionLibraries)
 if (RW_HAVE_PQP)
   list(APPEND CollisionDetectionLibraries ${PQP_LIB})
-  message("Have PQP")
+  message("--- PQP Found ")
 else()
-	message("No PQP")
+  message("--- PQP not found")
 endif ()
+
 if (RW_HAVE_YAOBI)
   list(APPEND CollisionDetectionLibraries ${YAOBI_LIB})
-	message("Have Yaobi")
+	message("--- Yaobi found")
 else()
-	message("No Yaobi")
+	message("--- Yaobi not found")
 endif ()
 
 # Libraries for programs using rw_proximitystrategies.
