22099e945ebcbcfbeb821321669b377d31854485
Jimmy Alison Jørgensen <jimali@mmmi.sdu.dk>
2009-04-15 07:15:00 +0000

    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/RobWork/trunk@1031 494c09c7-a67d-49d4-9938-151fb1965be8

--- src/sandbox/Log.cpp
+++ src/sandbox/Log.cpp
@@ -23,95 +23,129 @@
 #include <rw/common/LogStreamWriter.hpp>
 
 using namespace rw::common::sandbox;
-using namespace rw::common;
+using namespace rw;
 
-//Log::Map Log::_map;
+namespace {
 
-const std::string& Log::warningId()
-{
-	static const std::string str = "Warning";
-	return str;
+	class EmptyLogWriter: public rw::common::LogWriter
+	{
+	public:
+        virtual void flush(){};
+        virtual void write(const std::string& str){};
+        virtual void write(const rw::common::Message& msg){};
+        virtual void writeln(const std::string& str){};
+	};
+	typedef rw::common::Ptr<EmptyLogWriter> EmptyLogWriterPtr;
 }
 
-const std::string& Log::errorId()
-{
-	static const std::string str = "Error";
-	return str;
+Log _log;
+
+Log& Log::log(){
+	return getInstance();
 }
 
-const std::string& Log::infoId()
-{
-	static const std::string info = "Info";
-	return info;
+Log& Log::getInstance(){
+	return _log;
 }
 
-const std::string& Log::debugId()
+Log::Log():
+	_writers(32)
 {
-	static const std::string str = "Debug";
-	return str;
+	_defaultWriter = ownedPtr(new EmptyLogWriter());
+    setWriter(Info, new common::LogStreamWriter(&std::cout));
+    setWriter(Debug, new common::LogStreamWriter(&std::cout));
+    setWriter(Warning, new common::LogStreamWriter(&std::cerr));
+    setWriter(Error, new common::LogStreamWriter(&std::cerr));
 }
-
-namespace
-{
-    int init()
-    {
-        Log::setWriter(Log::infoId(), new LogStreamWriter(&std::cout));
-        Log::setWriter(Log::debugId(), new LogStreamWriter(&std::cout));
-        Log::setWriter(Log::warningId(), new LogStreamWriter(&std::cerr));
-        Log::setWriter(Log::errorId(), new LogStreamWriter(&std::cerr));
-        return 0;
-    }
-
-    const int x = init();
-}
-
-Log::Log() {}
 
 Log::~Log() {}
 
-void Log::setWriter(const std::string& id, LogWriter* writer)
+void Log::setWriter(LogLevel id, rw::common::LogWriterPtr writer)
 {
-    _map[id] = ownedPtr<LogWriter>(writer);
+	_writers[id] = writer;
 }
 
-LogWriter& Log::get(const std::string& id)
+void Log::setWriter(const std::string& id, rw::common::LogWriterPtr writer)
 {
-    Map::iterator it = _map.find(id);
-    if (it != _map.end())
+	_userWriters[id] = writer;
+}
+
+rw::common::LogWriter& Log::get(LogLevel id)
+{
+	if(isValidLogLevel(id))
+		return *_writers[id];
+	return *_defaultWriter;
+	//RW_ASSERT("No such writer");
+    //RW_THROW("LogWriter named: " << id << " does not exist");
+}
+
+rw::common::LogWriter& Log::get(const std::string& id)
+{
+    Map::iterator it = _userWriters.find(id);
+    if (it != _userWriters.end())
         return *it->second;
 
-    RW_THROW("LogWriter named: " << id << " does not exist");
+    return *_defaultWriter;
+    //RW_THROW("LogWriter named: " << id << " does not exist");
 }
 
-void Log::write(const std::string& id, const std::string& message)
-{
+void Log::write(LogLevel id, const std::string& message){
     get(id).write(message);
 }
 
-void Log::write(const std::string& id, const Message& message)
-{
+void Log::write(const std::string& id, const std::string& message){
     get(id).write(message);
 }
 
-void Log::writeln(const std::string& id, const std::string& message)
-{
+void Log::write(LogLevel id, const rw::common::Message& message){
+    get(id).write(message);
+}
+
+void Log::write(const std::string& id, const rw::common::Message& message){
+    get(id).write(message);
+}
+
+void Log::writeln(LogLevel id, const std::string& message){
     get(id).write(message + '\n');
 }
 
-void Log::flush(const std::string& id)
-{
+void Log::writeln(const std::string& id, const std::string& message){
+    get(id).write(message + '\n');
+}
+
+void Log::flush(const std::string& id){
     get(id).flush();
 }
 
-void Log::flushAll()
-{
-    typedef std::pair<std::string, boost::shared_ptr<LogWriter> > Pair;
-    BOOST_FOREACH(const Pair& pair, _map) {
+void Log::flush(LogLevel id){
+    get(id).flush();
+}
+
+void Log::flushAll(){
+    typedef std::pair<std::string, Ptr<LogWriter> > Pair;
+    BOOST_FOREACH(const Pair& pair, _userWriters) {
         pair.second->flush();
+    }
+    for(size_t i=0;i<_writers.size();i++){
+    	if(_writers[i]!=NULL)
+    		_writers[i]->flush();
     }
 }
 
 void Log::remove(const std::string& id)
 {
-    _map.erase(id);
+    _userWriters.erase(id);
+}
+
+void Log::remove(LogLevel id)
+{
+	_writers[id] = NULL;
+}
+
+bool Log::isValidLogLevel(LogLevel id){
+	if(id<0 || _writers.size()<id)
+		return false;
+	if(_writers[id]==NULL)
+		return false;
+	return true;
 }
