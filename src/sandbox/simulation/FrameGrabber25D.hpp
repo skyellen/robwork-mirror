0563cdfb87c4dc501a637296fdf83c2b359f7bf2
Jimmy Alison Jørgensen <jimali@mmmi.sdu.dk>
2009-04-22 13:40:11 +0000

    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/RobWork/trunk@1059 494c09c7-a67d-49d4-9938-151fb1965be8

---
+++ src/sandbox/simulation/FrameGrabber25D.hpp
@@ -0,0 +1,119 @@
+/*********************************************************************
+ * RobWork Version 0.3
+ * Copyright (C) Robotics Group, Maersk Institute, University of Southern
+ * Denmark.
+ *
+ * RobWork can be used, modified and redistributed freely.
+ * RobWork is distributed WITHOUT ANY WARRANTY; including the implied
+ * warranty of merchantability, fitness for a particular purpose and
+ * guarantee of future releases, maintenance and bug fixes. The authors
+ * has no responsibility of continuous development, maintenance, support
+ * and insurance of backwards capability in the future.
+ *
+ * Notice that RobWork uses 3rd party software for which the RobWork
+ * license does not apply. Consult the packages in the ext/ directory
+ * for detailed information about these packages.
+ *********************************************************************/
+
+#ifndef RWLIBS_SIMULATION_CAMERA_FRAMEGRAPPER_HPP
+#define RWLIBS_SIMULATION_CAMERA_FRAMEGRAPPER_HPP
+
+/**
+ * @file FrameGrabber.hpp
+ */
+
+#include <rw/sensor/Image25D.hpp>
+#include <rw/kinematics/Frame.hpp>
+
+namespace rwlibs { namespace simulation {
+    /** @addtogroup simulation */
+    /* @{ */
+
+    /**
+     * @brief The FrameGrabber25D abstract interface, can be used to grab images from a
+     * specialized source.
+     */
+    class FrameGrabber25D
+    {
+    public:
+        /**
+         * @brief constructor
+         * @param width [in] width of the image that this FrameGrabber25D uses.
+         * @param height [in] height of the image that this FrameGrabber25D uses.
+         * @param encoding [in] color encoding of the image that this FrameGrabber25D uses.
+         */
+        FrameGrabber25D(int width, int height) :
+            _colorCode(encoding)
+        {
+            _img = new rw::sensor::Image( width, height );
+        }
+
+        /**
+         * @brief destructor
+         */
+        virtual ~FrameGrabber25D()
+        {
+            delete _img;
+        }
+
+        /**
+         * @brief returns the width of the image
+         * @return the height of the image
+         */
+        int getWidth() { return _img->getWidth(); }
+
+        /**
+         * @brief returns the height of the image
+         * @return the height of the image
+         */
+        int getHeight() { return _img->getHeight(); }
+
+        /**
+         * @brief resizes the image that this frameGrabber use. The colorcode will
+         * default to the one that FrameGrabber25D was initialized with.
+         * @param width [in] width of image
+         * @param height [in] height of image
+         */
+        void resize(int width, int height) {
+            //delete _img;
+            _img = new rw::sensor::Image(width, height, _colorCode);
+        };
+
+        /**
+         * @brief resizes the image that this frameGrabber use.
+         * @param width [in] width of image.
+         * @param height [in] height of image.
+         * @param colorCode [in] Color encoding of the image.
+         */
+        void resize(int width, int height, rw::sensor::Image::ColorCode colorCode)
+        {
+            _colorCode = colorCode;
+            //delete _img; // TODO: shared_ptr
+            _img = new rw::sensor::Image(width, height, colorCode);
+        };
+
+        /**
+         * @brief returns the image
+         * @return the image
+         */
+        virtual rw::sensor::Image& getImage()
+        {
+            return *_img;
+        }
+
+        /**
+         * @brief this function grabs a image from the specialized source and
+         * copies it to the FrameGrabber25D image.
+         */
+        virtual void grab(rw::kinematics::Frame *frame,
+                          const rw::kinematics::State& state) = 0;
+
+    protected:
+        //! @brief The image
+        rw::sensor::Image25D *_img;
+    };
+
+    /* @} */
+}} // end namespaces
+
+#endif // end include guard
---
+++ src/sandbox/simulation/GLFrameGrabber25D.hpp
@@ -0,0 +1,91 @@
+/*********************************************************************
+ * RobWork Version 0.3
+ * Copyright (C) Robotics Group, Maersk Institute, University of Southern
+ * Denmark.
+ *
+ * RobWork can be used, modified and redistributed freely.
+ * RobWork is distributed WITHOUT ANY WARRANTY; including the implied
+ * warranty of merchantability, fitness for a particular purpose and
+ * guarantee of future releases, maintenance and bug fixes. The authors
+ * has no responsibility of continuous development, maintenance, support
+ * and insurance of backwards capability in the future.
+ *
+ * Notice that RobWork uses 3rd party software for which the RobWork
+ * license does not apply. Consult the packages in the ext/ directory
+ * for detailed information about these packages.
+ *********************************************************************/
+
+#ifndef RWLIBS_SIMULATION_CAMERA_GLFRAMEGRAPPER25D_HPP
+#define RWLIBS_SIMULATION_CAMERA_GLFRAMEGRAPPER25D_HPP
+
+/**
+ * @file GLFrameGrabber.hpp
+ */
+
+#include "FrameGrabber.hpp"
+
+#include <rwlibs/drawable/WorkCellGLDrawer.hpp>
+#include <rw/math/Transform3D.hpp>
+#include <rw/math/PerspectiveTransform3D.hpp>
+#include <rw/kinematics/Frame.hpp>
+#include <rw/kinematics/State.hpp>
+
+namespace rwlibs { namespace simulation {
+    /** @addtogroup simulation */
+    /* @{ */
+
+    /**
+     * @brief An implementation of the FrameGrabber interface. The GLFrameGrabber25D
+     * grabs images from a OpenGL scene using a simple pinhole camera model.
+     *
+     * a framethe opengl rendering to
+     * take pictures of the scene.
+
+     * The most basic parameter of a camera is its Field of view. This
+     * can be used as an initial camera model. Field of view can be
+     * calculated from the focal length and the size of the CCD
+     * typically (1/2, 1/3, 1/4) inch.
+     * If a more realistic camera model is
+     * required the perspective transform of a specific camera can be added
+     */
+    class GLFrameGrabber25D : public FrameGrabber25D
+    {
+    public:
+        /**
+         * @brief constructor
+         * @param width [in] width of image
+         * @param height [in] height of image
+         * @param fov [in] the vertical field of view angle in degree
+         * @param drawer [in] the WorkCellGLDrawer that draws the OpenGL scene
+         * @param state [in] the state of the workcell
+         */
+        GLFrameGrabber25D(
+            int width, int height, double fov,
+            rwlibs::drawable::WorkCellGLDrawer *drawer,
+            rw::kinematics::State &state)
+            :
+            FrameGrabber25D(width,height),
+            _fieldOfView(fov),_drawer(drawer),
+            _perspTrans(rw::math::Transform3D<double>::identity())
+        {}
+
+        /**
+         * @brief destructor
+         */
+        virtual ~GLFrameGrabber25D(){};
+
+        /**
+         * @copydoc FrameGrabber::grab
+         */
+        void grab(rw::kinematics::Frame* frame, const rw::kinematics::State& state);
+
+    private:
+        double _fieldOfView; // in the y-axis
+        rwlibs::drawable::WorkCellGLDrawer *_drawer;
+        rw::math::Transform3D<double> _perspTrans;
+    };
+
+    /* @} */
+}} // end namespaces
+
+#endif // end include guard
