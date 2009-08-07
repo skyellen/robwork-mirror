bb35193cd9706fca0d2a2ef6839ff1a20e127a11
Jimmy Alison Jørgensen <jimali@mmmi.sdu.dk>
2009-08-07 12:32:01 +0000

    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/RobWork/trunk@1156 494c09c7-a67d-49d4-9938-151fb1965be8

---
+++ src/rw/common/Common.hpp
@@ -0,0 +1,39 @@
+/********************************************************************************
+ * Copyright 2009 The Robotics Group, The Maersk Mc-Kinney Moller Institute,
+ * Faculty of Engineering, University of Southern Denmark
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ********************************************************************************/
+
+#ifndef RW_COMMON_COMMON_HPP_
+#define RW_COMMON_COMMON_HPP_
+
+#include "Cache.hpp"
+#include "ConcatVectorIterator.hpp"
+#include "IOUtil.hpp"
+#include "Log.hpp"
+#include "LogWriter.hpp"
+#include "LogBufferedChar.hpp"
+#include "LogBufferedMsg.hpp"
+#include "LogStreamWriter.hpp"
+#include "Message.hpp"
+#include "Property.hpp"
+#include "PropertyBase.hpp"
+#include "PropertyMap.hpp"
+#include "PropertyType.hpp"
+#include "StringUtil.hpp"
+#include "Timer.hpp"
+#include "TimerUtil.hpp"
+#include "VectorIterator.hpp"
+
+#endif /* COMMON_HPP_ */
