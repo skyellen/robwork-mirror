aa65ad4c0b11411e7983f2ed5151bab012cd5a19
Jimmy Alison Jørgensen <jimali@mmmi.sdu.dk>
2009-09-14 01:07:46 +0000

    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/RobWork/trunk@1197 494c09c7-a67d-49d4-9938-151fb1965be8

--- src/rw/sensor/Contact3D.hpp
+++ src/rw/sensor/Contact3D.hpp
@@ -1,7 +1,7 @@
 /********************************************************************************
- * Copyright 2009 The Robotics Group, The Maersk Mc-Kinney Moller Institute, 
- * Faculty of Engineering, University of Southern Denmark 
- * 
+ * Copyright 2009 The Robotics Group, The Maersk Mc-Kinney Moller Institute,
+ * Faculty of Engineering, University of Southern Denmark
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -44,6 +44,9 @@ public:
     rw::math::Vector3D<> n; // Surface contact normal
     rw::math::Vector3D<> f; // the actual force
     double normalForce;
+
+    // index to the geometric primitive on which the contact is located
+    unsigned int _faceIdx,_faceIdx2;
 
     // hmm, dunno about 3d curvature
     double curvature; // surface curvature
