7b069aff08791f5334a1cb535ac6fa792e82e4de
Jimmy Alison Jørgensen <jimali@mmmi.sdu.dk>
2009-09-14 03:06:58 +0000

    header file includes per namespace
    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/RobWork/trunk@1205 494c09c7-a67d-49d4-9938-151fb1965be8

---
+++ src/rw/loaders.hpp
@@ -0,0 +1,46 @@
+/********************************************************************************
+ * Copyright 2009 The Robotics Group, The Maersk Mc-Kinney Moller Institute,
+ * Faculty of Engineering, University of Southern Denmark
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ********************************************************************************/
+/**
+ * @file loaders.hpp
+ *
+ * this file includes all header files from the loaders namespace
+ */
+
+#ifndef RW_LOADERS_HPP_
+#define RW_LOADERS_HPP_
+
+#include "./loaders/colsetup/CollisionSetupLoader.hpp"
+#include "./loaders/image/PGMLoader.hpp"
+#include "./loaders/image/RGBLoader.hpp"
+#include "./loaders/ImageFactory.hpp"
+#include "./loaders/path/PathLoader.hpp"
+#include "./loaders/rwxml/DependencyGraph.hpp"
+#include "./loaders/rwxml/MultipleFileIterator.hpp"
+#include "./loaders/rwxml/XMLParserUtil.hpp"
+#include "./loaders/rwxml/XMLRWLoader.hpp"
+#include "./loaders/rwxml/XMLRWParser.hpp"
+#include "./loaders/rwxml/XMLRWPreParser.hpp"
+//#include "./loaders/TaskLoader.hpp"
+#include "./loaders/tul/Tag.hpp"
+#include "./loaders/tul/TULLoader.hpp"
+#include "./loaders/WorkCellLoader.hpp"
+#include "./loaders/rwxml/XML.hpp"
+#include "./loaders/rwxml/XMLErrorHandler.hpp"
+#include "./loaders/rwxml/XMLParser.hpp"
+#include "./loaders/WorkCellLoader.hpp"
+
+#endif /* LOADERS_HPP_ */
---
+++ src/rw/proximity.hpp
@@ -0,0 +1,37 @@
+/********************************************************************************
+ * Copyright 2009 The Robotics Group, The Maersk Mc-Kinney Moller Institute,
+ * Faculty of Engineering, University of Southern Denmark
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ********************************************************************************/
+/**
+ * @file proximity.hpp
+ *
+ * this file includes all header files from the proximity namespace
+ */
+
+
+#ifndef PROXIMITY_HPP_
+#define PROXIMITY_HPP_
+
+#include "./proximity/CollisionDetector.hpp"
+#include "./proximity/CollisionSetup.hpp"
+#include "./proximity/CollisionStrategy.hpp"
+#include "./proximity/CollisionToleranceStrategy.hpp"
+#include "./proximity/DistanceCalculator.hpp"
+#include "./proximity/DistanceStrategy.hpp"
+#include "./proximity/DistanceToleranceStrategy.hpp"
+#include "./proximity/Proximity.hpp"
+#include "./proximity/ProximityStrategy.hpp"
+
+#endif /* PROXIMITY_HPP_ */
