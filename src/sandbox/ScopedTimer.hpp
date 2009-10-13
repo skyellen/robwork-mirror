a2a8d3d3b670edfc960df68b3361af4c1b73fed4
Lars-Peter Ellekilde <lpe@mmmi.sdu.dk>
2009-10-13 07:37:35 +0000

    Added ScopedTimer to sandbox
    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/RobWork/trunk@1248 494c09c7-a67d-49d4-9938-151fb1965be8

---
+++ src/sandbox/ScopedTimer.hpp
@@ -0,0 +1,58 @@
+/*
+ * ScopedTimer.hpp
+ *
+ *  Created on: 01-12-2008
+ *      Author: jimali
+ */
+
+#ifndef RW_COMMON_SCOPEDTIMER_HPP
+#define RW_COMMON_SCOPEDTIMER_HPP
+
+#include <rw/common/Timer.hpp>
+
+namespace rw {
+namespace common {
+
+    /**
+     * @brief times what is executed in a scope.
+     *
+     * @note usage
+     * \code
+     * ...
+     * long time;
+     * {
+     *   // put code here that is not to be timed
+     *   ScopedTimer timer(time);
+     *   // put code here that is to be timed
+     * }
+     * std::cout << "Time: " << time << std::endl;
+     * \endcode
+     */
+    class ScopedTimer {
+    public:
+        /**
+         * @brief constructor. Starts the timer
+         * @param timer
+         * @return
+         */
+        ScopedTimer(Timer& timer): _timer(timer) {
+        	_timer.resume();
+        }
+
+        /**
+         * @brief destructor, stops the timer
+         * @return
+         */
+        virtual ~ScopedTimer(){
+            _timer.pause();
+        }
+
+    private:
+        Timer& _timer;
+    };
+
+}
+}
+
+
+#endif //End include guard
