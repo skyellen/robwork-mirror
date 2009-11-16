80d67a04b3b0f3df0992525c2766a0f5b21ae2fc
Jimmy Alison Jørgensen <jimali@mmmi.sdu.dk>
2009-11-16 15:54:47 +0000

    qonfigs are now added to base of robot
    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@1336 494c09c7-a67d-49d4-9938-151fb1965be8

--- RobWork/src/rw/loaders/rwxml/XMLRWLoader.cpp
+++ RobWork/src/rw/loaders/rwxml/XMLRWLoader.cpp
@@ -1,7 +1,7 @@
 /********************************************************************************
- * Copyright 2009 The Robotics Group, The Maersk Mc-Kinney Moller Institute, 
- * Faculty of Engineering, University of Southern Denmark 
- * 
+ * Copyright 2009 The Robotics Group, The Maersk Mc-Kinney Moller Institute,
+ * Faculty of Engineering, University of Southern Denmark
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -637,10 +637,10 @@ namespace {
                 rw::math::Q q(config.q.size());
                 for(size_t i = 0; i < q.size(); i++)
                     q[i] = config.q[i];
-
                 DeviceInitState *initDevState = new DeviceInitState(q,model);
                 setup.actions.push_back(initDevState);
             }
+            model->getBase()->getPropertyMap().add<Q>(config.name,"",Q(config.q.size(),&config.q[0]));
         }
 
         return model;
