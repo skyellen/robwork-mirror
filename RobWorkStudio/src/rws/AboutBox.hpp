42eac16637404bd888f9c9f41b661130898245ac
Lars-Peter Ellekilde <lpe@mmmi.sdu.dk>
2009-12-17 22:48:13 +0000

    Added documentation to AboutBox.hpp
    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@1350 494c09c7-a67d-49d4-9938-151fb1965be8

--- RobWorkStudio/src/rws/AboutBox.hpp
+++ RobWorkStudio/src/rws/AboutBox.hpp
@@ -4,14 +4,37 @@
 #include <QDialog>
 #include "ui_AboutBox.h"
 
+/**
+ * @brief AboutBox in RobWorkStudio.
+ *
+ * Displays the RobWork/RobWorkStudio Copyright and license information.
+ * Provides a place for Plugins/Addons to visualize their "about" information.
+ */
 class AboutBox : public QDialog
 {
     Q_OBJECT
-
 public:
+    /**
+     * @brief Constructs about box displaying \b version and \b revision    
+     */ 
     AboutBox(const QString& version, const QString& revision, QWidget *parent = 0);
+    /**
+     * @brief Destructor
+     */
     ~AboutBox();
+
+    /**
+     * @brief Adds about text for a plugin.
+     * @param name [in] Name of the plugin
+     * @param text [in] The text to be displayed
+     */
     void addPluginAboutText(const QString& title, const QString& text);
+
+    /**
+     * @brief Adds widget with about box information for a plugin.
+     * @param name [in] Name of the plugin
+     * @param widget [in] The widget to display
+     */
     void addPluginAboutWidget(const QString& title, QWidget* widget);
 private:
     Ui::AboutBoxClass ui;
