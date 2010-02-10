88bc19b8f41a0d9dd6053a73e2fdc38b280792c2
Rune Søe-Knudsen <rusoe08@student.sdu.dk>
2010-02-10 08:28:18 +0000

    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@1413 494c09c7-a67d-49d4-9938-151fb1965be8

--- RobWorkHardware/build/FindESDCAN.cmake
+++ RobWorkHardware/build/FindESDCAN.cmake
@@ -1,13 +1,16 @@
 
 
-FIND_FILE(NTCAN "ntcan.h" ${ESDCAN_INCLUDE_DIR})
-IF(NOT NTCAN)
+FIND_PATH(ESDCAN_INCLUDE_DIR ntcan.h
+	"$ENV{ProgramFiles}/ESD/CAN/SDK/include"
+)
+IF(NOT ESDCAN_INCLUDE_DIR)
  MESSAGE(STATUS "ESDCAN disabled! ESDCAN not Found!")
 ELSE ()
+ SET(ESDCAN_FOUND 1)
  MESSAGE(STATUS "ESDCAN enabled! ESDCAN Found!")
  
- INCLUDE_DIRECTORIES( ${ESDCAN_INCLUDE_DIR} )
- SET(CANFiles ./IEICAN02/icanapi.cpp
-              ./IEICAN02/IEICANPort.cpp
-              ./ESDCAN/ESDCANPort.cpp)
+ #INCLUDE_DIRECTORIES( ${ESDCAN_INCLUDE_DIR} )
+ #SET(CANFiles ./IEICAN02/icanapi.cpp
+ #            ./IEICAN02/IEICANPort.cpp
+ #             ./ESDCAN/ESDCANPort.cpp)
 ENDIF ()
