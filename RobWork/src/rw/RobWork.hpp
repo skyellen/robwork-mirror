3f5a15bd5a84db1e9cffe7bde4eddb6e7ac6bbb6
Lars-Peter Ellekilde <lpe@mmmi.sdu.dk>
2010-07-07 06:41:08 +0000

    Updates to RobWork.hpp documentation
    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@1899 494c09c7-a67d-49d4-9938-151fb1965be8

--- RobWork/src/rw/RobWork.hpp
+++ RobWork/src/rw/RobWork.hpp
@@ -1,3 +1,20 @@
+/********************************************************************************
+ * Copyright 2009 The Robotics Group, The Maersk Mc-Kinney Moller Institute, 
+ * Faculty of Engineering, University of Southern Denmark 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ********************************************************************************/
+
 #ifndef RW_ROBWORK_HPP
 #define RW_ROBWORK_HPP
 
@@ -8,20 +25,43 @@
 
 namespace rw {
 
+/**
+ * @brief RobWork instance which holds objects to be shared among multiple plugins
+ *
+ * A RobWork instance contains common objects which may be used by multiple plugins
+ * which may originate from different shared libraries.
+ */
 class RobWork
 {
 public:
+    /**
+     * @brief Creates RobWork instance
+     */
     RobWork(void);
+
+    /**
+     * @brief Closes all plugins, stream etc. hold by the RobWork instance before destruction
+     */
     ~RobWork(void);
 
+    /**
+     * @brief Returns the PluginRepository 
+     * @return PluginRepository
+     */
     rw::plugin::PluginRepository& getPluginRepository() {
         return _pluginRepository;
     }
 
+    /**
+     * @brief Returns the common log
+     */
     rw::common::Log& getLog() {
         return _log;
     }
 
+    /**
+     * @brief Returns the version of RobWork
+     */
     std::string getVersion() const {
         return RW_VERSION;
     }
