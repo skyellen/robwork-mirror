88dfe7940dbc5dc39f0f761cb3fcbac16a30ef81
Jimmy Alison Jørgensen <jimali@mmmi.sdu.dk>
2010-09-03 10:36:55 +0000

    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@1996 494c09c7-a67d-49d4-9938-151fb1965be8

---
+++ RobWorkStudio/test/TestSuiteConfig.hpp
@@ -0,0 +1,10 @@
+// -*- c++ -*-
+
+#include <string>
+#include <RobWorkConfig.hpp>
+
+#define RW_TEST_FILE_DIR "C:/local/rwworkspace/RobWorkStudio/test/testfiles/"
+
+namespace {
+	std::string testFilePath() { return RW_TEST_FILE_DIR; }
+}
