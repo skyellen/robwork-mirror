8ce4873bd310d60f3115b4e558bd05a7717efae1
Anders Glent Buch <anbu@mmmi.sdu.dk>
2010-11-01 12:03:35 +0000

    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@2218 494c09c7-a67d-49d4-9938-151fb1965be8

--- RobWorkStudio/src/rws/plugins/sensors/ui_SensorsPlugin.h
+++ RobWorkStudio/src/rws/plugins/sensors/ui_SensorsPlugin.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'SensorsPlugin.ui'
 **
-** Created: Sat 30. Oct 17:30:51 2010
+** Created: Mon 1. Nov 12:27:41 2010
 **      by: Qt User Interface Compiler version 4.7.0
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
