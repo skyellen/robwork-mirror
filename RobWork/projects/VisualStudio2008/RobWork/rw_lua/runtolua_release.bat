9173e73fa3eb65240225612364c636697e51267e
Anders Glent Buch <anbu@mmmi.sdu.dk>
2011-01-18 11:27:40 +0000

    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@2438 494c09c7-a67d-49d4-9938-151fb1965be8

---
+++ RobWork/projects/VisualStudio2008/RobWork/rw_lua/runtolua_release.bat
@@ -0,0 +1,11 @@
+..\Release\tolua -o ../../../../src/rwlibs/lua/LuaMathStub.cpp -H ../../../../src/rwlibs/lua/LuaMathStub.hpp -n LuaMath ../../../../src/rwlibs/lua/LuaMath.pkg
+..\Release\tolua -o ../../../../src/rwlibs/lua/LuaKinematicsStub.cpp -H ../../../../src/rwlibs/lua/LuaKinematicsStub.hpp -n LuaKinematics ../../../../src/rwlibs/lua/LuaKinematics.pkg
+..\Release\tolua -o ../../../../src/rwlibs/lua/LuaLoadersStub.cpp -H ../../../../src/rwlibs/lua/LuaLoadersStub.hpp -n LuaLoaders ../../../../src/rwlibs/lua/LuaLoaders.pkg
+..\Release\tolua -o ../../../../src/rwlibs/lua/LuaModelsStub.cpp -H ../../../../src/rwlibs/lua/LuaModelsStub.hpp -n LuaModels ../../../../src/rwlibs/lua/LuaModels.pkg
+..\Release\tolua -o ../../../../src/rwlibs/lua/LuaSensorStub.cpp -H ../../../../src/rwlibs/lua/LuaSensorStub.hpp -n LuaSensor ../../../../src/rwlibs/lua/LuaSensor.pkg
+..\Release\tolua -o ../../../../src/rwlibs/lua/LuaTrajectoryStub.cpp -H ../../../../src/rwlibs/lua/LuaTrajectoryStub.hpp -n LuaTrajectory ../../../../src/rwlibs/lua/LuaTrajectory.pkg
+..\Release\tolua -o ../../../../src/rwlibs/lua/LuaProximityStub.cpp -H ../../../../src/rwlibs/lua/LuaProximityStub.hpp -n LuaProximity ../../../../src/rwlibs/lua/LuaProximity.pkg
+..\Release\tolua -o ../../../../src/rwlibs/lua/LuaCommonStub.cpp -H ../../../../src/rwlibs/lua/LuaCommonStub.hpp -n LuaCommon ../../../../src/rwlibs/lua/LuaCommon.pkg
+..\Release\tolua -o ../../../../src/rwlibs/lua/LuaPathPlanningStub.cpp -H ../../../../src/rwlibs/lua/LuaPathPlanningStub.hpp -n LuaPathPlanning ../../../../src/rwlibs/lua/LuaPathPlanning.pkg
+    
+
