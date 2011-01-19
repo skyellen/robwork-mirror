31585b61214b686b55e7995606de6a465cbf01fd
Jimmy Alison Jørgensen <jimali@mmmi.sdu.dk>
2011-01-19 13:06:07 +0000

    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@2489 494c09c7-a67d-49d4-9938-151fb1965be8

---
+++ RobWork/src/sandbox/graspplanning/ParallelGraspPlanner.cpp
@@ -0,0 +1,4 @@
+#include "ParallelGraspPlanner.hpp"
+
+
+
---
+++ RobWork/src/sandbox/graspplanning/ParallelGraspPlanner.hpp
@@ -0,0 +1,23 @@
+/*
+ * ParallelGraspPlanner.hpp
+ *
+ *  Created on: 19/12/2010
+ *      Author: jimali
+ */
+
+#ifndef PARALLELGRASPPLANNER_HPP_
+#define PARALLELGRASPPLANNER_HPP_
+
+
+/**
+ * @brief this
+ */
+class ParallelGraspPlanner {
+public:
+
+
+
+
+};
+
+#endif /* PARALLELGRASPPLANNER_HPP_ */
