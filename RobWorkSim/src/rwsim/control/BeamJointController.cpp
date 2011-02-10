923f45f736d1903b5c56f810c83d1db5bb835774
Jimmy Alison Jørgensen <jimali@mmmi.sdu.dk>
2011-02-10 13:55:47 +0000

    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@2643 494c09c7-a67d-49d4-9938-151fb1965be8

---
+++ RobWorkSim/src/rwsim/control/BeamJointController.cpp
@@ -0,0 +1,96 @@
+#include "BeamJointController.hpp"
+
+#include <rw/common/macros.hpp>
+
+using namespace rwsim::control;
+using namespace rwsim::dynamics;
+using namespace rw::math;
+
+namespace {
+
+}
+
+BeamJointController::BeamJointController(
+        const std::string& name,
+        RigidDevice* rdev,
+		ControlMode cmode,
+		double dt):
+	JointController(name, &rdev->getModel()),
+	_ddev(rdev),
+	_lastError(rw::math::Q::zero(rdev->getModel().getDOF())),
+	_target(rw::math::Q::zero(rdev->getModel().getDOF())),
+	_currentQ(_target),
+	_currentVel(rw::math::Q::zero(_target.size())),
+	_targetVel(rw::math::Q::zero(_target.size())),
+	_mode(cmode),
+	_stime(dt)
+{
+
+}
+
+void BeamJointController::setTargetPos(const rw::math::Q& target){
+    _target = target;
+}
+
+void BeamJointController::setTargetVel(const rw::math::Q& vals){
+	_targetVel = vals;
+}
+
+void BeamJointController::setTargetAcc(const rw::math::Q& vals){};
+
+
+double BeamJointController::getSampleTime(){
+	return _stime;
+}
+
+void BeamJointController::setSampleTime(double stime){
+	_stime = stime;
+}
+
+void BeamJointController::update(double dt, rw::kinematics::State& state) {
+    // all joints in the device are dependent on a single input
+    double pressure = _target[0];
+
+    double angle = pressure;
+
+    // the pressure indicate the size of the torques that are applied on the beam joints.
+    // the closer the beamjoints are at thier resting configuration the smaller torque
+    std::cout << "a";
+	Q q = _ddev->getModel().getQ(state);
+    std::cout << "a";
+
+	Q q_error = Q(q.size(), angle)-q;
+    std::cout << "a";
+
+	// the error in configuration result in a torque
+    std::cout << "a";
+
+	Q torque = q;
+
+    std::cout << "a";
+for(int i=0;i<q.size();i++){ torque[i] = 2; }
+
+std::cout << "a";
+	_ddev->setForceLimit(torque);
+    std::cout << "a";
+	_ddev->setVelocity(q_error, state);
+    std::cout << "a";
+
+	_currentQ = q;
+    std::cout << "a";
+
+}
+
+void BeamJointController::reset(const rw::kinematics::State& state){
+    _currentQ = _ddev->getModel().getQ(state);
+    _target = _currentQ;
+    _targetVel = rw::math::Q::zero(_currentQ.size());
+    //_time = 0;
+}
+
+void BeamJointController::setControlMode(ControlMode mode){
+    if(mode!=POSITION || mode !=VELOCITY )
+        RW_THROW("Unsupported control mode!");
+    _mode = mode;
+}
+
---
+++ RobWorkSim/src/rwsim/control/BeamJointController.hpp
@@ -0,0 +1,121 @@
+#ifndef RWSIM_CONTROL_BeamJointController_HPP_
+#define RWSIM_CONTROL_BeamJointController_HPP_
+
+//! @file BeamJointController.hpp
+
+#include <rwlibs/control/JointController.hpp>
+#include <rwlibs/simulation/SimulatedController.hpp>
+
+#include <rwsim/dynamics/RigidDevice.hpp>
+
+namespace rwsim {
+namespace control {
+	//! @addtogroup control @{
+
+	/**
+	 * @brief The beamjoint controller controls a joint device composed of coupled beam joints.
+	 * The coupling of beamjoints is somewhat special since its not an configuration space
+	 * coupling (position) but rather a coupling of the forces acting on the joints.
+	 *
+	 * This controller continuesly updates the torques acting on the joints by considering the
+	 * current deflection of the joint.
+	 *
+	 */
+	class BeamJointController: public rwlibs::control::JointController, public rwlibs::simulation::SimulatedController {
+	public:
+
+
+		/**
+		 * @brief constructor
+		 * @param rdev [in] device that is to be controlled
+		 * @param state [in] target state
+		 * @param cmode [in] the control mode used
+		 * @param pdparams [in] list of pd parameters. must be same length as number of joints.
+		 * @param dt [in] the sampletime (time between samples in seconds) used in the control
+		 * loop, this should be larger than the expected update sample time.
+		 */
+		BeamJointController(
+		        const std::string& name,
+		        dynamics::RigidDevice* rdev,
+				ControlMode cmode,
+				double dt
+				);
+
+
+		/**
+		 * @brief destructor
+		 */
+		virtual ~BeamJointController(){};
+
+
+		/**
+		 * @brief the time between samples
+		 * @return the sample time in seconds
+		 */
+		double getSampleTime();
+
+		/**
+		 * @brief set the time between samples in seconds
+		 * @param stime [in] sample time
+		 */
+		void setSampleTime(double stime);
+
+		//! @copydoc SimulatedController::update
+		void update(double dt, rw::kinematics::State& state);
+
+		//! @copydoc SimulatedController::reset
+		void reset(const rw::kinematics::State& state);
+
+		//! @copydoc SimulatedController::getController
+		Controller* getController(){ return this; };
+
+		std::string getControllerName(){ return getName(); };
+
+		////// inherited from JointController
+
+		/**
+		 * @copydoc JointController::getControlModes
+		 *
+		 * This controller supports both position and velocity control.
+		 */
+		unsigned int getControlModes(){return _mode;}
+
+		//! @copydoc JointController::setControlModes
+		void setControlMode(ControlMode mode);
+
+		//! @copydoc JointController::setTargetPos
+		void setTargetPos(const rw::math::Q& target);
+
+		//! @copydoc JointController::setTargetVel
+		void setTargetVel(const rw::math::Q& vals);
+
+		//! @copydoc JointController::setTargetAcc
+		void setTargetAcc(const rw::math::Q& vals);
+
+		//! @copydoc JointController::getQ
+		rw::math::Q getQ(){ return _currentQ;}
+
+		//! @copydoc JointController::getQd
+		rw::math::Q getQd(){ return _currentVel;}
+
+	private:
+		BeamJointController();
+
+	private:
+		//std::vector<rw::models::BeamJoint*> _beamJoints;
+
+		dynamics::RigidDevice *_ddev;
+		rw::math::Q _maxVel;
+		rw::math::Q _lastError, _target, _currentQ, _currentVel;
+		rw::math::Q _targetVel;
+		ControlMode _mode;
+		double _stime, _accTime; // sample time
+		rw::math::Q  _P, _D;
+	};
+
+	typedef rw::common::Ptr<BeamJointController> BeamJointControllerPtr;
+	//! @}
+}
+}
+
+#endif /*BeamJointController_HPP_*/
