35fc08a10cddf0e66a03421eab0876e6d9f810a7
Lars-Peter Ellekilde <lpe@mmmi.sdu.dk>
2011-02-22 15:56:24 +0000

    Added the start of some Statistics tools to sandbox
    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@2723 494c09c7-a67d-49d4-9938-151fb1965be8

---
+++ RobWork/src/sandbox/Statistics.hpp
@@ -0,0 +1,20 @@
+
+#ifndef RW_PATH_STATISTICS_HPP
+#define RW_PATH_STATISTICS_HPP
+
+namespace rw {
+namespace math {
+
+class Statistics {
+public:
+	/*std::pair<double, double> normalDistribution(std::vector<double>& data);
+
+	std::pair<Q, Q> normalDistrubitionIndependent(std::vector<Q>& data);
+	
+*/
+
+};
+
+} } //end namespaces
+
+#endif /* RW_PATH_STATISTICS_HPP */
