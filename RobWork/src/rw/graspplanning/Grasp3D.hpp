4460794f6d6cc87d207f068d71e4c284cb038487
Jimmy Alison Jørgensen <jimali@mmmi.sdu.dk>
2011-03-08 13:57:37 +0000

    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@2764 494c09c7-a67d-49d4-9938-151fb1965be8

--- RobWork/src/rw/graspplanning/Grasp3D.hpp
+++ RobWork/src/rw/graspplanning/Grasp3D.hpp
@@ -25,35 +25,35 @@
 namespace rw {
 namespace graspplanning {
 
-/**
- * @brief a grasp is a set of contacts between the object to be grasped and
- * the robot gripper.
- */
-class Grasp3D {
-public:
+    /**
+     * @brief a grasp is a set of contacts between the object to be grasped and
+     * the robot gripper.
+     */
+    class Grasp3D {
+    public:
 
-    Grasp3D(int nrOfContacts=1):
-        contacts(nrOfContacts),
-        approach(nrOfContacts)
-    {}
+        Grasp3D(int nrOfContacts=1):
+            contacts(nrOfContacts),
+            approach(nrOfContacts)
+        {}
 
-    Grasp3D(const std::vector<rw::sensor::Contact3D> cons):
-        contacts(cons),
-        approach(cons.size())
-    {}
+        Grasp3D(const std::vector<rw::sensor::Contact3D> cons):
+            contacts(cons),
+            approach(cons.size())
+        {}
 
-    void scale(double clerance){
-        for(size_t i=0;i<contacts.size();i++)
-            contacts[i].p += normalize(approach[i])*clerance;
-    }
+        void scale(double clerance){
+            for(size_t i=0;i<contacts.size();i++)
+                contacts[i].p += normalize(approach[i])*clerance;
+        }
 
-    double phi,psi,quality;
-    std::vector<rw::sensor::Contact3D> contacts;
-    std::vector<rw::math::Vector3D<> > approach;
-    rw::math::Vector3D<> center;
+        double phi,psi,quality;
+        std::vector<rw::sensor::Contact3D> contacts;
+        std::vector<rw::math::Vector3D<> > approach;
+        rw::math::Vector3D<> center;
 
 
-};
+    };
 }
 }
 #endif /*GRASP_HPP_*/
