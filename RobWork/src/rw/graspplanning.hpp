97df022dc4e7ab2cb5d37ac09cd7780ed62b02da
Jimmy Alison Jørgensen <jimali@mmmi.sdu.dk>
2011-03-09 08:09:17 +0000

    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@2786 494c09c7-a67d-49d4-9938-151fb1965be8

---
+++ RobWork/src/rw/graspplanning.hpp
@@ -0,0 +1,44 @@
+/********************************************************************************
+ * Copyright 2009 The Robotics Group, The Maersk Mc-Kinney Moller Institute,
+ * Faculty of Engineering, University of Southern Denmark
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ********************************************************************************/
+/**
+ * @file rw/kinematics.hpp
+ *
+ * this file includes all header files from the kinematics namespace
+ */
+
+#ifndef RW_GRASPPLANNING_HPP_
+#define RW_GRASPPLANNING_HPP_
+
+#include "graspplanning/ApproachMeasure3D.hpp"
+#include "graspplanning/CMDistCCPMeasure3D.hpp"
+#include "graspplanning/CompositeGraspFilter.hpp"
+#include "graspplanning/CompositeContactFilter.hpp"
+#include "graspplanning/ContactDistThresFilter.hpp"
+#include "graspplanning/Contour2DInfoMap.hpp"
+#include "graspplanning/CurvatureThresFilter.hpp"
+#include "graspplanning/DiceContactG3D.hpp"
+#include "graspplanning/Grasp2D.hpp"
+#include "graspplanning/Grasp3D.hpp"
+#include "graspplanning/GraspQualityMeasure3D.hpp"
+#include "graspplanning/GraspTable.hpp"
+#include "graspplanning/GraspValidateFilter.hpp"
+#include "graspplanning/PlaneClearanceFilter.hpp"
+#include "graspplanning/QualityMeasure2D.hpp"
+#include "graspplanning/SemiForceClosureFilter.hpp"
+#include "graspplanning/WrenchMeasure3D.hpp"
+
+#endif /* graspplanning_HPP_ */
