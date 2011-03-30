f9927b8a91974d8ac57c7f9a54e531e0490660a6
Jimmy Alison Jørgensen <jimali@mmmi.sdu.dk>
2011-03-30 10:20:29 +0000

    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@2852 494c09c7-a67d-49d4-9938-151fb1965be8

---
+++ RobWork/src/rw/graspplanning/GWSMeasure3D.cpp
@@ -0,0 +1,121 @@
+/********************************************************************************
+ * Copyright 2009 The Robotics Group, The Maersk Mc-Kinney Moller Institute,
+ * Faculty of Engineering, University of Southern Denmark
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ********************************************************************************/
+
+#include "GWSMeasure3D.hpp"
+
+#include <rw/math/EAA.hpp>
+#include <rw/math/Vector3D.hpp>
+#include <rw/math/Constants.hpp>
+#include <boost/foreach.hpp>
+#include <iostream>
+
+using namespace rw::math;
+using namespace rw::geometry;
+using namespace rw::graspplanning::sandbox;
+
+
+namespace {
+
+    /**
+     * calculate the normalized friction cone from a force and friction
+     * @param force
+     * @param mu
+     * @return
+     */
+    std::vector<Vector3D<> > getCone(const Vector3D<>& normal, double nforce, double mu, int resolution){
+        Vector3D<> unormal = normalize(normal);
+        //double angle = atan(mu);
+        //tan(angle)*1
+        // create a vector that is tangent to the normal
+        const double EPSILON = 0.000001;
+        Vector3D<> tdir;
+        if( fabs(normal(0))<EPSILON && fabs(normal(1))<EPSILON ){
+            tdir = normalize(  Vector3D<>(0,-normal(2),normal(1)) );
+        } else {
+            tdir = normalize(  Vector3D<>(-normal(1),normal(0),0) );
+        }
+        Vector3D<> tdirScaled = tdir * (nforce * mu);
+
+        Vector3D<> fnormal = unormal * nforce;
+        std::vector<Vector3D<> > coneVerts;
+        // rotate the vector around the normal in some fixed steps
+        Vector3D<> P = tdirScaled; // vector to rotate
+        Vector3D<> A = unormal; // vector to rotate about
+
+        EAA<> eaa(unormal, 2*Pi/resolution );
+        Rotation3D<> rot( eaa.toRotation3D() );
+        for(int i=0; i<resolution-1; i++){
+            coneVerts.push_back(fnormal+P);
+            P = rot*P;
+        }
+        return coneVerts;
+    }
+
+}
+
+GWSMeasure3D::GWSMeasure3D(int resolution, bool useUnitVectors):
+    _chullCalculator( rw::common::ownedPtr(new QHullND<6>() ) ),
+    _resolution(resolution),
+    _useUnitVectors(useUnitVectors),
+    _lambda(1.0/0.1)
+{
+}
+
+
+double GWSMeasure3D::quality(const rw::graspplanning::Grasp3D& grasp) const {
+    std::vector< QHullND<6>::VectorND > vertices;
+
+    BOOST_FOREACH(const rw::sensor::Contact3D& c, grasp.contacts ){
+        // std::cout  << "get cone: " << c.n << " " << c.normalForce << std::endl;
+         if(c.normalForce<0.0001){
+        	 RW_WARN("Normal force too small! : " << c.normalForce);
+        	 continue;
+         }
+
+    	 Vector3D<> arm = c.p - _objCenter;
+    	 double normalForce = 1.0;
+    	 if(!_useUnitVectors)
+    	     normalForce = c.normalForce;
+         std::vector<Vector3D<> > verts = getCone(c.n,normalForce,c.mu,_resolution);
+         BOOST_FOREACH(const Vector3D<> &force, verts){
+             Vector3D<> torque =  cross(arm,force);
+             QHullND<6>::VectorND vertice;
+             vertice[0] = force[0];
+             vertice[1] = force[1];
+             vertice[2] = force[2];
+             vertice[3] = _lambda * torque[0];
+             vertice[4] = _lambda * torque[1];
+             vertice[5] = _lambda * torque[2];
+             vertices.push_back(vertice);
+         }
+    }
+
+
+
+    // first do the force space
+    _chullCalculator->rebuild( vertices );
+
+    QHullND<6>::VectorND origin;
+    for(int i=0;i<6;i++) origin[i] = 0;
+
+    // test if the center is inside
+    //_isInside = _chullCalculator->isInside( origin );
+    _minWrench = _chullCalculator->getMinDistInside( origin, vertices);
+    _isInside = _minWrench>=0;
+
+    return _minWrench;
+}
---
+++ RobWork/src/rw/graspplanning/GWSMeasure3D.hpp
@@ -0,0 +1,95 @@
+/********************************************************************************
+ * Copyright 2009 The Robotics Group, The Maersk Mc-Kinney Moller Institute,
+ * Faculty of Engineering, University of Southern Denmark
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ********************************************************************************/
+
+
+#ifndef RW_GRASPPLANNING_SANDBOX_GWSMeasure3D_HPP_
+#define RW_GRASPPLANNING_SANDBOX_GWSMeasure3D_HPP_
+
+
+#include <rw/math/Vector3D.hpp>
+#include <rw/geometry/PlainTriMesh.hpp>
+#include <rw/graspplanning/GraspQualityMeasure3D.hpp>
+#include <rw/sensor/Contact3D.hpp>
+#include <rw/geometry/ConvexHull3D.hpp>
+#include <sandbox/QHullND.hpp>
+
+namespace rw {
+namespace graspplanning {
+
+/**
+ * @brief Represents the grasp wrench space as a 3D force space and a
+ * 3D torque space.
+ *
+ *
+ */
+class GWSMeasure3D: public GraspQualityMeasure3D {
+public:
+    /**
+     * @brief constructor
+     * @param chull [in] a convex hull factory
+     * @param resolution [in] the number of vertices to use in the approximation
+     * of the friction cone.
+     * @param useUnitVectors [in] true if the unit wrench should be used, false otherwise
+     */
+    GWSMeasure3D(int resolution=8, bool useUnitVectors=false);
+
+	/**
+	 * @brief destructor
+	 */
+    virtual ~GWSMeasure3D(){};
+
+    //// inherited from quality measure
+    double quality(const rw::graspplanning::Grasp3D& grasp) const;
+
+    //// member functions
+    /**
+     * @brief set the center of the object
+     * @param center
+     */
+    void setObjectCenter( const rw::math::Vector3D<>& center){ _objCenter = center; };
+
+    /**
+     * @brief tests if center of object is inside wrench space
+     * @return true if center is inside both force and torque space
+     */
+    bool isGraspStable(){ return _isInside;};
+
+    /**
+     * @brief get the minimum force that will break the grasp
+     * @return minimum breaking force
+     */
+    double getMinWrench(){ return _minWrench; };
+
+    void setLambda(double lambda){ _lambda = lambda;}
+
+private:
+    rw::common::Ptr<rw::geometry::QHullND<6> > _chullCalculator;
+    rw::math::Vector3D<> _objCenter;
+    int _resolution;
+
+    mutable bool _isInside;
+    mutable double _minWrench;
+    bool _useUnitVectors;
+
+    double _lambda;
+};
+
+
+}
+}
+
+#endif /* FORCECLOSURE3D_HPP_ */
