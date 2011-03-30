1fa9e0ebe0406f730d9356b401c3169c0e3edd87
Jimmy Alison Jørgensen <jimali@mmmi.sdu.dk>
2011-03-30 11:38:33 +0000

    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@2858 494c09c7-a67d-49d4-9938-151fb1965be8

--- RobWork/src/rw/graspplanning/WrenchMeasure3D.hpp
+++ RobWork/src/rw/graspplanning/WrenchMeasure3D.hpp
@@ -25,81 +25,13 @@
 #include "GraspQualityMeasure3D.hpp"
 #include <rw/sensor/Contact3D.hpp>
 #include <rw/geometry/ConvexHull3D.hpp>
-
+#include <rw/graspplanning/GWSMeasure3D.hpp>
 
 namespace rw {
 namespace graspplanning {
 
-/**
- * @brief Represents the grasp wrench space as a 3D force space and a
- * 3D torque space.
- *
- *
- */
-class WrenchMeasure3D: public GraspQualityMeasure3D {
-public:
-    /**
-     * @brief constructor
-     * @param chull [in] a convex hull factory
-     * @param resolution [in] the number of vertices to use in the approximation
-     * of the friction cone.
-     * @param useUnitVectors [in] true if the unit wrench should be used, false otherwise
-     */
-    WrenchMeasure3D(int resolution=8, bool useUnitVectors=false);
 
-    /**
-     * @brief constructor
-     * @param chull [in] a convex hull factory
-     * @param resolution [in] the number of vertices to use in the approximation
-     * of the friction cone.
-     * @param useUnitVectors [in] true if the unit wrench should be used, false otherwise
-     */
-    WrenchMeasure3D(rw::geometry::ConvexHull3D::Ptr chull, int resolution=8, bool useUnitVectors=false):
-        _chullCalculator( chull ), _resolution(resolution),_useUnitVectors(useUnitVectors)
-    {}
-
-	/**
-	 * @brief destructor
-	 */
-    virtual ~WrenchMeasure3D(){};
-
-    //// inherited from quality measure
-    double quality(const Grasp3D& grasp) const;
-
-    //// member functions
-    /**
-     * @brief set the center of the object
-     * @param center
-     */
-    void setObjectCenter( const rw::math::Vector3D<>& center){ _objCenter = center; };
-
-    /**
-     * @brief tests if center of object is inside wrench space
-     * @return true if center is inside both force and torque space
-     */
-    bool isGraspStable(){ return _isForceInside && _isTorqueInside;};
-
-    /**
-     * @brief get the minimum force that will break the grasp
-     * @return minimum breaking force
-     */
-    double getMinForce(){ return _minForce; };
-
-    /**
-     * @brief get the minimum torque that will break the grasp
-     * @return minimum breaking torque
-     */
-    double getMinTorque(){ return _minTorque; };
-
-private:
-    rw::common::Ptr<rw::geometry::ConvexHull3D> _chullCalculator;
-    rw::math::Vector3D<> _objCenter;
-    int _resolution;
-
-    mutable bool _isForceInside,_isTorqueInside;
-    mutable double _minForce,_minTorque;
-    bool _useUnitVectors;
-};
+    typedef GWSMeasure3D WrenchMeasure3D;
 
 }
 }
