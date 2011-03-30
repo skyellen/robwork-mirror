39d39d8dd68cfc69e15c3ddcaf5b81cfcb49f714
Jimmy Alison Jørgensen <jimali@mmmi.sdu.dk>
2011-03-30 11:37:25 +0000

    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@2856 494c09c7-a67d-49d4-9938-151fb1965be8

---
+++ RobWork/src/rw/math/Vector.hpp
@@ -0,0 +1,494 @@
+/********************************************************************************
+ * Copyright 2009 The Robotics Group, The Maersk Mc-Kinney Moller Institute, 
+ * Faculty of Engineering, University of Southern Denmark 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ********************************************************************************/
+
+
+#ifndef RW_MATH_VECTOR_HPP
+#define RW_MATH_VECTOR_HPP
+
+/**
+ * @file Vector.hpp
+ */
+#include <rw/common/macros.hpp>
+#include <boost/numeric/ublas/vector.hpp>
+#include <boost/numeric/ublas/vector_expression.hpp>
+#include <boost/numeric/ublas/io.hpp>
+
+namespace rw { namespace math {
+
+
+    /**
+     * @brief Configuration vector
+     */
+    template<class T = double>
+    class Vector
+    {
+    public:
+        //! The type of the internal Boost vector implementation.
+        typedef boost::numeric::ublas::vector<T> Base;
+
+        //! The Boost vector expression for initialization to zero.
+        typedef boost::numeric::ublas::zero_vector<T> ZeroBase;
+
+        //! Const forward iterator.
+        typedef typename Base::const_iterator const_iterator;
+
+        //! Forward iterator.
+        typedef typename Base::iterator iterator;
+
+        //! Value type.
+        typedef typename Base::value_type value_type;
+
+        //! Reference type.
+        typedef typename Base::reference reference;
+
+        //! Pointer type.
+        typedef typename Base::pointer pointer;
+
+        //! Const pointer type.
+        typedef typename Base::const_pointer const_pointer;
+
+        //! Difference type.
+        typedef typename Base::difference_type difference_type;
+
+        /**
+         * @brief A configuration of vector of length \b dim.
+         */
+        explicit Vector(size_t dim) : _vec(dim) {}
+
+        /**
+         * @brief Default constructor.
+         *
+         * The vector will be of dimension zero.
+         */
+        Vector() : _vec(0) {}
+
+        /**
+         * @brief Creates a Vector of length \b n and initialized with values from \b values
+         *
+         * The method reads n values from \b values and do not check whether reading out of bounds.
+         *
+         * @param n [in] Length of q.
+         * @param values [in] Values to initialize with
+         */
+        Vector(size_t n, const T* values):
+            _vec(n)
+        {
+            for (size_t i = 0; i<n; i++)
+                _vec(i) = values[i];
+        }
+
+        /**
+         * @brief Creates a Vector of length \b n and initialize all values in Vector to \b value
+         *
+         * @param n [in] Length of q.
+         * @param value [in] Value to initialize
+         */
+        Vector(size_t n, T value):
+            _vec(n)
+        {
+            for (size_t i = 0; i<n; i++)
+                _vec(i) = value;
+        }
+
+
+        /**
+         * @brief Returns Vector of length \b n initialized with 0's
+         */
+        static Vector zero(int n)
+        {
+            return Vector(ZeroBase(n));
+        }
+
+        /**
+         * @brief The dimension of the configuration vector.
+         */
+        size_t size() const { return m().size(); }
+
+        /**
+           @brief True if the configuration is of dimension zero.
+         */
+        bool empty() const { return size() == 0; }
+
+        /**
+         * @brief Construct a configuration vector from a Boost vector
+         * expression.
+         *
+         * @param r [in] An expression for a vector of doubles
+         */
+        template <class R>
+        explicit Vector(const boost::numeric::ublas::vector_expression<R>& r) :
+            _vec(r)
+        {}
+
+        /**
+         * @brief Accessor for the internal Boost vector state.
+         */
+        const Base& m() const { return _vec; }
+
+        /**
+         * @brief Accessor for the internal Boost vector state.
+         */
+        Base& m() { return _vec; }
+
+        /**
+           @brief Start of sequence iterator.
+        */
+        const_iterator begin() const { return m().begin(); }
+
+        /**
+           @brief End of sequence iterator.
+        */
+        const_iterator end() const { return m().end(); }
+
+        /**
+           @brief Start of sequence iterator.
+        */
+        iterator begin() { return m().begin(); }
+
+        /**
+           @brief End of sequence iterator.
+        */
+        iterator end() { return m().end(); }
+
+        /**
+         * @brief Extracts a sub part (range) of this Vector.
+         * @param start [in] Start index
+         * @param cnt [in] the number of elements to include
+         * @return
+         */
+        const Vector getSubPart(size_t start, size_t cnt) const {
+            RW_ASSERT(start+cnt <= size());
+
+            Vector res(cnt);
+            for (size_t i = 0; i<cnt; i++) {
+                res(i) = (*this)[start+i];
+            }
+            return res;
+        }
+
+        void setSubPart(size_t index, const Vector& part) {
+            RW_ASSERT(index + part.size() <= size());
+            for (size_t i = 0; i<part.size(); i++) {
+                (*this)[index+i] = part(i);
+            }
+        }
+
+        //----------------------------------------------------------------------
+        // Norm utility methods
+
+        /**
+         * @brief Returns the Euclidean norm (2-norm) of the configuration
+         * @return the norm
+         */
+        T norm2() const {
+            return norm_2(m());
+        }
+
+        /**
+         * @brief Returns the Manhatten norm (1-norm) of the configuration
+         * @return the norm
+         */
+        T norm1() const {
+            return norm_1(m());
+        }
+
+        /**
+         * @brief Returns the infinte norm (\f$\inf\f$-norm) of the configuration
+         * @return the norm
+         */
+        T normInf() const {
+            return norm_inf(m());
+        }
+
+        //----------------------------------------------------------------------
+        // Various operators
+
+        /**
+         * @brief Returns reference to vector element
+         * @param i [in] index in the vector
+         * @return const reference to element
+         */
+        const T& operator()(size_t i) const { return m()(i); }
+
+        /**
+         * @brief Returns reference to vector element
+         * @param i [in] index in the vector
+         * @return reference to element
+         */
+        T& operator()(size_t i) { return m()(i); }
+
+        /**
+         * @brief Returns reference to vector element
+         * @param i [in] index in the vector
+         * @return const reference to element
+         */
+        const T& operator[](size_t i) const { return m()(i); }
+
+        /**
+         * @brief Returns reference to vector element
+         * @param i [in] index in the vector
+         * @return reference to element
+         */
+        T& operator[](size_t i) { return m()(i); }
+
+        /**
+           @brief Scalar division.
+         */
+        const Vector operator/(T s) const
+        {
+            return Vector(m() / s);
+        }
+
+        /**
+         * @brief Scalar multiplication.
+         */
+        const Vector operator*(T s) const
+        {
+            return Vector(m() * s);
+        }
+
+        /**
+         * @brief Scalar multiplication.
+         */
+        friend const Vector operator*(T s, const Vector& v)
+        {
+            return Vector(s * v.m());
+        }
+
+        /**
+         * @brief Vector subtraction.
+         */
+        const Vector operator-(const Vector& b) const
+        {
+            return Vector(m() - b.m());
+        }
+
+        /**
+         * @brief Vector addition.
+         */
+        const Vector operator+(const Vector& b) const
+        {
+            return Vector(m() + b.m());
+        }
+
+        /**
+         * @brief Scalar multiplication.
+         */
+        Vector& operator*=(T s)
+        {
+            m() *= s;
+            return *this;
+        }
+
+        /**
+         * @brief Scalar division.
+         */
+        Vector& operator/=(T s)
+        {
+            m() /= s;
+            return *this;
+        }
+
+        /**
+         * @brief Vector addition.
+         */
+        Vector& operator+=(const Vector& v)
+        {
+            m() += v.m();
+            return *this;
+        }
+
+        /**
+         * @brief Vector subtraction.
+         */
+        Vector& operator-=(const Vector& v)
+        {
+            m() -= v.m();
+            return *this;
+        }
+
+        /**
+         * @brief Unary minus.
+         */
+        const Vector operator-() const
+        {
+            return Vector(-m());
+        }
+
+        /**
+         * @brief Compares whether this is less than \b q
+         *
+         * The less operator is defined such that the first index is the most significant. That is
+         * if (*this)[0] < q[0] then true is returned. If (*this)[0] > q[0] false is returned and
+         * only if (*this)[0] == q[0] is the next index considered.
+         */
+        const bool operator<(const Vector& q) const
+        {
+            RW_ASSERT(size() == q.size());
+            for (size_t i = 0; i<size(); i++) {
+                if (_vec[i] < q[i])
+                    return true;
+                else if (_vec[i] > q[i])
+                    return false;
+            }
+            return false;
+        }
+
+    private:
+        Base _vec;
+    };
+
+
+
+
+
+    /**
+     * @brief Compares \b q1 and \b q2 for equality.
+     *
+     * \b q1 and \b q2 are considered equal if and only if they have equal
+     * length and if q1(i) == q2(i) for all i.
+     *
+     * @relates Vector
+     *
+     * @param q1 [in]
+     * @param q2 [in]
+     * @return True if q1 equals q2, false otherwise.
+     */
+    template<class A>
+    bool operator==(const Vector<A>& q1, const Vector<A>& q2){
+        if (q1.size() != q2.size())
+            return false;
+
+        for (size_t i = 0; i < q1.size(); i++)
+            if (q1(i) != q2(i))
+                return false;
+        return true;
+    }
+
+    /**
+       @brief Inequality operator
+
+       The inverse of operator==().
+     */
+    template<class A>
+    inline bool operator!=(const Vector<A>& q1, const Vector<A>& q2) { return !(q1 == q2); }
+
+    /**
+     * @brief Streaming operator.
+     *
+     * @relates Vector
+     */
+    template<class A>
+    std::ostream& operator<<(std::ostream& out, const Vector<A>& v){
+        if (v.size() == 0)
+            return out << "Q[0]{}";
+        else {
+            out << "Q[" << (int)v.size() << "]{";
+            for (size_t i = 0; i < v.size() - 1; i++)
+                out << v[i] << ", ";
+            return out << v[v.size() - 1] << "}";
+        }
+    }
+
+    /**
+     * @brief Input streaming operator
+     *
+     * Parse input stream according to how operator<< streams out
+     *
+     * @relates Vector
+     * @param in [in] Input stream
+     * @param q [in] Target of q read in
+     * @return reference to \b in
+     */
+    template<class A>
+    std::istream& operator>>(std::istream& in, Vector<A>& q){
+        char ch1, ch2;
+        do {
+            in.get(ch1);
+        } while (ch1 == ' ' || ch1 == '\t'); //Ignore space and tab, but not line changes.
+
+
+        int size = -1;
+
+        if (ch1 == 'Q') {
+            in.get(ch2);
+            if (ch1 != 'Q' || ch2 != '[')
+                RW_THROW("Content of input stream does not match format of Q");
+            in >> size;
+
+            in.get(ch1);
+            in.get(ch2);
+            if (ch1 != ']' || ch2 != '{')
+                RW_THROW("Content of input stream does not match format of Q");
+        } else if (ch1 != '{') {
+            RW_THROW("Content of input stream does not match format of Q");
+        }
+
+        std::vector<double> res;
+        while (ch1 != '}') {
+            double d;
+            in >> d;
+            if (!in.eof()) {
+                res.push_back(d);
+            }
+            in.get(ch1);
+        }
+
+        if (ch1 != '}')
+            RW_THROW("Content of input stream does not match format of Q");
+
+        if (size > -1 && (int)res.size() != size) {
+            RW_THROW("Length of Q does not match device");
+        }
+
+        q = Vector<A>(res.size(), &res[0]);
+        return in;
+    }
+
+    /**
+       @brief The dot product (inner product) of \b a and \b b.
+
+       @relates Vector
+    */
+    template<class A>
+    A dot(const Vector<A>& a, const Vector<A>& b){
+        return inner_prod(a.m(), b.m());
+    }
+
+    /**
+     * @brief concatenates q1 onto q2 such that the returned q has
+     * the configurations of q1 in [0;q1.size()[ and has q2 in
+     * [q1.size();q1.size()+q2.size()[
+     * @param q1 [in] the first Vector
+     * @param q2 [in] the second Vector
+     * @return the concatenation of q1 and q2
+     */
+    template<class A>
+    rw::math::Vector<A> concat(const Vector<A>& q1, const Vector<A>& q2){
+        Vector<A> q(q1.size()+q2.size());
+        for(size_t i=0;i<q1.size();i++)
+            q(i) = q1(i);
+        for(size_t i=0;i<q2.size();i++)
+            q(q1.size()+i) = q2(i);
+        return q;
+    }
+
+    /*@}*/
+
+}} // end namespaces
+
+#endif // end include guard
---
+++ RobWork/src/rw/math/VectorND.hpp
@@ -0,0 +1,395 @@
+/********************************************************************************
+ * Copyright 2009 The Robotics Group, The Maersk Mc-Kinney Moller Institute, 
+ * Faculty of Engineering, University of Southern Denmark 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ********************************************************************************/
+
+
+#ifndef RW_MATH_VectorND_HPP
+#define RW_MATH_VectorND_HPP
+
+/**
+ * @file VectorND.hpp
+ */
+
+#include <boost/numeric/ublas/vector.hpp>
+#include <boost/numeric/ublas/vector_expression.hpp>
+#include <boost/numeric/ublas/io.hpp>
+
+namespace rw { namespace math {
+
+    /** @addtogroup math */
+    /*@{*/
+
+    /**
+     * @brief A ND VectorND
+     *
+     */
+    template<size_t N, class T = double>
+    class VectorND
+    {
+    public:
+        //! The type of the internal Boost VectorND implementation.
+        typedef boost::numeric::ublas::bounded_vector<T, N> BoostBoundedVector;
+		
+
+        //! Value type.
+        typedef T value_type;
+
+        /**
+         * @brief Creates a 3D VectorND initialized with 0's
+         */
+        VectorND()
+        {
+            _vec = boost::numeric::ublas::zero_vector<T>(N);
+        }
+
+        /**
+         * @brief Creates a 3D VectorND from VectorND_expression
+         *
+         * @param r [in] an ublas VectorND_expression
+         */
+        template <class R>
+        explicit VectorND(const boost::numeric::ublas::vector_expression<R>& r)
+        {
+			_vec = r;
+		}
+
+        /**
+           @brief Accessor for the internal Boost VectorND state.
+         */
+        const BoostBoundedVector& m() const {
+			return _vec;
+		}
+
+        /**
+           @brief Accessor for the internal Boost VectorND state.
+         */
+        BoostBoundedVector& m() {
+            return _vec;
+        }
+
+        /**
+           @brief The dimension of the VectorND (i.e. 3).
+
+           This method is provided to help support generic algorithms using
+           size() and operator[].
+        */
+        size_t size() const { return N; }
+
+        //----------------------------------------------------------------------
+        // Various operators
+
+        /**
+         * @brief Returns reference to VectorND element
+         * @param i [in] index in the VectorND \f$i\in \{0,1,2\} \f$
+         * @return const reference to element
+         */
+        const T& operator()(size_t i) const { return _vec[i]; }
+
+        /**
+         * @brief Returns reference to VectorND element
+         * @param i [in] index in the VectorND \f$i\in \{0,1,2\} \f$
+         * @return reference to element
+         */
+        T& operator()(size_t i) { return _vec[i]; }
+
+        /**
+         * @brief Returns reference to VectorND element
+         * @param i [in] index in the VectorND \f$i\in \{0,1,2\} \f$
+         * @return const reference to element
+         */
+        const T& operator[](size_t i) const { return _vec[i]; }
+
+        /**
+         * @brief Returns reference to VectorND element
+         * @param i [in] index in the VectorND \f$i\in \{0,1,2\} \f$
+         * @return reference to element
+         */
+        T& operator[](size_t i) { return _vec[i]; }
+
+
+
+        /**
+           @brief Scalar division.
+         */
+        const VectorND<N,T> operator/( T s) const
+        {
+            return VectorND<N,T>( _vec / s );
+        }
+
+        /**
+           @brief Scalar multiplication.
+         */
+        const VectorND<N,T> operator*( T s) const
+        {
+            return VectorND<N,T>( _vec * s );
+        }
+
+        /**
+           @brief Scalar multiplication.
+         */
+        friend const VectorND<N,T> operator*(T s, const VectorND<N,T>& v)
+        {
+            return VectorND<N,T>( s * v._vec );
+        }
+
+        /**
+           @brief VectorND subtraction.
+         */
+        const VectorND<N,T> operator-( const VectorND<N,T>& b) const
+        {
+            return VectorND<N,T>( _vec - b._vec );
+        }
+
+        /**
+           @brief VectorND addition.
+         */
+        const VectorND<N,T> operator+( const VectorND<N,T>& b) const
+        {
+            return VectorND<N,T>( _vec + b._vec );
+        }
+
+        /**
+           @brief Scalar multiplication.
+         */
+        VectorND<N,T>& operator*=(T s)
+        {
+            _vec *= s;
+            return *this;
+        }
+
+        /**
+           @brief Scalar division.
+         */
+        VectorND<N,T>& operator/=(T s)
+        {
+            _vec /= s;
+            return *this;
+        }
+
+        /**
+           @brief VectorND addition.
+         */
+        VectorND<N,T>& operator+=(const VectorND<N,T>& v)
+        {
+            _vec += v._vec;
+            return *this;
+        }
+
+        /**
+           @brief VectorND subtraction.
+         */
+        VectorND<N,T>& operator-=(const VectorND<N,T>& v)
+        {
+            _vec -= v._vec;
+            return *this;
+        }
+
+        /**
+           @brief Unary minus.
+         */
+        const VectorND<N,T> operator-() const
+        {
+            return VectorND<N,T>(-_vec);
+        }
+
+        /**
+           @brief Streaming operator.
+         */
+        friend std::ostream& operator<<(std::ostream& out, const VectorND<N,T>& v)
+        {
+            out << "VectorND {" << v[0];
+
+            for(size_t i=1;i<N;i++)
+                out << ", " << v[i];
+            out << "}";
+            return out;
+        }
+
+        //----------------------------------------------------------------------
+        // Various friend functions
+
+        /**
+         * @brief Returns the Euclidean norm (2-norm) of the VectorND
+         * @return the norm
+         */
+        T norm2() const {
+            return norm_2(_vec);
+        }
+
+        /**
+         * @brief Returns the Manhatten norm (1-norm) of the VectorND
+         * @return the norm
+         */
+        T norm1() const {
+            return norm_1(_vec);
+        }
+
+        /**
+         * @brief Returns the infinte norm (\f$\inf\f$-norm) of the VectorND
+         * @return the norm
+         */
+        T normInf() const {
+            return norm_inf(_vec);
+        }
+
+        /**
+           @brief Compares \b a and \b b for equality.
+
+           @relates VectorND
+
+           @param a [in]
+           @param b [in]
+           @return True if a equals b, false otherwise.
+        */
+        bool operator==(const VectorND<N,T>& b) const
+        {
+            for(int i=0;i<N;i++)
+                if(_vec[i]!=b._vec[i])
+                    return false;
+            return true;
+        }
+
+        static VectorND<N,T> zero(){ return VectorND<N,T>( boost::numeric::ublas::zero_vector<T>(N) ); }
+    private:
+        BoostBoundedVector _vec;
+    };
+
+    /**
+     * @brief Calculates the 3D VectorND cross product @f$ \mathbf{v1} \times \mathbf{v2} @f$
+     * @param v1 [in] @f$ \mathbf{v1} @f$
+     * @param v2 [in] @f$ \mathbf{v2} @f$
+     *
+     * @return the 3D VectorND cross product @f$ \mathbf{v1} \times \mathbf{v2} @f$
+     *
+     * The 3D VectorND cross product is defined as:
+     * @f$
+     * \mathbf{v1} \times \mathbf{v2} = \left[\begin{array}{c}
+     *  v1_y * v2_z - v1_z * v2_y \\
+     *  v1_z * v2_x - v1_x * v2_z \\
+     *  v1_x * v2_y - v1_y * v2_x
+     * \end{array}\right]
+     * @f$
+     *
+     * @relates VectorND
+     */
+    template <size_t ND, class T>
+    const VectorND<ND,T> cross(const VectorND<ND,T>& v1, const VectorND<ND,T>& v2)
+    {
+        return cross(v1.m(),v2.m());
+    }
+
+    template <size_t ND, class T>
+    void cross(const VectorND<ND,T>& v1, const VectorND<ND,T>& v2, VectorND<ND,T>& dst)
+    {
+        dst = cross(v1.m(),v2.m());
+    }
+
+
+    /**
+     * @brief Calculates the dot product @f$ \mathbf{v1} . \mathbf{v2} @f$
+     * @param v1 [in] @f$ \mathbf{v1} @f$
+     * @param v2 [in] @f$ \mathbf{v2} @f$
+     *
+     * @return the dot product @f$ \mathbf{v1} . \mathbf{v2} @f$
+     *
+     * @relates VectorND
+     */
+    template <size_t ND, class T>
+    T dot(const VectorND<ND,T>& v1, const VectorND<ND,T>& v2)
+    {
+		return v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2];
+        //return inner_prod(v1.m(), v2.m());
+    }
+
+    /**
+     * @brief Returns the normalized VectorND \f$\mathbf{n}=\frac{\mathbf{v}}{\|\mathbf{v}\|} \f$.
+     * In case \f$ \|mathbf{v}\| = 0\f$ the zero VectorND is returned.
+     * @param v [in] \f$ \mathbf{v} \f$ which should be normalized
+     * @return the normalized VectorND \f$ \mathbf{n} \f$
+     *
+     * @relates VectorND
+     */
+    template <size_t ND, class T>
+    const VectorND<ND,T> normalize(const VectorND<ND,T>& v)
+    {
+        T length = v.norm2();
+        if (length != 0)
+            return VectorND<ND,T>(v(0)/length, v(1)/length, v(2)/length);
+        else
+            return VectorND<ND,T>(0,0,0);
+    }
+
+    /**
+     * @brief Calculates the angle from @f$ \mathbf{v1}@f$ to @f$ \mathbf{v2} @f$
+     * around the axis defined by @f$ \mathbf{v1} \times \mathbf{v2} @f$ with n
+     * determining the sign.
+     * @param v1 [in] @f$ \mathbf{v1} @f$
+     * @param v2 [in] @f$ \mathbf{v2} @f$
+     * @param n [in] @f$ \mathbf{n} @f$
+     *
+     * @return the angle
+     *
+     * @relates VectorND
+     */
+    template <size_t ND, class T>
+    double angle(const VectorND<ND, T>& v1, const VectorND<ND, T>& v2, const VectorND<ND, T>& n)
+    {
+        const VectorND<ND, T> nv1 = normalize(v1);
+        const VectorND<ND, T> nv2 = normalize(v2);
+        const VectorND<ND, T> nn = normalize(n);
+        return atan2(dot(nn, cross(nv1, nv2)), dot(nv1, nv2));
+    }
+
+    /**
+     * @brief Calculates the angle from @f$ \mathbf{v1}@f$ to @f$ \mathbf{v2} @f$
+     * around the axis defined by @f$ \mathbf{v1} \times \mathbf{v2} @f$
+     * @param v1 [in] @f$ \mathbf{v1} @f$
+     * @param v2 [in] @f$ \mathbf{v2} @f$
+     *
+     * @return the angle
+     *
+     * @relates VectorND
+     */
+    template <size_t ND, class T>
+    double angle(const VectorND<ND, T>& v1, const VectorND<ND, T>& v2)
+    {
+        VectorND<ND,T> n = cross(v1, v2);
+        return angle(v1,v2,n);
+    }
+
+    /**
+     * @brief Casts VectorND<N,T> to VectorND<Q>
+     * @param v [in] VectorND with type T
+     * @return VectorND with type Q
+     *
+     * @relates VectorND
+     */
+    template<size_t ND, class Q, class T>
+    const VectorND<ND, Q> cast(const VectorND<ND,T>& v)
+    {
+
+
+        return VectorND<ND, Q>(
+            static_cast<Q>(v(0)),
+            static_cast<Q>(v(1)),
+            static_cast<Q>(v(2)));
+    }
+
+
+    /**@}*/
+}} // end namespaces
+
+#endif // end include guard
