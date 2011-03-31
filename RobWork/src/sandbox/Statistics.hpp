caf3b5dd3b92769c1f1a9353c0cbae9ba3c78aa7
Lars-Peter Ellekilde <lpe@mmmi.sdu.dk>
2011-03-31 07:27:20 +0000

    Implemented a bit in the Statistics class in sandbox
    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@2866 494c09c7-a67d-49d4-9938-151fb1965be8

--- RobWork/src/sandbox/Statistics.hpp
+++ RobWork/src/sandbox/Statistics.hpp
@@ -1,18 +1,55 @@
 
-#ifndef RW_PATH_STATISTICS_HPP
-#define RW_PATH_STATISTICS_HPP
+#ifndef RW_MATH_STATISTICS_HPP
+#define RW_MATH_STATISTICS_HPP
+
+#include <vector>
 
 namespace rw {
 namespace math {
 
+template <class T>
 class Statistics {
 public:
-	/*std::pair<double, double> normalDistribution(std::vector<double>& data);
+	static std::pair<T, T> normalDistribution(std::vector<T>& data) {
+		T sum = 0;
+		BOOST_FOREACH(T d, data) {
+			sum += d;
+		}
+		T average = sum / data.length;
 
+		T var = 0;
+		BOOST_FOREACH(T d, data) {
+			err2 += Math::sqr(d-average);
+		}
+		return std::make_pair(average, err2/(data.size()-1));	
+	}
+
+	/*
 	std::pair<Q, Q> normalDistrubitionIndependent(std::vector<Q>& data);
 	
 */
+T mean() const {
+	T sum = 0;
+	BOOST_FOREACH(T d, _data) {
+		sum += d;
+	}
+	return sum / data.length;	
+}
 
+
+T variance() const {
+	T mean = mean();	
+
+	T var = 0;
+	BOOST_FOREACH(T d, _data) {
+		var += Math::sqr(d-mean);
+	}
+	return var /(data.size()-1);	
+
+}
+
+private:
+	std::vector<T> _data;
 };
 
 } } //end namespaces
