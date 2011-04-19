3ba029816cf51186147e1a95123fe23308dc5f1a
Lars-Peter Ellekilde <lpe@mmmi.sdu.dk>
2011-04-19 13:36:03 +0000

    Updated how collision checking maintains a list of frame pairs to test. Introduced rulebased ProximitySetup as replacement for CollisionSetup.
    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@2894 494c09c7-a67d-49d4-9938-151fb1965be8

---
+++ RobWork/src/rw/loaders/xml/XMLProximitySetupLoader.cpp
@@ -0,0 +1,137 @@
+/********************************************************************************
+ * Copyright 2009 The Robotics Group, The Maersk Mc-Kinney Moller Institute,
+ * Faculty of Engineering, University of Southern Denmark
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ********************************************************************************/
+
+#include "XMLProximitySetupLoader.hpp"
+
+#include <iostream>
+
+#include <xercesc/dom/DOM.hpp>
+#include <xercesc/dom/DOMDocument.hpp>
+#include <xercesc/dom/DOMDocumentType.hpp>
+#include <xercesc/dom/DOMElement.hpp>
+#include <xercesc/dom/DOMImplementation.hpp>
+#include <xercesc/dom/DOMImplementationLS.hpp>
+#include <xercesc/dom/DOMNodeIterator.hpp>
+#include <xercesc/dom/DOMNodeList.hpp>
+#include <xercesc/dom/DOMText.hpp>
+
+#include <xercesc/parsers/XercesDOMParser.hpp>
+#include <xercesc/util/XMLUni.hpp>
+#include <xercesc/util/XMLDouble.hpp>
+
+#include <xercesc/validators/common/Grammar.hpp>
+#include <xercesc/sax/ErrorHandler.hpp>
+#include <xercesc/sax/SAXParseException.hpp>
+#include <xercesc/sax/SAXException.hpp>
+
+
+#include "XercesErrorHandler.hpp"
+#include "XMLBasisTypes.hpp"
+#include "XMLPathFormat.hpp"
+
+using namespace xercesc;
+using namespace rw::common;
+using namespace rw::loaders;
+using namespace rw::proximity;
+
+
+//Small trick to make sure Xerces is initialized before we start using XMLString::transcode
+const XercesInitializer XMLProximitySetupFormat::initializer;
+
+const XMLCh* XMLProximitySetupFormat::ProximitySetupId = XMLString::transcode("ProximitySetup");
+const XMLCh* XMLProximitySetupFormat::ExcludeRuleId = XMLString::transcode("Exclude");
+const XMLCh* XMLProximitySetupFormat::IncludeRuleId = XMLString::transcode("Include");
+const XMLCh* XMLProximitySetupFormat::PatternAAttributeId = XMLString::transcode("PatternA");
+const XMLCh* XMLProximitySetupFormat::PatternBAttributeId = XMLString::transcode("PatternB");
+
+const XMLCh* XMLProximitySetupFormat::IncludeAllAttributeId = XMLString::transcode("UseIncludeAll");
+const XMLCh* XMLProximitySetupFormat::ExcludeStaticAttributePairsId = XMLString::transcode("UseExcludeStaticPairs");
+
+
+
+ProximitySetup XMLProximitySetupLoader::load(const std::string& filename, const std::string& schemaFileName)
+{
+    XercesDOMParser parser;
+    xercesc::DOMDocument* doc = XercesDocumentReader::readDocument(parser, filename, schemaFileName);
+    xercesc::DOMElement* elementRoot = doc->getDocumentElement();
+    return readProximitySetup(elementRoot);
+}
+
+
+ProximitySetup XMLProximitySetupLoader::load(std::istream& instream, const std::string& schemaFileName) {    
+    XercesDOMParser parser;
+    xercesc::DOMDocument* doc = XercesDocumentReader::readDocument(parser, instream, schemaFileName);
+    xercesc::DOMElement* elementRoot = doc->getDocumentElement();
+    return readProximitySetup(elementRoot);
+}
+
+
+ProximitySetup XMLProximitySetupLoader::readProximitySetup(xercesc::DOMElement* element) {
+    if (XMLString::equals(XMLProximitySetupFormat::ProximitySetupId, element->getNodeName())) {
+        ProximitySetup setup;
+
+		if (element->hasAttributes()) {
+			if (element->hasAttribute(XMLProximitySetupFormat::IncludeAllAttributeId)) {
+		        const XMLCh* attr = element->getAttribute(XMLProximitySetupFormat::IncludeAllAttributeId);				
+				if (XMLStr(attr).str() == "true") {
+					setup.setUseIncludeAll(true);
+				} else if (XMLStr(attr).str() == "false"){
+					setup.setUseIncludeAll(false);
+				} else {
+					RW_THROW("Unknown value for "<<XMLStr(XMLProximitySetupFormat::IncludeAllAttributeId).str()<<" attribute. Please specify \"true\" or \"false\"");
+				}
+			} else if (element->hasAttribute(XMLProximitySetupFormat::ExcludeStaticAttributePairsId)) {
+		        const XMLCh* attr = element->getAttribute(XMLProximitySetupFormat::ExcludeStaticAttributePairsId);
+				if (XMLStr(attr).str() == "true") {
+					setup.setUseExcludeStaticPairs(true);
+				} else if (XMLStr(attr).str() == "false"){
+					setup.setUseExcludeStaticPairs(false);
+				} else {
+					RW_THROW("Unknown value for "<<XMLStr(XMLProximitySetupFormat::ExcludeStaticAttributePairsId).str()<<" attribute. Please specify \"true\" or \"false\"");
+				}
+			} else {
+				RW_THROW("Unknown attribute on "<<XMLStr(XMLProximitySetupFormat::ProximitySetupId).str());
+			}
+        } 
+
+        DOMNodeList* children = element->getChildNodes();
+        const  XMLSize_t nodeCount = children->getLength();
+        for(XMLSize_t i = 0; i < nodeCount; ++i ) {
+            xercesc::DOMElement* child = dynamic_cast<xercesc::DOMElement*>(children->item(i));
+            if (child != NULL) {
+				if (XMLString::equals(child->getNodeName(), XMLProximitySetupFormat::ExcludeRuleId)) {		
+					setup.addProximitySetupRule(ProximitySetupRule::makeExclude(readFramePatternAttributes(child)));					
+				} else if (XMLString::equals(child->getNodeName(), XMLProximitySetupFormat::IncludeRuleId)) {
+					setup.addProximitySetupRule(ProximitySetupRule::makeInclude(readFramePatternAttributes(child)));					
+				}
+			}
+		}
+		return setup;
+	}
+	RW_THROW("Failed to parse element \""<<XMLStr(element->getNodeName()).str()<<" as ProximitySetup");
+}
+
+std::pair<std::string, std::string> XMLProximitySetupLoader::readFramePatternAttributes(xercesc::DOMElement* element) {
+	if (element->hasAttribute(XMLProximitySetupFormat::PatternAAttributeId) &&
+		element->hasAttribute(XMLProximitySetupFormat::PatternBAttributeId)) 
+	{
+		const XMLCh* attr1 = element->getAttribute(XMLProximitySetupFormat::PatternAAttributeId);
+		const XMLCh* attr2 = element->getAttribute(XMLProximitySetupFormat::PatternBAttributeId);
+		return std::make_pair(XMLStr(attr1).str(), XMLStr(attr2).str());
+	}
+	RW_THROW("Element does not have the expected attributes");
+}
---
+++ RobWork/src/rw/loaders/xml/XMLProximitySetupLoader.hpp
@@ -0,0 +1,121 @@
+/********************************************************************************
+ * Copyright 2009 The Robotics Group, The Maersk Mc-Kinney Moller Institute,
+ * Faculty of Engineering, University of Southern Denmark
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ********************************************************************************/
+
+#ifndef RW_LOADERS_XMLPROXIMITYSETUPLOADER_HPP
+#define RW_LOADERS_XMLPROXIMITYSETUPLOADER_HPP
+
+
+#include <rw/proximity/ProximitySetup.hpp>
+
+#include <rw/loaders/xml/XercesUtils.hpp>
+#include <xercesc/dom/DOMElement.hpp>
+#include <string>
+
+namespace rw {
+namespace loaders {
+
+/** @addtogroup loaders */
+/*@{*/
+
+/**
+ * @brief Format specification for the XML PropertySetup format 
+ */
+class XMLProximitySetupFormat {
+	private:
+		//Needed to make sure Xerces is initialized
+		static const XercesInitializer initializer;
+	public:
+
+    /** @brief Identifier for rw::proximity::ProximitySetup in the XML format  */
+    static const XMLCh* ProximitySetupId;
+
+    /** @brief Identifier for exclude rule in the XML format  */
+    static const XMLCh* ExcludeRuleId;
+
+    /** @brief Identifier for include rule in the XML format  */
+    static const XMLCh* IncludeRuleId;
+
+	/** @brief Identifier for the attribute specifying whether to include all frame pairs */
+	static const XMLCh* IncludeAllAttributeId;
+
+	/** @brief Identifier for the attribute specifying whether to exclude frame pairs with static transformation*/
+	static const XMLCh* ExcludeStaticAttributePairsId;
+
+	/** @brief Identifier for the attributed specifying the first pattern in rule */
+	static const XMLCh* PatternAAttributeId;
+
+	/** @brief Identifier for the attributed specifying the second pattern in rule */
+	static const XMLCh* PatternBAttributeId;
+
+private:
+    XMLProximitySetupFormat() {};
+};
+
+
+/**
+ * @brief Loader for the XML PropertySetup format
+ * 
+ * The loader is based on Xerces
+ */
+class XMLProximitySetupLoader
+{
+public:
+	/**
+	 * @brief Loads ProximitySetup from \b filename
+	 *
+	 * Throws rw::common::Exception on errors
+	 *
+	 * @param filename [in] Name of input file
+	 * @param schemaFileName [in] Optional name of schema file to be used for verification
+	 * @return The ProximitySetup
+	 */
+	static rw::proximity::ProximitySetup load(const std::string& filename, const std::string& schemaFileName = "");
+
+	/**
+	 * @brief Loads ProximitySetup from \b instream
+	 *
+	 * Throws rw::common::Exception on errors
+	 *
+	 * @param instream [in] Stream containing XML ProximitySetup
+	 * @param schemaFileName [in] Optional name of schema file to be used for verification
+	 * @return The ProximitySetup
+	 */
+	static rw::proximity::ProximitySetup load(std::istream& instream, const std::string& schemaFileName = "");
+
+	/**
+	 * @brief Reads ProximitySetup from \b element
+	 *
+	 * Throws rw::common::Exception on errors
+	 *
+	 * @param element [in] Element containing ProximitySetup
+	 * @return The ProximitySetup
+	 */
+	static rw::proximity::ProximitySetup readProximitySetup(xercesc::DOMElement* element);
+
+
+private:
+	static std::pair<std::string, std::string> readFramePatternAttributes(xercesc::DOMElement* element);
+
+};
+
+/** @} */
+
+
+} //end namespace loaders
+} //end namespace rw
+
+#endif //enc include guard
