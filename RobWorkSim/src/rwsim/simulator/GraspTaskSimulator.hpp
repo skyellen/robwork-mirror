7344156744cdcdc6507d336d53742175d643edd1
Jimmy Alison Jørgensen <jimali@mmmi.sdu.dk>
2011-06-28 14:55:47 +0000

    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@3122 494c09c7-a67d-49d4-9938-151fb1965be8

---
+++ RobWorkSim/src/rwsim/simulator/GraspTaskSimulator.hpp
@@ -0,0 +1,12 @@
+/*
+ * GraspTaskSimulator.hpp
+ *
+ *  Created on: Jun 28, 2011
+ *      Author: jimali
+ */
+
+#ifndef GRASPTASKSIMULATOR_HPP_
+#define GRASPTASKSIMULATOR_HPP_
+
+
+#endif /* GRASPTASKSIMULATOR_HPP_ */
