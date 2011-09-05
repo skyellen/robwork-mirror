1e498edf8b726e4225ce511546dd0310f27ab9cf
Jimmy Alison Jørgensen <jimali@mmmi.sdu.dk>
2011-09-05 08:24:40 +0000

    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@3300 494c09c7-a67d-49d4-9938-151fb1965be8

---
+++ RobWorkSim/src/rwsim/loaders/GraspTask.hpp
@@ -0,0 +1,35 @@
+/*
+ * GraspTask.hpp
+ *
+ *  Created on: Aug 15, 2011
+ *      Author: jimali
+ */
+
+#ifndef GRASPTASK_HPP_
+#define GRASPTASK_HPP_
+
+/**
+ * @brief a container for describing one or multiple grasping tasks. It is based on the rwlibs::tasks library
+ *
+ *
+ * Definition of GraspTask xml format
+ *
+ * GraspTask<br>
+ *  - p:string:"GripperName" - name of the
+ *  - p:string:"ControllerName" - defaults to GraspController
+ *  - p:string:"TCP" - name of the TCP frame
+ *
+ *
+ *
+ *
+ */
+
+class GraspTask {
+public:
+
+
+    //static void save(GraspTask::Ptr task, const std::string& name );
+
+};
+
+#endif /* GRASPTASK_HPP_ */
