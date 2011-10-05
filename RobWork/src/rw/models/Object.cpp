12d72c545e55e3533159985de7416aa21a2a9f9a
Jimmy Alison Jørgensen <jimali@mmmi.sdu.dk>
2011-10-05 08:35:19 +0000

    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@3406 494c09c7-a67d-49d4-9938-151fb1965be8

---
+++ RobWork/src/rw/models/Object.cpp
@@ -0,0 +1,82 @@
+#include "Object.hpp"
+
+
+using namespace rw::kinematics;
+using namespace rw::geometry;
+using namespace rw::models;
+
+Object::Object(rw::kinematics::Frame* baseframe):
+        _base(baseframe),
+        _frames(std::vector<Frame*>(1,baseframe))
+{
+
+}
+
+Object::Object(rw::kinematics::Frame* baseframe, Geometry::Ptr geom):
+        _base(baseframe),
+        _frames(std::vector<Frame*>(1,baseframe)),
+        _geometry(std::vector<Geometry::Ptr>(1,geom))
+{
+
+}
+
+Object::Object(rw::kinematics::Frame* baseframe, std::vector<Geometry::Ptr> geoms):
+        _base(baseframe),
+        _frames(std::vector<Frame*>(1,baseframe)),
+        _geometry(geoms)
+{
+
+}
+
+Object::Object(std::vector<rw::kinematics::Frame*> frames):
+        _base(frames[0]),
+        _frames(frames)
+{
+
+}
+
+Object::Object(std::vector<rw::kinematics::Frame*> frames, Geometry::Ptr geom):
+        _base(frames[0]),
+        _frames(frames),
+        _geometry(std::vector<Geometry::Ptr>(1,geom))
+{
+
+}
+
+Object::Object(std::vector<rw::kinematics::Frame*> frames, std::vector<Geometry::Ptr> geom):
+        _base(frames[0]),
+        _frames(frames),
+        _geometry(geom)
+{
+
+}
+
+Object::~Object(){
+
+}
+
+void Object::addGeometry(Geometry::Ptr geom){
+    _geometry.push_back(geom);
+}
+
+void Object::addFrame(rw::kinematics::Frame* frame){
+    _frames.push_back(frame);
+}
+
+void Object::removeGeometry(Geometry::Ptr geom){
+    std::vector<Geometry::Ptr>::iterator iter = std::find(_geometry.begin(),_geometry.end(),geom);
+    if(iter!=_geometry.end())
+        _geometry.erase(iter);
+}
+const std::vector<Geometry::Ptr>& Object::getGeometry(){
+    return _geometry;
+}
+rw::kinematics::Frame* Object::getBase(){
+    return _base;
+}
+const rw::kinematics::Frame* Object::getBase() const{
+    return _base;
+}
+const std::vector<rw::kinematics::Frame*>& Object::getFrames(){
+    return _frames;
+}
---
+++ RobWork/src/rw/models/Object.hpp
@@ -0,0 +1,84 @@
+/********************************************************************************
+ * Copyright 2009 The Robotics Group, The Maersk Mc-Kinney Moller Institute,
+ * Faculty of Engineering, University of Southern Denmark
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ********************************************************************************/
+
+#ifndef RW_MODELS_OBJECT_HPP_
+#define RW_MODELS_OBJECT_HPP_
+
+#include <rw/common/Ptr.hpp>
+#include <rw/kinematics/Frame.hpp>
+#include <rw/kinematics/State.hpp>
+#include <rw/kinematics/StateStructure.hpp>
+#include <rw/geometry/Geometry.hpp>
+#include <vector>
+
+namespace rw { namespace models {
+
+    /** @addtogroup models */
+    /*@{*/
+
+    /**
+       @brief An object is an element in the scene with a geometric representation. The Object
+       has a base frame and may have additional frames attached.
+    */
+    class Object
+    {
+    public:
+        typedef rw::common::Ptr<Object> Ptr;
+
+        Object(rw::kinematics::Frame* baseframe);
+        Object(rw::kinematics::Frame* baseframe, rw::geometry::Geometry::Ptr geom);
+        Object(rw::kinematics::Frame* baseframe, std::vector<rw::geometry::Geometry::Ptr> geom);
+
+        Object(std::vector<rw::kinematics::Frame*> frames);
+        Object(std::vector<rw::kinematics::Frame*> frames, rw::geometry::Geometry::Ptr geom);
+        Object(std::vector<rw::kinematics::Frame*> frames, std::vector<rw::geometry::Geometry::Ptr> geom);
+
+        virtual ~Object();
+
+        void addGeometry(rw::geometry::Geometry::Ptr geom);
+
+        void addFrame(rw::kinematics::Frame* frame);
+
+        void removeGeometry(rw::geometry::Geometry::Ptr geom);
+
+        const std::vector<rw::geometry::Geometry::Ptr>& getGeometry();
+
+        rw::kinematics::Frame* getBase();
+
+        const rw::kinematics::Frame* getBase() const;
+
+        const std::vector<rw::kinematics::Frame*>& getFrames();
+
+        const std::string& getName(){ return _base->getName(); };
+
+    protected:
+        friend class WorkCell;
+        virtual void registerStateData(rw::kinematics::StateStructure::Ptr sstruct){};
+        virtual void removeStateData(rw::kinematics::StateStructure::Ptr sstruct){};
+
+    private:
+        rw::kinematics::Frame *_base;
+        std::vector<rw::kinematics::Frame*> _frames;
+        std::vector<rw::geometry::Geometry::Ptr> _geometry;
+
+
+    };
+
+    /*@}*/
+}}
+
+#endif /* OBJECT_HPP_ */
