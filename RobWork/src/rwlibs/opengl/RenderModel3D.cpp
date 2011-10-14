12e74d4d9f436c97e517c53bf63d3e638c41b595
Jimmy Alison Jørgensen <jimali@mmmi.sdu.dk>
2011-10-14 12:37:08 +0000

    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@3470 494c09c7-a67d-49d4-9938-151fb1965be8

--- RobWork/src/rwlibs/opengl/RenderModel3D.cpp
+++ RobWork/src/rwlibs/opengl/RenderModel3D.cpp
@@ -39,9 +39,9 @@ void RenderModel3D::draw(const DrawableNode::RenderInfo& info, DrawableNode::Dra
     case DrawableNode::SOLID:
     	glPolygonMode(GL_FRONT, GL_FILL);
     	//if(_model->_textures.size()>0)
-    	    drawUsingSimple(info, type, alpha);
+    	//    drawUsingSimple(info, type, alpha);
     	//else
-        //    drawUsingArrays(info,type, alpha);
+            drawUsingArrays(info,type, alpha);
     	break;
     case DrawableNode::OUTLINE: // Draw nice frame
     	glPolygonMode(GL_FRONT, GL_FILL);
