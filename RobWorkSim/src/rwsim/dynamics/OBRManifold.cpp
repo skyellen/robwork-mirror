633322e4914bfe45ba2ca868f21f98c5d2a4d907
Jimmy Alison Jørgensen <jimali@mmmi.sdu.dk>
2011-12-08 16:36:44 +0000

    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@3672 494c09c7-a67d-49d4-9938-151fb1965be8

--- RobWorkSim/src/rwsim/dynamics/OBRManifold.cpp
+++ RobWorkSim/src/rwsim/dynamics/OBRManifold.cpp
@@ -1,2 +1,192 @@
-
 #include "OBRManifold.hpp"
+
+using namespace boost::numeric;
+using namespace rw::math;
+using namespace rwsim::dynamics;
+
+
+bool OBRManifold::addPoint(ContactPoint& p){
+    //if( !inManifold(p) )
+    //    RW_THROW("Not in manifold");
+
+    // handle if we have less than 5 points
+    if(_nrOfContacts==5){
+        // if the point is inside the obb then we only need to check if its
+        // deeper than the deepest point and if it s replace it
+        if( isInsideOBB(p.p) ){
+            if(_points[_deepestIdx].penetration<p.penetration)
+                _points[_deepestIdx] = p;
+            return true;
+        }
+        // if its outside then we need to remove a point
+        fit(p);
+        // we remove the least penetrating point that are not on the border of the bounding box
+        double minPenetration = 1000;
+        int minPenIdx = -1;
+        for(int i=0;i<5;i++){
+            if(_onBorderMap[i])
+                continue;
+            if(minPenetration>_points[i].penetration){
+                minPenIdx = i;
+                minPenetration = _points[i].penetration;
+            }
+        }
+        if(minPenIdx==-1)
+            return true; // something went wrong, we keep the new manifold but the old points
+        _points[minPenIdx] = p;
+        if( _points[_deepestIdx].penetration<p.penetration )
+            _deepestIdx = minPenIdx;
+        return true;
+    } else if(_nrOfContacts>1) {
+        // only add a point if its on the contact manifold plane
+        //double dist = dot(p.p,_normal)+dot(_normal, _points[0].p);
+        double dist = dot(p.n,_normal);
+        if(acos(dist)>_threshold){
+            std::cout << acos(dist)*Rad2Deg << std::endl;
+            return false;
+        }
+
+        // if the contact point is too close to any of the current contacts then
+        // discard it
+        if(MetricUtil::dist2(p.p,_points[0].p)<0.000001)
+            return true;
+        if(MetricUtil::dist2(p.p,_points[1].p)<0.000001)
+            return true;
+
+        Vector3D<> nnorm = p.n;
+        for(size_t i=0;i<_nrOfContacts;i++){ nnorm += _points[i].n; }
+        nnorm /= _nrOfContacts;
+        //_t3d.R() = _t3d.R()*EAA<>(_normal,nnorm).toRotation3D();
+        _normal = nnorm;
+        Vector3D<> yaxis = normalize(_points[1].p - _points[0].p);
+        _t3d.R() = Rotation3D<>(cross(yaxis,_normal),yaxis,_normal);
+
+        fit(p);
+
+        _points[_nrOfContacts] = p;
+        if( _points[_deepestIdx].penetration<p.penetration )
+            _deepestIdx = _nrOfContacts;
+        _nrOfContacts++;
+
+    } else if(_nrOfContacts==1) {
+        // only add a point if its on the contact manifold plane
+        // here we only check if the angle between the contact normals is not too large
+        //double dist = dot(p.p,_normal)+dot(_normal, _points[0].p);
+        double dist = dot(p.n,_normal);
+        if( acos(dist)>_threshold ){
+            RW_WARN(acos(dist) << ">" << _threshold);
+            return false;
+        }
+        _normal = (p.n + _points[0].n)/2;
+        _points[_nrOfContacts] = p;
+        _t3d.P() = (p.p + _points[0].p)/2;
+        if( _points[_deepestIdx].penetration<p.penetration )
+            _deepestIdx = _nrOfContacts;
+        _nrOfContacts++;
+        // create a Rotation with _normal as z-axis and vector between points as y-axis
+        Vector3D<> yaxis = normalize(p.p - _points[0].p);
+        _t3d.R() = Rotation3D<>(cross(yaxis,_normal),yaxis,_normal);
+    } else {
+        _normal = p.n;
+        _points[0] = p;
+        _t3d.P() = p.p;
+        _deepestIdx = 0;
+        _nrOfContacts++;
+    }
+    return true;
+}
+
+
+
+void OBRManifold::fit(ContactPoint& p){
+    // re-fit the bounding box
+    ublas::matrix<double> covar( ublas::zero_matrix<double>(2, 2) );
+
+    size_t nrOfContacts = _nrOfContacts-1;
+
+    for(size_t i=0;i<nrOfContacts;i++){
+        Vector3D<> proj_p = inverse( _t3d.R() )* _points[i].p;
+        _projPoints[i](0) = proj_p(0);
+        _projPoints[i](1) = proj_p(1);
+    }
+
+    Vector3D<> proj_p = inverse( _t3d.R() ) * p.p;
+    _projPoints[nrOfContacts](0) = proj_p(0);
+    _projPoints[nrOfContacts](1) = proj_p(1);
+
+    Vector2D<> c(0,0);
+    for(size_t i=0;i<nrOfContacts+1;i++){
+        c += _projPoints[i];
+        covar(0,0) += _projPoints[i](0)*_projPoints[i](0);
+        covar(1,1) += _projPoints[i](1)*_projPoints[i](1);
+        covar(0,1) += _projPoints[i](0)*_projPoints[i](1);
+    }
+
+    const int n = nrOfContacts+1;
+    covar(0,0) = covar(0,0)-c(0)*c(0)/n;
+    covar(1,1) = covar(1,1)-c(1)*c(1)/n;
+    covar(0,1) = covar(0,1)-c(0)*c(1)/n;
+    covar(1,0) = covar(0,1);
+
+
+    typedef std::pair<ublas::matrix<double>,ublas::vector<double> > ResultType;
+    //std::cout << "COVAR: " << covar << std::endl;
+    ResultType res = LinearAlgebra::eigenDecompositionSymmetric( covar );
+
+    // 4.1 create the rotationmatrix from the normalized eigenvectors
+    // find max and the second maximal eigenvalue
+    size_t maxEigIdx=1, minEigIdx=0;
+    double maxEigVal = res.second(maxEigIdx);
+    double minEigVal = res.second(minEigIdx);
+    if( maxEigVal < minEigVal ){
+        std::swap(minEigVal,maxEigVal);
+        std::swap(minEigIdx,maxEigIdx);
+    }
+    // specify x and y axis, x will be the axis with largest spred
+    Vector2D<> maxAxis( res.first(0,maxEigIdx), res.first(1,maxEigIdx));
+    Vector2D<> minAxis( res.first(0,minEigIdx), res.first(1,minEigIdx));
+    //std::cout << maxAxis << minAxis << std::endl;
+    const Rotation2D<> rot(normalize(maxAxis),normalize(minAxis));
+    const Rotation2D<> rotInv = inverse( rot );
+
+    Vector2D<> max = rotInv * _projPoints[0];
+    Vector2D<> min = max;
+    int x_max = 0, x_min = 0;
+    int y_max = 0, y_min = 0;
+
+    for(int i = 1; i<nrOfContacts+1; i++ ){
+        const Vector2D<> prot = rotInv * _projPoints[i];
+        if( prot(0)>max(0) ) {
+            max(0) = prot(0); x_max = i;
+        } else if( prot(0)<min(0) ) {
+            min(0) = prot(0); x_min = i;
+        }
+        if( prot(1)>max(1) ) {
+            max(1) = prot(1); y_max = i;
+        } else if( prot(1)<min(1) ) {
+            min(1) = prot(1); y_min = i;
+        }
+    }
+    for(int i=0;i<4;i++)
+        _onBorderMap[i] = false;
+    _onBorderMap[x_min] = true;
+    _onBorderMap[x_max] = true;
+    _onBorderMap[y_min] = true;
+    _onBorderMap[y_max] = true;
+
+    // compute halflength of box and its midpoint
+    Vector2D<> center = rot*( 0.5*(max+min));
+    Rotation2D<> rotation = rot;
+    // transform it back to 3d
+    _t3d.P()(0) = center(0);
+    _t3d.P()(1) = center(1);
+    Vector2D<> hspace = 0.5*(max-min);
+    _h(0) = hspace(0);
+    _h(1) = hspace(1);
+    //_normal = p.n;
+    //for(int i=0; i<_nrOfContacts;i++){
+    //    _normal += _points[0].n;
+    //}
+    //_normal /= _nrOfContacts+1;
+
+}
--- RobWorkSim/src/rwsim/dynamics/OBRManifold.hpp
+++ RobWorkSim/src/rwsim/dynamics/OBRManifold.hpp
@@ -12,12 +12,14 @@
 #include <rw/math/MetricUtil.hpp>
 #include <rw/math/Transform3D.hpp>
 #include <rw/math/Vector3D.hpp>
+#include <rw/math/Vector2D.hpp>
+#include <rw/math/Rotation2D.hpp>
 
 namespace rwsim {
 namespace dynamics {
 
     /**
-     * @brief Contact manifold based on Oriented Bounding Boxes
+     * @brief Contact manifold based on Oriented Bounding Rectangle, so in 2D.
      */
     class OBRManifold {
     public:
@@ -37,88 +39,14 @@ namespace dynamics {
 
         virtual ~OBRManifold(){};
 
-
-
         /**
          * @brief adds and updates the manifold with a new point
          * @param p
          */
-        bool addPoint(ContactPoint& p){
-            using namespace rw::math;
-            //if( !inManifold(p) )
-            //    RW_THROW("Not in manifold");
+        bool addPoint(ContactPoint& p);
 
-            // handle if we have less than 5 points
-            if(_nrOfContacts==5){
-                // if the point is inside the obb then we only need to check if its
-                // deeper than the deepest point and if it s replace it
-                if( isInsideOBB(p.p) ){
-                    if(_points[_deepestIdx].penetration<p.penetration)
-                        _points[_deepestIdx] = p;
-                    return true;
-                }
-                // if its outside then we need to remove a point
-                fit(p);
-                // we remove the least penetrating point that are not on the border of the bounding box
-                double minPenetration = 1000;
-                int minPenIdx = -1;
-                for(int i=0;i<5;i++){
-                    if(_onBorderMap[i])
-                        continue;
-                    if(minPenetration>_points[i].penetration){
-                        minPenIdx = i;
-                        minPenetration = _points[i].penetration;
-                    }
-                }
-                if(minPenIdx==-1)
-                    return true; // something went wrong, we keep the new manifold but the old points
-                _points[minPenIdx] = p;
-                if( _points[_deepestIdx].penetration<p.penetration )
-                    _deepestIdx = minPenIdx;
-                return true;
-            } else if(_nrOfContacts>1) {
-                // only add a point if its on the contact manifold plane
-                //double dist = dot(p.p,_normal)+dot(_normal, _points[0].p);
-                double dist = fabs( dot(p.n,_normal) );
-                if(acos(dist)>_threshold)
-                    return false;
-                // now also test if the point is close enough to the contacting plane
-                //double d = -dot(_normal, _points[0].p);
-                //dist = fabs( dot(_normal,p.p)+d );
-                //if(dist>_sepThreshold)
-                //    return false;
-
-                // with at least 3 points we can create a manifold plane
-                fit(p);
-                _points[_nrOfContacts] = p;
-                if( _points[_deepestIdx].penetration<p.penetration )
-                    _deepestIdx = _nrOfContacts;
-                _nrOfContacts++;
-            } else if(_nrOfContacts==1) {
-                // only add a point if its on the contact manifold plane
-                // here we only check if the angle between the contact normals is not too large
-                //double dist = dot(p.p,_normal)+dot(_normal, _points[0].p);
-                double dist = dot(p.n,_normal);
-
-                if( acos(dist)>_threshold ){
-                    std::cout << acos(dist) << ">" << _threshold << std::endl;
-                    return false;
-                }
-
-                _normal = (p.n + _points[0].n)/2;
-                _points[_nrOfContacts] = p;
-                _t3d.P() = (p.p + _points[0].n)/2;
-                if( _points[_deepestIdx].penetration<p.penetration )
-                    _deepestIdx = _nrOfContacts;
-                _nrOfContacts++;
-            } else {
-                _normal = p.n;
-                _points[0] = p;
-                _t3d.P() = p.p;
-                _deepestIdx = 0;
-                _nrOfContacts++;
-            }
-            return true;
+        ContactPoint& getDeepestPoint(){
+            return _points[_deepestIdx];
         }
 
         void update(const rw::math::Transform3D<> &aT, const rw::math::Transform3D<> &bT){
@@ -179,124 +107,7 @@ namespace dynamics {
          * @brief fits a new manifold to the list of contact points
          * @param p
          */
-        void fit(ContactPoint& p){
-            using namespace boost::numeric;
-            using namespace rw::math;
-            // re-fit the bounding box
-            ublas::matrix<double> covar( ublas::zero_matrix<double>(3, 3) );
-            Vector3D<> c = p.p+_points[0].p+_points[1].p;
-            covar(0,0) += _points[0].p(0)*_points[0].p(0) +
-                          _points[1].p(0)*_points[1].p(0) +
-                          p.p(0)+p.p(0);
-            covar(1,1) += _points[0].p(1)*_points[0].p(1) +
-                          _points[1].p(1)*_points[1].p(1) +
-                          p.p(1)+p.p(1);
-            covar(2,2) += _points[0].p(2)*_points[0].p(2) +
-                          _points[1].p(2)*_points[1].p(2) +
-                          p.p(2)+p.p(2);
-            covar(0,1) += _points[0].p(0)*_points[0].p(1) +
-                          _points[1].p(0)*_points[1].p(1) +
-                          p.p(0)+p.p(1);
-            covar(0,2) += _points[0].p(0)*_points[0].p(2) +
-                          _points[1].p(0)*_points[1].p(2) +
-                          p.p(0)+p.p(2);
-            covar(1,2) += _points[0].p(1)*_points[0].p(2) +
-                          _points[1].p(1)*_points[1].p(2) +
-                          p.p(1)+p.p(2);
-
-            for(int i=2; i<_nrOfContacts;i++){
-                const Vector3D<> &point = _points[i].p;
-                c += point;
-                covar(0,0) += point(0)*point(0);
-                covar(1,1) += point(1)*point(1);
-                covar(2,2) += point(2)*point(2);
-                covar(0,1) += point(0)*point(1);
-                covar(0,2) += point(0)*point(2);
-                covar(1,2) += point(1)*point(2);
-            }
-            const int n = _nrOfContacts+1;
-            covar(0,0) = covar(0,0)-c(0)*c(0)/n;
-            covar(1,1) = covar(1,1)-c(1)*c(1)/n;
-            covar(2,2) = covar(2,2)-c(2)*c(2)/n;
-            covar(0,1) = covar(0,1)-c(0)*c(1)/n;
-            covar(0,2) = covar(0,2)-c(0)*c(2)/n;
-            covar(1,2) = covar(1,2)-c(1)*c(2)/n;
-            covar(1,0) = covar(0,1);
-            covar(2,0) = covar(0,2);
-            covar(2,1) = covar(1,2);
-
-            typedef std::pair<ublas::matrix<double>,ublas::vector<double> > ResultType;
-            //std::cout << "COVAR: " << covar << std::endl;
-            ResultType res = LinearAlgebra::eigenDecompositionSymmetric( covar );
-
-            // 4.1 create the rotationmatrix from the normalized eigenvectors
-            // find max and the second maximal eigenvalue
-            size_t maxEigIdx=2, midEigIdx=1, minEigIdx=0;
-            double maxEigVal = res.second(maxEigIdx);
-            double midEigVal = res.second(midEigIdx);
-            double minEigVal = res.second(minEigIdx);
-            if( maxEigVal < midEigVal ){
-                std::swap(midEigVal,maxEigVal);
-                std::swap(midEigIdx,maxEigIdx);
-            }
-            if( minEigVal>midEigVal ){
-                std::swap(midEigVal,minEigVal);
-                std::swap(midEigIdx,minEigIdx);
-                if( midEigVal>maxEigVal ){
-                    std::swap(midEigVal,maxEigVal);
-                    std::swap(midEigIdx,maxEigIdx);
-                }
-            }
-            // specify x and y axis, x will be the axis with largest spred
-            Vector3D<> maxAxis( res.first(0,maxEigIdx), res.first(1,maxEigIdx), res.first(2,maxEigIdx) );
-            Vector3D<> midAxis( res.first(0,midEigIdx), res.first(1,midEigIdx), res.first(2,midEigIdx) );
-
-            // make sure to turn the z-axis in the direction of the normal
-            Vector3D<> crossAxis = cross(maxAxis,midAxis);
-            if( dot(crossAxis,_points[_deepestIdx].n)<0 ){
-                std::swap(maxAxis,midAxis);
-                crossAxis = -crossAxis;
-            }
-
-
-            _normal = normalize(crossAxis);
-
-            const Rotation3D<> rot(normalize(maxAxis),normalize(midAxis), _normal);
-            const Rotation3D<> rotInv = inverse( rot );
-
-            Vector3D<> max = rotInv * p.p;
-            Vector3D<> min = max;
-            int x_max = _nrOfContacts, x_min = _nrOfContacts,
-                    y_max = _nrOfContacts, y_min = _nrOfContacts,
-                    z_max = _nrOfContacts, z_min = _nrOfContacts;
-            for(int i = 0; i<_nrOfContacts; i++ ){
-                const Vector3D<> prot = rotInv * _points[i].p;
-                if( prot(0)>max(0) ) {max(0) = prot(0); x_max = i;}
-                else if( prot(0)<min(0) ) {min(0) = prot(0); x_min = i;}
-                if( prot(1)>max(1) ) {max(1) = prot(1); y_max = i;}
-                else if( prot(1)<min(1) ) {min(1) = prot(1); y_min=i;}
-                if( prot(2)>max(2) ) {max(2) = prot(2); z_max = i;}
-                else if( prot(2)<min(2) ) {min(2) = prot(2); z_min = i;}
-            }
-            for(int i=0;i<6;i++) _onBorderMap[i] = false;
-            _onBorderMap[x_min] = true;
-            _onBorderMap[x_max] = true;
-            _onBorderMap[y_min] = true;
-            _onBorderMap[y_max] = true;
-            //_onBorderMap[z_min] = true;
-            //_onBorderMap[z_max] = true;
-
-            // compute halflength of box and its midpoint
-            _t3d.P() = rot*( 0.5*(max+min));
-            _t3d.R() = rot;
-            _h = 0.5*(max-min);
-            //_normal = p.n;
-            //for(int i=0; i<_nrOfContacts;i++){
-            //    _normal += _points[0].n;
-            //}
-            //_normal /= _nrOfContacts+1;
-
-        }
+        void fit(ContactPoint& p);
 
         rw::math::Vector3D<> getNormal(){
             return _normal;
@@ -334,7 +145,8 @@ namespace dynamics {
         rw::math::Vector3D<> _h; //halflengths off the obb
         // only used when 3 or more points are available
         ContactPoint _points[5];
-        bool _onBorderMap[6];
+        rw::math::Vector2D<> _projPoints[6];
+        bool _onBorderMap[4];
         //Frame *_objA,*_objB;
         double _threshold, _cosThreshold, _sepThreshold;
         int _deepestIdx,_nrOfContacts;
