a3986ee074178b5a9956d3335f8d25625e876bf9
Jimmy Alison Jørgensen <jimali@mmmi.sdu.dk>
2011-12-12 14:13:16 +0000

    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@3687 494c09c7-a67d-49d4-9938-151fb1965be8

--- RobWork/test/math/QuaternionTest.cpp
+++ RobWork/test/math/QuaternionTest.cpp
@@ -21,8 +21,10 @@
 #include <rw/math/Rotation3D.hpp>
 #include <rw/math/Transform3D.hpp>
 #include <rw/math/RPY.hpp>
-
+#include <rw/math/Math.hpp>
+#include <rw/math/MetricUtil.hpp>
 #include <iostream>
+#include <rw/common/Timer.hpp>
 
 using namespace rw::math;
 
@@ -31,11 +33,169 @@ using namespace rw::math;
 typedef Quaternion<double> QuatD;	// just abbreviations
 typedef Quaternion<float>  QuatF;
 
+
 bool close_enough(Quaternion<> q1,Quaternion<> q2){
     return fabs((q1).getQx()-(q2).getQx())<1e-16 &&
     fabs((q1).getQy()-(q2).getQy())<1e-16 &&
     fabs((q1).getQz()-(q2).getQz())<1e-16 &&
     fabs((q1).getQw()-(q2).getQw())<1e-16;
+}
+
+#include <math.h>
+
+Quaternion<> toQuatN( const Rotation3D<>& rot){
+
+    double d = sqrt( std::max( 0.0, 1 + rot(0,0) + rot(1,1) + rot(2,2) ) ) / 2;
+    double a = sqrt( std::max( 0.0, 1 + rot(0,0) - rot(1,1) - rot(2,2) ) ) / 2;
+    double b = sqrt( std::max( 0.0, 1 - rot(0,0) + rot(1,1) - rot(2,2) ) ) / 2;
+    double c = sqrt( std::max( 0.0, 1 - rot(0,0) - rot(1,1) + rot(2,2) ) ) / 2;
+    a = copysign( a, rot(2,1) - rot(1,2) );
+    b = copysign( b, rot(0,2) - rot(2,0) );
+    c = copysign( c, rot(1,0) - rot(0,1) );
+
+    return Quaternion<>(a,b,c,d);
+}
+
+Quaternion<> toQuatN2( const Rotation3D<>& rot){
+    double a,b,c,d;
+    double tr = 1.0;
+
+    if( rot(0,0) > rot(1,1) ){
+        tr -= rot(1,1);
+        if( rot(0,0) > rot(2,2) ){
+            // 00 was biggest
+            tr += rot(0,0) - rot(2,2);
+            tr = sqrt(tr);
+            d = (rot(1,2)-rot(2,1))/(2*tr);
+            a = tr/2;
+            b = (rot(0,2)+rot(2,0))/(2*tr);
+            c = (rot(1,0)+rot(0,1))/(2*tr);
+        } else {
+            // 22 was biggest
+            tr += rot(2,2) - rot(0,0);
+            tr = sqrt(tr);
+            d = (rot(1,0)-rot(0,1))/(2*tr);
+            a = tr/2;
+            b = (rot(2,0)+rot(0,2))/(2*tr);
+            c = (rot(1,2)+rot(2,1))/(2*tr);
+        }
+    } else {
+        tr -= rot(0,0);
+        if( rot(1,1) > rot(2,2) ){
+            // 11 is biggest
+            tr += rot(1,1) - rot(2,2);
+            tr = sqrt(tr);
+            d = (rot(2,0)-rot(0,2))/(2*tr);
+            a = tr/2;
+            b = (rot(1,0)+rot(0,1))/(2*tr);
+            c = (rot(2,1)+rot(1,2))/(2*tr);
+
+        } else {
+            // 22 is biggest
+            tr += rot(2,2) -rot(1,1);
+            tr = sqrt(tr);
+            d = (rot(1,0)-rot(0,1))/(2*tr);
+            a = tr/2;
+            b = (rot(2,0)+rot(0,2))/(2*tr);
+            c = (rot(1,2)+rot(2,1))/(2*tr);
+        }
+    }
+
+
+
+    return Quaternion<>(a,b,c,d);
+}
+
+/*
+Quaternion<> toQuatN2( const Rotation3D<>& rot){
+    double a,b,c,d;
+    if( rot(0,0) > rot(1,1) ){
+
+    }
+    const double tr = static_cast<double>(rot(0, 0) + rot(1, 1) + rot(2, 2) + 1);
+
+    if (tr > 1e-5) {
+        const double s = static_cast<double>(0.5) / static_cast<double>(sqrt(tr));
+        d = static_cast<double>(0.25) / s;
+        a = static_cast<double>(rot(2, 1) - rot(1, 2)) * s;
+        b = static_cast<double>(rot(0, 2) - rot(2, 0)) * s;
+        c = static_cast<double>(rot(1, 0) - rot(0, 1)) * s;
+    } else {
+    }
+}*/
+Quaternion<> toQuat( const Rotation3D<>& rot){
+    double a,b,c,d;
+    const double tr = static_cast<double>(rot(0, 0) + rot(1, 1) + rot(2, 2) + 1);
+
+    if (tr > 1e-7) {
+        const double s = static_cast<double>(0.5) / static_cast<double>(sqrt(tr));
+        d = static_cast<double>(0.25) / s;
+        a = static_cast<double>(rot(2, 1) - rot(1, 2)) * s;
+        b = static_cast<double>(rot(0, 2) - rot(2, 0)) * s;
+        c = static_cast<double>(rot(1, 0) - rot(0, 1)) * s;
+    } else {
+        if (rot(0, 0) > rot(1, 1) && rot(0, 0) > rot(2, 2)) {
+            const double sa = static_cast<double>(sqrt(rot(0, 0) - rot(1, 1) - rot(2, 2) + 1.0));
+            a = static_cast<double>(0.5)  *  sa;
+
+            // s == 1 / (2.0  *  sa) == 0.25 / (0.5  *  sa)
+            const double s = static_cast<double>(0.25) / a;
+            b = static_cast<double>(rot(0, 1) + rot(1, 0)) * s;
+            c = static_cast<double>(rot(0, 2) + rot(2, 0)) * s;
+            d = static_cast<double>(rot(1, 2) - rot(2, 1)) * s;
+        } else if (rot(1, 1) > rot(2, 2)) {
+            const double sb = static_cast<double>(sqrt(rot(1, 1) - rot(2, 2) - rot(0, 0)  + 1));
+            b = static_cast<double>(0.5) * sb;
+
+            const double s = static_cast<double>(0.25) / b;
+            a = static_cast<double>(rot(0, 1) + rot(1, 0)) * s;
+            c = static_cast<double>(rot(1, 2) + rot(2, 1)) * s;
+            d = static_cast<double>(rot(0, 2) - rot(2, 0)) * s;
+        } else {
+            const double sc = static_cast<double>(sqrt(rot(2, 2) - rot(0, 0) - rot(1, 1)  + 1));
+            c = static_cast<double>(0.5) * sc;
+
+            const double s = static_cast<double>(0.25) / c;
+            a = static_cast<double>(rot(0, 2) + rot(2, 0)) * s;
+            b = static_cast<double>(rot(1, 2) + rot(2, 1)) * s;
+            d = static_cast<double>(rot(0, 1) - rot(1, 0)) * s;
+        }
+
+    }
+    return Quaternion<>(a,b,c,d);
+
+}
+
+
+BOOST_AUTO_TEST_CASE(QuaternionConversionTest){
+    BOOST_MESSAGE("- Testing Quaternion - Rotation conversion test");
+    // we generate a large amount of random rotations and and convert to and from rotation3d
+    const size_t count = 100000;
+    std::vector<Rotation3D<> > rotations(count);
+    for(size_t i=0;i<count; i++){
+        Rotation3D<> rot = Math::ranRotation3D<double>();
+        for(size_t j=0; j<30; j++)
+            rot = rot * Math::ranRotation3D<double>();
+        if(i==0)
+            rot = RPY<>(0,0,0).toRotation3D();
+        rotations[i] = rot;
+    }
+    const double epsilon = 0.00000001;
+    rw::common::Timer time;
+    //for(size_t m=0;m<100; m++)
+    for(size_t i=0;i<count; i++){
+        Rotation3D<> rot = rotations[i];
+        Quaternion<> q = toQuatN2( rot );
+        Rotation3D<> res = q.toRotation3D();
+
+        BOOST_CHECK_MESSAGE(MetricUtil::dist2(res*Vector3D<>::z(), rot*Vector3D<>::z() )<epsilon, i << " : " << MetricUtil::dist2(res*Vector3D<>::z(), rot*Vector3D<>::z() ) << " q:" << q);
+        BOOST_CHECK(MetricUtil::dist2(res*Vector3D<>::y(), rot*Vector3D<>::y() )<epsilon);
+        BOOST_CHECK(MetricUtil::dist2(res*Vector3D<>::x(), rot*Vector3D<>::x() )<epsilon);
+    }
+
+    time.pause();
+    //std::cout << "time1: " << time.getTime() << std::endl;
+
 }
 
 BOOST_AUTO_TEST_CASE(QuaternionTest){
