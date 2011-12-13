77e8113dd2ce85ca47f788dc30004ae88874b435
Jimmy Alison Jørgensen <jimali@mmmi.sdu.dk>
2011-12-13 21:32:29 +0000

    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@3711 494c09c7-a67d-49d4-9938-151fb1965be8

---
+++ RobWork/src/rw/models/DeviceJacobianCalculator.cpp
@@ -0,0 +1,80 @@
+/********************************************************************************
+ * Copyright 2009 The Robotics Group, The Maersk Mc-Kinney Moller Institute, 
+ * Faculty of Engineering, University of Southern Denmark 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ********************************************************************************/
+
+
+#include "DeviceJacobianCalculator.hpp"
+#include "JacobianUtil.hpp"
+#include "Accessor.hpp"
+
+#include <rw/models/DependentJoint.hpp>
+#include <rw/kinematics/FKTable.hpp>
+
+#include <boost/foreach.hpp>
+
+using namespace rw::models;
+using namespace rw::kinematics;
+using namespace rw::math;
+
+DeviceJacobianCalculator::DeviceJacobianCalculator(
+             std::vector<Device::Ptr> devices,
+             const Frame* base,
+             const std::vector<Frame*>& tcps,
+             const State& state):
+             _base(base),
+             _tcps(tcps)
+{
+    _dof = 0;
+    BOOST_FOREACH(Device::Ptr dev, _devices){
+        _dof += dev->getDOF();
+    }
+}
+
+
+
+DeviceJacobianCalculator::~DeviceJacobianCalculator()
+{
+}
+
+
+//Jacobian DeviceJacobianCalculator::get(const FKTable& fk) const {
+Jacobian DeviceJacobianCalculator::get(const rw::kinematics::State& state) const {
+    const rw::kinematics::FKTable fk(state);
+    Jacobian jacobian(Jacobian::ZeroBase(6 * _tcps.size(), _dof));
+
+    BOOST_FOREACH(Device::Ptr dev, _devices){
+        Jacobian jac = dev->baseJframes(_tcps, state);
+
+    }
+
+
+    for (size_t i = 0; i<_tcps.size(); i++) {
+        const Frame* tcpFrame = _tcps[i];
+        const JacobianSetup& setup = _jacobianSetups[i];        
+
+        Transform3D<> tcp = fk.get(*tcpFrame);
+        for (std::vector<std::pair<const Joint*, size_t> >::const_iterator it = setup.begin(); it != setup.end(); ++it) {
+            Transform3D<> jointTransform = fk.get(*(*it).first);
+            (*it).first->getJacobian(6*i, (*it).second, jointTransform, tcp, state, jacobian);
+        }
+    }
+
+    const Rotation3D<>& R = fk.get(*_base).R();
+    return inverse(R) * jacobian;
+}
+
+
+
---
+++ RobWork/src/rw/models/DeviceJacobianCalculator.hpp
@@ -0,0 +1,81 @@
+/********************************************************************************
+ * Copyright 2009 The Robotics Group, The Maersk Mc-Kinney Moller Institute, 
+ * Faculty of Engineering, University of Southern Denmark 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ********************************************************************************/
+
+
+#ifndef RW_MODELS_DEVICEJACOBIANCALCULATOR_HPP
+#define RW_MODELS_DEVICEJACOBIANCALCULATOR_HPP
+
+
+#include <rw/kinematics/State.hpp>
+
+#include "JacobianCalculator.hpp"
+#include "Joint.hpp"
+#include "JointDevice.hpp"
+
+namespace rw {
+namespace models {
+
+/**
+ * @brief Calculator for Jacobians of one or several Devices.
+ *
+ * Implements Jacobian calculations for one or several Devices.
+ *
+ * If more than one end-effector is given a "stacked" Jacobian is returned.
+ *
+ */
+class DeviceJacobianCalculator: public JacobianCalculator
+{
+public:
+    /**
+     * @brief Constructs JacobianCalculator.
+     *
+     * The dimension of the jacobian wil be (tcps.size() * 6, device.getDOF()).
+     *
+     * @param device [in] The device to calculate for
+     * @param base [in] Reference base of the Jacobian. Does not have to be the same as the base of the device
+     * @param tcps [in] List of tool end-effectors for which to calculate the Jacobian.
+     * @param state [in] State giving how frame are connected
+     */
+	DeviceJacobianCalculator(std::vector<Device::Ptr> devices,
+                             const kinematics::Frame* base,
+                             const std::vector<kinematics::Frame*>& tcps,
+                             const kinematics::State& state);
+
+    /**
+     * @brief Destructor
+     */
+    virtual ~DeviceJacobianCalculator();
+
+    /**
+     * @copydoc JacobianCalculator::get(const rw::kinematics::FKTable& fk) const
+     */
+    //virtual math::Jacobian get(const rw::kinematics::FKTable& fk) const;
+    virtual rw::math::Jacobian get(const rw::kinematics::State& state) const;
+
+
+private:
+    std::vector<Device::Ptr> _devices;
+    const kinematics::Frame* _base;
+    std::vector<kinematics::Frame*> _tcps;
+    size_t _dof;
+};
+
+} //end namespace models
+} //end namespace rw
+
+
+#endif /* JOINTDEVICEJACOBIANCALCULATOR_HPP_ */
