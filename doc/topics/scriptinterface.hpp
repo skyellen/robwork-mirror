2002ccf80c1f5084403953f44f0f8c4081bd2d58
Jimmy Alison Jørgensen <jimali@mmmi.sdu.dk>
2012-03-01 22:47:10 +0000

    more doc
    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@3946 494c09c7-a67d-49d4-9938-151fb1965be8

--- doc/topics/scriptinterface.hpp
+++ doc/topics/scriptinterface.hpp
@@ -14,6 +14,41 @@ Make sure you have both swig and python installed. You can check this when runni
 cmake. There should be lines like: "RobWork: python bindings ENABLED" and
 "RobWorkStudio: python bindings ENABLED"
 
+When compiling RobWork, RobWorkStudio and RobWorkSim the python interfaces will be
+generated. These consist of two files per project and they are placed in the
+libs/Release|Debug folder.
 
+Linux example:
+\verbatim
+RobWork/libs/Debug/_rw.so \n
+RobWork/libs/Debug/rw.py \n
+RobWorkStudio/libs/Debug/_rws.so \n
+RobWorkStudio/libs/Debug/rws.py \n
+RobWorkSim/libs/Debug/_rwsim.so \n
+RobWorkSim/libs/Debug/rwsim.py \n
+\endverbatim
+
+For the above example loading of the modules in the python interpreter could look like this:
+\code
+import sys
+sys.path.append('/home/jimali/rw/RobWork/libs/Debug')
+sys.path.append('/home/jimali/rw/RobWorkSim/libs/Debug')
+sys.path.append('/home/jimali/rw/RobWorkStudio/libs/Debug')
+import rw, rws, rwsim
+
+// now we can use all robwork python bindings
+\endcode
+
+Now all RobWork types available through the bindings should be available. For some purposes it might
+be usefull to start an instance of RobWorkStudio:
+
+\code
+rwstudio = rws.getRobWorkStudio()
+// now load a workcell
+rwstudio.loadWorkCell('some/workcell.wc.xml')
+// lets get the workcell
+wc = rwstudio.getWorkCell()
+print wc.getName()
+\endcode
 
 */
