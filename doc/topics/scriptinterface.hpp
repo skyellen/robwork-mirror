b652f3e8b62c2aa336766128b6320ec0354bbf11
Jimmy Alison Jørgensen <jimali@mmmi.sdu.dk>
2012-03-02 09:31:16 +0000

    added more python doc
    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@3951 494c09c7-a67d-49d4-9938-151fb1965be8

--- doc/topics/scriptinterface.hpp
+++ doc/topics/scriptinterface.hpp
@@ -5,6 +5,7 @@
 \page page_rw_scriptinterface Script interface
 - \ref sec_rw_sinterface_intro
 - \ref sec_robwork_python
+    - \ref sec_robwork_python_communicate_plugins
 \section sec_rw_sinterface_intro Introduction
 
 
@@ -39,8 +40,8 @@ import rw, rws, rwsim
 // now we can use all robwork python bindings
 \endcode
 
-Now all RobWork types available through the bindings should be available. For some purposes it might
-be usefull to start an instance of RobWorkStudio:
+Now all RobWork types, that have bindings, should be available. For some purposes it might
+be useful to start an instance of RobWorkStudio:
 
 \code
 rwstudio = rws.getRobWorkStudio()
@@ -50,5 +51,19 @@ rwstudio.loadWorkCell('some/workcell.wc.xml')
 wc = rwstudio.getWorkCell()
 print wc.getName()
 \endcode
+
+\subsection sec_robwork_python_communicate_plugins
+It is often necessary to send messages or data to one or more plugins. For this the
+generic event methods on RobWorkStudio is used. These are wrapped in utils such that
+the current send methods can be used in python
+\code
+rwstudio.send("someStr")
+rwstudio.send("msgId", "some string")
+rwstudio.send("msgId", 0.45235)
+rwstudio.send("msgId", rw.Q(4, 0.1, 0.2, 0.3, 0.5) )
+rwstudio.send("msgId", somePropertyMap )
+\endcode
+
+The first send method use RobWorkStudio::genericEvent the next 4 use RobWorkStudio::genericAnyEvent.
 
 */
