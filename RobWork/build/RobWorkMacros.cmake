47eb9022f9399ee1183540d3f363b28c73a2bef9
Jimmy Alison Jørgensen <jimali@mmmi.sdu.dk>
2012-03-13 07:55:41 +0000

    added macro
    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@3980 494c09c7-a67d-49d4-9938-151fb1965be8

--- RobWork/build/RobWorkMacros.cmake
+++ RobWork/build/RobWorkMacros.cmake
@@ -24,3 +24,49 @@ LIST( GET VERSIONS 0 ${MAJOR})
 LIST( GET VERSIONS 1 ${MINOR})
 LIST( GET VERSIONS 2 ${PATCH})
 ENDMACRO()
+
+######################################################################
+# Get a string describing the current system, e.g. windows-mingw-x64, mac-x64 or ubuntu-11.04-x64
+#
+MACRO(RW_SYS_INFO INFO)
+    IF(CMAKE_SIZEOF_VOID_P EQUAL 4)
+    	SET(ARCH "x86")
+    ELSE()
+    	SET(ARCH "amd64")
+    ENDIF()
+    
+    IF(UNIX)
+        IF(MAC)
+            SET(SUFFIX "mac-${ARCH}")
+        ELSE()
+            #EXECUTE_PROCESS(COMMAND cat /etc/issue OUTPUT_VARIABLE SUFFIX)
+            EXECUTE_PROCESS(COMMAND cat /etc/lsb-release OUTPUT_VARIABLE SUFFIX)
+            STRING( REGEX MATCHALL "\".+\"" SUFFIX ${SUFFIX} )
+            
+            # this will add kernel version eg Linux_3.0....
+            #EXECUTE_PROCESS(COMMAND uname  -r OUTPUT_VARIABLE KERNEL_VERSION)
+            #SET(SUFFIX "${SUFFIX}_${KERNEL_VERSION}")
+            SET(SUFFIX "${SUFFIX}_${ARCH}_${CMAKE_BUILD_TYPE}")
+            STRING( REPLACE "\"" "" SUFFIX ${SUFFIX} )
+            STRING( REPLACE " " "_" SUFFIX ${SUFFIX} )
+        
+            # this will make it lowercase        
+            #STRING(TOLOWER "${SUFFIX}" SUFFIX)
+            #MESSAGE("${SUFFIX}")
+        ENDIF()
+    ELSEIF(MINGW)
+    	SET(SUFFIX "windows-mingw-${ARCH}")
+    ELSEIF(MSVC)
+    	IF(MSVC80)
+    		SET(SUFFIX "windows-msvc2005-${ARCH}")
+    	ELSEIF(MSVC90)
+    		SET(SUFFIX "windows-msvc2008-${ARCH}")
+    	ELSEIF(MSVC10)
+    		SET(SUFFIX "windows-msvc2010-${ARCH}")
+    	ENDIF()
+    ELSE()
+    	# Trouble
+    	
+    ENDIF()
+    SET(${INFO} ${SUFFIX})
+ENDMACRO()
