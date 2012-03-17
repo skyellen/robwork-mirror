7477925151b8133e81f334d83adfe53acaaf5275
Jimmy Alison Jørgensen <jimali@mmmi.sdu.dk>
2012-03-17 10:27:35 +0000

    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@3987 494c09c7-a67d-49d4-9938-151fb1965be8

--- RobWorkStudio/example/scripts/grab_multiple_shots_ROTAXIS.lua
+++ RobWorkStudio/example/scripts/grab_multiple_shots_ROTAXIS.lua
@@ -1,12 +1,18 @@
 -- a vector pointing parallel with rotation axis
-rot_vec = rw.Vector3D(1,0,0)
+rot_vec = rw.Vector3D(0,0,1)
 -- a point on the rotation axis
 rot_pos = rw.Vector3D(0,0,0)
 -- the angle increments in degree
-angle = 10
+angle = 5
+video_name = "MyRWVideo"
 
+tmpdir = "tmp" .. os.clock()
+os.execute("mkdir " .. tmpdir)
+
+cview = rwstudio:getViewTransform()
+count = 0
 rot = rw.EAA(rot_vec, angle/180*3.14):toRotation3D()
-
+tmp2 = 0
 for tmp=0,359,angle do
  vt3d = rwstudio:getViewTransform()
  arm = vt3d:P()-rot_pos
@@ -14,7 +20,31 @@ for tmp=0,359,angle do
  nrot = rot * vt3d:R()
  nvt3d = rw.Transform3D(narm, nrot)
  rwstudio:setViewTransform(nvt3d)
- rw.sleep(0.5)
- rwstudio:saveViewGL("view_" .. tmp .. ".png")
+ rw.sleep(0.2)
+ rwstudio:saveViewGL(tmpdir .. "/" .. video_name .. count .. ".png")
+ tmp2=tmp
+ count = count + 1
 end
-form(nvt3d)sform(nvt3d)
+
+rot_vec = rw.Vector3D(1,0,0)
+rot = rw.EAA(rot_vec, angle/180*3.14):toRotation3D()
+for tmp=0,359,angle do
+ tmp3 = tmp2+tmp
+ vt3d = rwstudio:getViewTransform()
+ arm = vt3d:P()-rot_pos
+ narm = rot*arm
+ nrot = rot * vt3d:R()
+ nvt3d = rw.Transform3D(narm, nrot)
+ rwstudio:setViewTransform(nvt3d)
+ rw.sleep(0.2)
+
+ rwstudio:saveViewGL(tmpdir .. "/" .. video_name .. count .. ".png")
+ count = count + 1
+end
+
+rwstudio:setViewTransform(cview)
+print("\nExecuting: ffmpeg -qscale 5 -r 10 -b 9600 -i " .. tmpdir .. "/" .. video_name .. "%d.png " .. video_name .. ".avi")
+os.execute("ffmpeg -qscale 5 -r 10 -b 9600 -i " .. tmpdir .. "/" .. video_name .. "%d.png " .. video_name .. ".avi")
+print("\nDeleting temporary files..")
+os.execute("rm -rf " .. tmpdir)
+
