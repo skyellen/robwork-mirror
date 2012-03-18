b2fcf6517d847e663043cb8247573c94bf8a8324
Jimmy Alison Jørgensen <jimali@mmmi.sdu.dk>
2012-03-18 07:46:17 +0000

    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@4011 494c09c7-a67d-49d4-9938-151fb1965be8

--- RobWork/src/rwlibs/lua/LuaGraphicsStub.hpp
+++
@@ -1,8 +0,0 @@
-/*
-** Lua binding: LuaGraphics
-** Generated automatically by tolua++-1.0.92 on Sat Oct  8 12:04:58 2011.
-*/
-
-/* Exported function */
-TOLUA_API int  tolua_LuaGraphics_open (lua_State* tolua_S);
-
