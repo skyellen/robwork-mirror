aeb5a56d601c167ae808c2d1c86eff48cdb1f9bc
Jimmy Alison Jørgensen <jimali@mmmi.sdu.dk>
2012-05-03 07:20:43 +0000

    backward compatibility for old FindRobWorkSim
    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@4184 494c09c7-a67d-49d4-9938-151fb1965be8

---
+++ RobWorkSim/build/FindRobWorkSim.cmake
@@ -0,0 +1,30 @@
+#########################################################################################
+######################## THIS FILE IS DEPRECATED ########################################
+#
+# Allow the syntax else (), endif (), etc.
+#
+SET(CMAKE_ALLOW_LOOSE_LOOP_CONSTRUCTS 1)
+
+MESSAGE(STATUS "")
+MESSAGE(STATUS "DEPRECATION NOTICE: \nThis is the deprecated FindRobWork script. Please instead use \n SET(RobWork_DIR RWPATH/cmake) \n FIND_PACKAGE(RobWork ... ) ")
+MESSAGE(STATUS "")
+# Get the compiler architecture
+IF(CMAKE_SIZEOF_VOID_P EQUAL 8)
+	SET(AMD64 1)
+ELSE()
+	SET(AMD64 0)
+ENDIF()
+
+IF(DEFINED RWSIM_ROOT)
+	FILE(TO_CMAKE_PATH ${RWSIM_ROOT} RWSIM_ROOT)
+ENDIF()
+IF(DEFINED ROBWORKSIM_ROOT)
+	FILE(TO_CMAKE_PATH ${ROBWORKSIM_ROOT} ROBWORKSIM_ROOT)
+	IF(NOT DEFINED RWSIM_ROOT)
+		SET(RWSIM_ROOT "${ROBWORKSIM_ROOT}")
+	ENDIF()
+ENDIF()
+
+# now include the correct setup stuff
+SET(RobWorkSim_DIR ${CMAKE_CURRENT_LIST_DIR}/../cmake)
+FIND_PACKAGE(RobWorkSim ${RobWorkSim_FIND_VERSION} REQUIRED NO_MODULE)
