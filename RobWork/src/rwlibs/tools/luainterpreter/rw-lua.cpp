fd9ea1c6b9dab21d3189fdc2ccd57f3e7042ca17
Jimmy Alison Jørgensen <jimali@mmmi.sdu.dk>
2012-07-08 07:50:20 +0000

    added tools component
    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@4296 494c09c7-a67d-49d4-9938-151fb1965be8

---
+++ RobWork/src/rwlibs/tools/luainterpreter/rw-lua.cpp
@@ -0,0 +1,23 @@
+#include <rwlibs/swig/Lua.hpp>
+
+#include <iostream>
+#include <string>
+
+int main(int argc, char** argv)
+{
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <lua-script>\n";
+        return 1;
+    }
+
+    lua_State *lstate = lua_open();
+    luaL_openlibs(lstate);
+
+    rwlibs::swig::openLuaLibRW(lstate);
+
+    const int error = luaL_dofile(lstate, argv[1]);
+    if (error) std::cerr << lua_tostring(lstate, -1) << "\n";
+    lua_close(lstate);
+
+    return error;
+}
