571cc41430b7bc63bbf9f01a76305d2917b948f2
Jimmy Alison Jørgensen <jimali@mmmi.sdu.dk>
2012-08-01 19:59:41 +0000

    doxygen does not detect markdown files correctly
    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@4342 494c09c7-a67d-49d4-9938-151fb1965be8

---
+++ doc/apidoc/index.hpp
@@ -0,0 +1,65 @@
+/**
+
+# RobWork # {#index}
+
+[TOC]
+
+# Introduction # {#sec_index_intro}
+
+*RobWork* is a collection of C++ libraries for simulation and control
+of robot systems. RobWork is used for research and education as well
+as for practical robot applications. Features of the library include:
+
+- Kinematic modeling of various types of industrial manipulators.
+- Path-planning and inverse kinematics algorithms.
+- Simulation of sensors
+- Task and trajectory representations
+
+Besides the core part RobWork has a number of add-ons including
+
+- RobWorkStudio which provides a graphical user interface
+- RobWorkSim which is a simulator suited for grasping.
+
+Target audience of RobWork is:
+- Researchers who needs a common framework for experimental robotics
+- Students who wants to experiment with concepts of robotics
+- Implementers of robot applications
+
+
+RobWork is developed at the <a
+href="http://www.mip.sdu.dk/robotics">robotics department</a> of the
+<a href="http://www.sdu.dk/mmmi">Maersk McKinney Moller Institute</a>
+at the <a href="http://www.sdu.dk">University of Southern
+Denmark</a>. The focus of the department is on industrial robots and
+their applications.
+
+
+# Release Notes # {#sec_release_notes}
+Changelog and release notes can be found here [Release Notes](@ref page_release_notes)
+
+# Manuals and tutorials # {#sec_index_manual}
+
+- [Getting Started](@ref md_gettingstarted), [Installation](@ref md_installation), [Manual (C++)](@ref md_manual), 
+
+Other topics include
+- [Motion Planning](@ref md_motionplanning), [Online Control](@ref md_onlinecontrol), [Grasp Simulation](@ref md_graspsimulation), 
+[Plugins](@ref md_pugins), [Scrip Interface](@ref md_scriptinterface), [Codeguidelines](@ref md_codeguidelines)  
+
+
+# Download # {#sec_index_download}
+- Download *RobWork* and *RobWorkStudio* from the <a href="http://www.robwork.dk">RobWork homepage</a>.
+
+# Installation # {#sec_index_installation}
+- Installation instructions are included in \ref page_rw_installation. However, online installation
+instructions are typically more up to date and these can be found at the <a href="http://www.robwork.dk">RobWork homepage</a>.
+
+# License # {#sec_index_license}
+RobWork is distributed under the "Apache License, Version 2.0", [See here](@ref md_license). For convenience, a number of
+open-source libraries are distributed together with RobWork; the
+RobWork license does not apply to these libraries.
+
+# Mailing lists # {#sec_index_mailinglists}
+- <a href="http://www.mip.sdu.dk/robwork/joomla/index.php?option=com_content&view=article&id=54&Itemid=57">RobWork mailing list</a>
+
+
+*/
