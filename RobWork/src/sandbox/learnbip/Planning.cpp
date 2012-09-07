0682265915cce07f2bc73034a8fb5cbdf67319de
Thomas Nicky Thulesen <tnt@mmmi.sdu.dk>
2012-09-07 12:07:44 +0000

    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@4425 494c09c7-a67d-49d4-9938-151fb1965be8

--- RobWork/src/sandbox/learnbip/Planning.cpp
+++ RobWork/src/sandbox/learnbip/Planning.cpp
@@ -253,7 +253,8 @@ Planning::Status Planning::getPath(QPath &res, const rw::math::Q qTo, const rw::
 	if (planner->query(qFrom,qTo,path,10.)) {
 		PathLengthOptimizer optimizer(constraint, _metric);
 		path = optimizer.pathPruning(path);
-		path = optimizer.partialShortCut(path,10,0.1,0.25);
+		if (path.size() > 2)
+			path = optimizer.partialShortCut(path,10,0.1,0.25);
 		path = optimizer.pathPruning(path);
 		res.clear();
 		for (unsigned int i = 0; i < path.size(); i++) {
@@ -313,7 +314,8 @@ Planning::Status Planning::getPath(QPath &res, const Transform3D<> pose, const Q
 					if (planner->query(start,end,path,10.)) {
 						PathLengthOptimizer optimizer(constraint, _metric);
 						path = optimizer.pathPruning(path);
-						path = optimizer.partialShortCut(path,10,0.1,0.25);
+						if (path.size() > 2)
+							path = optimizer.partialShortCut(path,10,0.1,0.25);
 						path = optimizer.pathPruning(path);
 						res.clear();
 						for (unsigned int i = 0; i < path.size(); i++) {
