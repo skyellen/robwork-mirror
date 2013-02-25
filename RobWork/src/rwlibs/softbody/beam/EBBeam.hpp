3dfe9086e1f2b41a6b66c95bd49eb024a4002e4f
Andreas Rune Fugl <arf@mmmi.sdu.dk>
2013-02-25 14:30:09 +0000

    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@4637 494c09c7-a67d-49d4-9938-151fb1965be8

--- RobWork/src/rwlibs/softbody/beam/EBBeam.hpp
+++ RobWork/src/rwlibs/softbody/beam/EBBeam.hpp
@@ -18,8 +18,48 @@
 #ifndef EBBEAM_HPP
 #define EBBEAM_HPP
 
-class EBBeam
-{
+class EBBeam {
+	public:
+		EBBeam(
+		const double H, 
+		   const double K, 
+		   const double L, 
+		   const double E, 
+		   const double rho,
+		   const double h
+		   ) :
+			_H(H),
+			_K(K),
+			_L(L),
+			_E(E),
+			_rho(rho),
+			_h(h)
+		   {
+			_J = (_H * pow(_K, 3.0) ) / 12.0;
+			_q = -9.82 * _rho * _K * _H;
+		};
+		
+		double operator() (const int i) const {
+		const double x = i * _h;
+		
+		
+		
+		return (_q * pow(x, 2.0) * (6.0 * pow(_L, 2.0) - 4.0 * _L * x + pow(x, 2.0))) / (24.0 * _E * _J);
+		
+		};
+		
+		double d(const int i) const {
+		const double x = i * _h;
+		
+		return (_q * x * (3 * pow(_L, 2.0) - 3 *  _L * x + pow(x, 2.0)))/(6 *  _E * _J);
+		
+		};
+    
+    private:   
+		const double _H, _K, _L, _E, _rho, _h;
+		
+		double _J;
+		double _q;
 };
 
 #endif // EBBEAM_HPP
