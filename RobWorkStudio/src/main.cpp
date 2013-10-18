17a876eb6bedc68ab7af991535268c527f1b3ab6
Thomas Nicky Thulesen <tnt@mmmi.sdu.dk>
2013-10-18 22:19:48 +0000

    Changed RobWorkStudio to not build as Console application in Visual Studio in Release mode (avoiding the terminal in the background).
    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@5029 494c09c7-a67d-49d4-9938-151fb1965be8

--- RobWorkStudio/src/main.cpp
+++ RobWorkStudio/src/main.cpp
@@ -183,3 +183,8 @@ int main(int argc, char** argv)
     return 0;
 }
 
+#ifdef _WIN32
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+	return main(__argc, __argv);
+}
+#endif
