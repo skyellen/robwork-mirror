eabed57c0ddea215282653b47a234359694cbca5
Jimmy Alison Jørgensen <jimali@mmmi.sdu.dk>
2013-10-23 10:22:07 +0000

    aded test for VectorND
    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@5033 494c09c7-a67d-49d4-9938-151fb1965be8

--- RobWork/test/math/VectorNDTest.cpp
+++ RobWork/test/math/VectorNDTest.cpp
@@ -21,40 +21,26 @@
 using namespace rw::math;
 
 
-BOOST_AUTO_TEST_CASE(Vector3DTest)
+BOOST_AUTO_TEST_CASE(VectorNDTest)
 {
     BOOST_MESSAGE("- Testing VectorND");
-    const VectorND<3,double> v1(1.0, 2.0, 3.0);
-    const VectorND<3,double> v2(v1);
-    const VectorND<3,double> v3 = v1 + v2;
-    const VectorND<3, double> v4 = Vector3D<>(2.0, 4.0, 6.0);
-    BOOST_CHECK(( v3 - v4 ).normInf() == 0);
-    BOOST_CHECK( norm_inf(cross(v1, v2)) == 0);
+    const VectorND<3,double> v1 = VectorND<3,double>::zero();
+    const VectorND<3,double> v2 = VectorND<3,double>::zero();
+    BOOST_CHECK(( v1 - v2 ).normInf() == 0);
 	
-	
-    const VectorND<3, double> v5(3.0, 4.0, 5.0);
-    const VectorND<3, double> v5_norm = normalize(v5);
-    BOOST_CHECK(fabs(norm_2(v5_norm)-1)<1e-15);
-    const double len = v5.norm2();
-    BOOST_CHECK(fabs(v5_norm(0) - v5(0)/len)<1e-15);
-    BOOST_CHECK(fabs(v5_norm(1) - v5(1)/len)<1e-15);
-    
     VectorND<3, double> v6;
-    v6(0) = len;
-    BOOST_CHECK(v6(0) == len);
+    v6(0) = 0.3;
+    BOOST_CHECK(v6(0) == 0.3);
 
-    const VectorND<4,std::string> vs1("x1", "y1", "z1", "a1");
-
-    BOOST_CHECK(vs1[0] == "x1");
-    BOOST_CHECK(vs1[1] == "y1");
-    BOOST_CHECK(vs1[2] == "z1");
-	BOOST_CHECK(vs1[3] == "a1");
+	Eigen::Vector3d ev(1.1, 5.51, -10.3);
+    const VectorND<3,double> vd(ev);
 
 
-	Eigen::Vector3d ev(1.1, 5.51, -10.3)
-    const VectorND<double> vd(ev);
-    const VectorND<int> vi = cast<int>(vd);
-    BOOST_CHECK(vi(0) == 1);
-    BOOST_CHECK(vi(1) == 5);
-    BOOST_CHECK(vi(2) == -10);
+    std::vector< VectorND<6,double> > vOfVectors;
+
+    vOfVectors.push_back( VectorND<6,double>::zero() );
+    vOfVectors.push_back( VectorND<6,double>::zero() );
+    vOfVectors.push_back( VectorND<6,double>::zero() );
+    vOfVectors.push_back( VectorND<6,double>::zero() );
+
 }
