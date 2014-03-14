7bb7b28d336e9122fe3800b78128dc0f8e215037
Jimmy Alison Jørgensen <jimali@mmmi.sdu.dk>
2014-03-14 14:21:15 +0000

    see Issue 74: This adds an ODEPlugin that follows the new rw plugin concept with extensions and extension points.
    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@5247 494c09c7-a67d-49d4-9938-151fb1965be8

---
+++ RobWorkSim/src/rwsimlibs/ode/ODEPlugin.cpp
@@ -0,0 +1,59 @@
+/********************************************************************************
+ * Copyright 2009 The Robotics Group, The Maersk Mc-Kinney Moller Institute,
+ * Faculty of Engineering, University of Southern Denmark
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ********************************************************************************/
+
+#include "ODEPlugin.hpp"
+#include "ODESimulator.hpp"
+
+using namespace rwsim::simulator;
+using namespace rw::common;
+
+RW_ADD_PLUGIN(ODEPlugin)
+
+ODEPlugin::ODEPlugin():Plugin("ODEPlugin", "ODEPlugin", "0.1")
+{
+}
+
+ODEPlugin::~ODEPlugin()
+{
+}
+
+std::vector<rw::common::Extension::Descriptor> ODEPlugin::getExtensionDescriptors()
+{
+    std::vector<Extension::Descriptor> exts;
+    exts.push_back(Extension::Descriptor("ODEPhysicsEngine","rwsim.simulator.PhysicsEngine"));
+
+    // todo: add posible properties to the extension descriptor
+    exts.back().getProperties().set<std::string>("engineID", "ODE");
+    //exts.back().getProperties().set<std::string>("engineID", "ODE");
+
+    return exts;
+}
+
+rw::common::Ptr<rw::common::Extension> ODEPlugin::makeExtension(const std::string& str)
+{
+    if(str=="ODEPhysicsEngine"){
+        Extension::Ptr extension = rw::common::ownedPtr( new Extension("ODEPhysicsEngine","rwsim.simulator.PhysicsEngine",
+                this, ownedPtr(new rwsim::simulator::ODESimulator()) ) );
+
+        // todo: add posible properties to the extension descriptor
+        //exts.back().getProperties().set<std::string>(propid, value);
+        extension->getProperties().set<std::string>("engineID", "ODE");
+        return extension;
+    }
+    return NULL;
+}
+
---
+++ RobWorkSim/src/rwsimlibs/ode/ODEPlugin.hpp
@@ -0,0 +1,52 @@
+/********************************************************************************
+ * Copyright 2009 The Robotics Group, The Maersk Mc-Kinney Moller Institute,
+ * Faculty of Engineering, University of Southern Denmark
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ********************************************************************************/
+
+#ifndef RWSIM_SIMULATOR_ODEPLUGIN_HPP_
+#define RWSIM_SIMULATOR_ODEPLUGIN_HPP_
+
+#include <rw/common/Plugin.hpp>
+#include "ODESimulator.hpp"
+
+namespace rwsim {
+namespace simulator {
+
+/**
+ * @brief A ODE plugin that define extensions for rwsim.simulator.PhysicsEngine
+ */
+class ODEPlugin: public rw::common::Plugin {
+public:
+
+    /**
+     * @brief constructor
+     */
+    ODEPlugin();
+
+    //! destructor
+    virtual ~ODEPlugin();
+
+    //! @copydoc rw::common::Plugin::getExtensionDescriptors
+    std::vector<rw::common::Extension::Descriptor> getExtensionDescriptors();
+
+    //! @copydoc rw::common::Plugin::makeExtension
+    rw::common::Ptr<rw::common::Extension> makeExtension(const std::string& str);
+
+};
+
+}
+}
+
+#endif /* ODEBODY_HPP_ */
