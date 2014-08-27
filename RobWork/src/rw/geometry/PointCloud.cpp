39c7011e12385d1f40d213450dbfa1eb4acd571b
Jimmy Alison Jørgensen <jimali@mmmi.sdu.dk>
2014-08-27 07:06:10 +0000

    The loader has been updated to include PCD 0.7 loading, however binary format is not supported.
    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@5463 494c09c7-a67d-49d4-9938-151fb1965be8

--- RobWork/src/rw/geometry/PointCloud.cpp
+++ RobWork/src/rw/geometry/PointCloud.cpp
@@ -71,6 +71,64 @@ void PointCloud::savePCD(const PointCloud& img, const std::string& filename, con
     output.close();
 }
 
+namespace {
+    PointCloud::Ptr loadPCD_0_7(const std::string& filename ){
+        //Start by storing the current locale. This is retrieved by passing NULL to setlocale
+        std::string locale = setlocale(LC_ALL, NULL);
+        setlocale( LC_ALL, "C" );
+
+        char line[500];
+        int width, height, nr_points;
+        float version;
+        std::ifstream input(filename.c_str());
+        input.getline(line, 500); // output << "# .PCD v.5 - Point Cloud Data file format\n";
+        input.getline(line, 500); // output << "FIELDS x y z\n";
+        input.getline(line, 500); // output << "SIZE 4 4 4\n";
+        input.getline(line, 500); //output << "TYPE F F F\n";
+        std::string tmp;
+        input >> tmp;
+        while(tmp!="DATA"){
+            if(tmp=="WIDTH"){ input >> width;}
+            else if(tmp=="HEIGHT"){input >> height;}
+            else if(tmp=="VERSION"){input >> version;}
+            else if(tmp=="FIELDS"){}
+            else if(tmp=="HEIGHT"){}
+            else if(tmp=="SIZE"){}
+            else if(tmp=="TYPE"){}
+            else if(tmp=="COUNT"){}
+            else if(tmp=="VIEWPOINT"){}
+            else if(tmp=="POINTS"){input >> nr_points;}
+            input.getline(line, 500);
+            input >> tmp;
+        }
+        PointCloud::Ptr img;
+        // next data should be either ascii or binary
+        std::string type;
+        input >> type;
+        if(type=="binary"){
+            setlocale( LC_ALL, locale.c_str());
+            RW_THROW("binary PCD format not supported! Please convert to ascii PCD format in order to use this loader!");
+        } else {
+            // assume that data type is ascii
+            input.getline(line, 500);
+            //std::cout << "w:" << width << " h:" << height << " p:" << nr_points << std::endl;
+            PointCloud::Ptr img = rw::common::ownedPtr( new PointCloud(width,height) );
+            input.getline(line, 500); // output << "DATA ascii\n";
+
+            for(int i=0;i<nr_points;i++){
+                rw::math::Vector3D<float> p;
+                input.getline(line, 500);
+                sscanf(line, "%f %f %f", &p[0], &p[1], &p[2]);
+                img->getData()[i] = p;
+            }
+
+            setlocale( LC_ALL, locale.c_str());
+        }
+        return img;
+    }
+}
+
+
 PointCloud::Ptr PointCloud::loadPCD(const std::string& filename ){
     //Start by storing the current locale. This is retrieved by passing NULL to setlocale
     std::string locale = setlocale(LC_ALL, NULL);
