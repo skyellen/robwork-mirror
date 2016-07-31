f1ac430269332e845890a101ce1f6f8f50f38f18
Thomas Nicky Thulesen <tnt@mmmi.sdu.dk>
2016-07-31 11:31:29 +0000

    Added minimal test for QHull which currently fails on Windows 64 bit. Internal QHull should be updated to at least QHull 2011.1 to support Windows 64 bit (released 17/4-2011).
    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@6055 494c09c7-a67d-49d4-9938-151fb1965be8

---
+++ RobWork/gtest/geometry/QHullTest.cpp
@@ -0,0 +1,41 @@
+/********************************************************************************
+* Copyright 2016 The Robotics Group, The Maersk Mc-Kinney Moller Institute,
+* Faculty of Engineering, University of Southern Denmark
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+********************************************************************************/
+
+#include <gtest/gtest.h>
+
+#include <rw/geometry/QHullND.hpp>
+#include <rw/math/VectorND.hpp>
+
+#include <vector>
+
+using rw::geometry::QHullND;
+using rw::math::VectorND;
+
+TEST(QHullND, build2D) {
+	QHullND<2> qhull;
+	std::vector<VectorND<2> > vertices(3);
+	vertices[0][0] = 1;
+	vertices[0][1] = 0;
+	vertices[1][0] = 0;
+	vertices[1][1] = 1;
+	vertices[2][0] = -1;
+	vertices[2][1] = -1;
+	qhull.rebuild(vertices);
+	const VectorND<2> center = qhull.getCentroid();
+	EXPECT_EQ(center[0], 0);
+	EXPECT_EQ(center[1], 0);
+}
