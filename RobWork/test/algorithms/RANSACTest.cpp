0cfb7d7759b55bfc2c7b79e6457207bc413d8b5a
Thomas Nicky Thulesen <tnt@mmmi.sdu.dk>
2017-04-24 14:12:26 +0000

    Fixed RANSAC tests (by Michael).
    git-svn-id: https://svnsrv.sdu.dk/svn/RobWork/trunk@6198 494c09c7-a67d-49d4-9938-151fb1965be8

--- RobWork/test/algorithms/RANSACTest.cpp
+++ RobWork/test/algorithms/RANSACTest.cpp
@@ -25,10 +25,12 @@
 #include <rwlibs/algorithms/StablePose1DModel.hpp>
 #include <rwlibs/algorithms/StablePose0DModel.hpp>
 
+#include <boost/foreach.hpp>
+
 #include <vector>
 #include <fstream>
 #include <iostream>
-#include <boost/foreach.hpp>
+#include <string>
 
 using namespace std;
 using namespace rw::math;
@@ -69,211 +71,240 @@ BOOST_AUTO_TEST_CASE(RANSACLineTest) {
 	 */
 	BOOST_TEST_MESSAGE("- Testing line fitting with RANSAC");
 	
-	std::srand(2); // fix permutation order in findModels
-	
 	// read data file
 	string filePath = testFilePath() + "ransac/line_data.csv";
 	ifstream inFile(filePath.c_str());
 	vector<Transform3D<> > data = readData(inFile);
 	inFile.close();
-	BOOST_CHECK(data.size() > 0);
-	
-	// extract positions
-	vector<Vector3D<> > pos;
-	BOOST_FOREACH (const Transform3D<>& t, data) {
-		pos.push_back(t.P());
-	}
-	
-	// find models
-	vector<LineModel> models = LineModel::findModels(pos, 100, 5, 0.1, 0.1);
-	LineModel bestModel = LineModel::bestModel(models);
-	
-	// check if any model found
-	BOOST_CHECK(models.size() > 0);
-	
-	// check if the model is acceptably close to ground truth
-	rw::geometry::Line referenceLine(
-		Vector3D<>(0.570924747488628, -0.796576799930771, 0.198772066742534),
-		Vector3D<>(0.989086430219537, -0.722790548702032, -0.706598794410653)
-	);
-	LineModel referenceModel(referenceLine);
-	
-	BOOST_CHECK(bestModel.same(referenceModel, 0.01));
-}
+    BOOST_CHECK(data.size() == 15);
 
+    // extract positions
+    vector<Vector3D<> > pos;
+    BOOST_FOREACH (const Transform3D<>& t, data) {
+        pos.push_back(t.P());
+    }
+
+    int seed = 0;
+
+    // Run the test for 100 iterations, to make sure it does not succeed based on a lucky seed.
+    while (seed < 100)
+    {
+        std::srand(seed++);
+        // find models
+        vector<LineModel> models = LineModel::findModels(pos, 100, 5, 0.1, 0.001);
+        LineModel bestModel = LineModel::bestModel(models);
+
+        // check if any model found
+        BOOST_CHECK(models.size() > 0);
+
+        // check if the model is acceptably close to ground truth
+        rw::geometry::Line referenceLine(
+                    Vector3D<>(0.570924747488628, -0.796576799930771, 0.198772066742534),
+                    Vector3D<>(0.989086430219537, -0.722790548702032, -0.706598794410653)
+                    );
+        LineModel referenceModel(referenceLine);
+
+        BOOST_CHECK(bestModel.same(referenceModel, 0.01));
+    }
+}
 
 
 BOOST_AUTO_TEST_CASE(RANSACStructuredLineTest) {
-	/* Test whether RANSAC finds a structured line model in supplied data file.
-	 * 
-	 * Test file contains 10 points lying on a line with noise of sigma=0.1 added to their position.
-	 * Test file also contains 5 random outliers.
-	 */
-	BOOST_TEST_MESSAGE("- Testing structured line fitting with RANSAC");
-	
-	std::srand(3);
-	
-	// read data file
-	string filePath = testFilePath() + "ransac/sline_data.csv";
-	ifstream inFile(filePath.c_str());
-	vector<Transform3D<> > data = readData(inFile);
-	inFile.close();
-	BOOST_CHECK(data.size() > 0);
-	
-	// extract positions
-	vector<Vector3D<> > pos;
-	BOOST_FOREACH (const Transform3D<>& t, data) {
-		pos.push_back(t.P());
-	}
-	
-	// find models
-	vector<StructuredLineModel> models = StructuredLineModel::findModels(pos, 100, 3, 0.1, 0.1);
-	StructuredLineModel bestModel = StructuredLineModel::bestModel(models);
-	
-	// check if any model found
-	BOOST_CHECK(models.size() > 0);
-	
-	// check if the model is acceptably close to ground truth
-	rw::geometry::Line referenceLine(
-		Vector3D<>(-0.00421171, 0.0240888, 0.023985),
-		Vector3D<>(0.677764, -0.428622, 0.598408)
-	);
-	StructuredLineModel referenceModel(referenceLine, Vector3D<>(0.68698, -0.43474, 0.60617), 0.944152);
+    /* Test whether RANSAC finds a structured line model in supplied data file.
+     *
+     * Test file contains 10 points lying on a line with noise of sigma=0.1 added to their position.
+     * Test file also contains 5 random outliers.
+     */
+    BOOST_TEST_MESSAGE("- Testing structured line fitting with RANSAC");
 
-	BOOST_CHECK(bestModel.same(referenceModel, 0.03));
+
+    // read data file
+    string filePath = testFilePath() + "ransac/sline_data.csv";
+    ifstream inFile(filePath.c_str());
+    vector<Transform3D<> > data = readData(inFile);
+    inFile.close();
+    BOOST_CHECK(data.size() == 15);
+
+    // extract positions
+    vector<Vector3D<> > pos;
+    BOOST_FOREACH (const Transform3D<>& t, data) {
+        pos.push_back(t.P());
+    }
+
+
+    int seed = 0;
+
+    // Run the test for 100 iterations, to make sure it does not succeed based on a lucky seed.
+    while (seed < 100)
+    {
+        std::srand(seed++);
+
+
+        // find models
+        vector<StructuredLineModel> models = StructuredLineModel::findModels(pos, 500, 5, 0.2, 0.001);
+        StructuredLineModel bestModel = StructuredLineModel::bestModel(models);
+
+        // check if any model found
+        BOOST_CHECK(models.size() > 0);
+
+        // check if the model is acceptably close to ground truth
+        rw::geometry::Line referenceLine(
+                    Vector3D<>(-0.00421171, 0.0240888, 0.023985),
+                    Vector3D<>(0.677764, -0.428622, 0.598408)
+                    );
+        StructuredLineModel referenceModel(referenceLine, Vector3D<>(0.68698, -0.43474, 0.60617), 0.944152);
+
+        BOOST_CHECK(bestModel.same(referenceModel, 0.03));
+    }
 }
-
 
 
 BOOST_AUTO_TEST_CASE(RANSACPlaneTest) {
-	/* Test whether RANSAC finds a plane model in provided data file.
-	 * 
-	 * Test file contains 10 points lying on a line with noise of sigma=0.1 added to their position.
-	 * Test file also contains 5 random outliers.
-	 */
-	BOOST_TEST_MESSAGE("- Testing plane fitting with RANSAC");
-	
-	std::srand(3547); // This is extremely random - a seed between 0 and 3546 causes the test to fail!
-	
-	// read data file
-	string filePath = testFilePath() + "ransac/plane_data.csv";
-	ifstream inFile(filePath.c_str());
-	vector<Transform3D<> > data = readData(inFile);
-	inFile.close();
-	BOOST_CHECK(data.size() > 0);
-	
-	// extract positions
-	vector<Vector3D<> > pos;
-	BOOST_FOREACH (const Transform3D<>& t, data) {
-		pos.push_back(t.P());
-	}
-	
-	// find models
-	vector<PlaneModel> models = PlaneModel::findModels(pos, 100, 5, 0.1, 1);
-	PlaneModel bestModel = PlaneModel::bestModel(models);
-	
-	// check if any model found
-	BOOST_CHECK(models.size() > 0);
-	
-	// check if the model is acceptably close to ground truth
-	rw::geometry::Plane referencePlane(
-		Vector3D<>(-0.254345014605346, -0.962099998458569, 0.098347376738927),
-		0.25
-	);
-	PlaneModel referenceModel(referencePlane);
-	
-	BOOST_CHECK(bestModel.same(referenceModel, 0.01));
+    /* Test whether RANSAC finds a plane model in provided data file.
+     *
+     * Test file contains 10 points lying on a plane.
+     * Test file also contains 5 random outliers.
+     */
+    BOOST_TEST_MESSAGE("- Testing plane fitting with RANSAC");
+
+
+    // read data file
+    string filePath = testFilePath() + "ransac/plane_data.csv";
+    ifstream inFile(filePath.c_str());
+    vector<Transform3D<> > data = readData(inFile);
+    inFile.close();
+    BOOST_CHECK(data.size() == 15);
+
+    // extract positions
+    vector<Vector3D<> > pos;
+    BOOST_FOREACH (const Transform3D<>& t, data) {
+        pos.push_back(t.P());
+    }
+
+
+    int seed = 0;
+
+    // Run the test 100 times, to ensure it does not pass based on luck!
+    while (seed < 100)
+    {
+        std::srand(seed++);
+
+        // find models
+        vector<PlaneModel> models = PlaneModel::findModels(pos, 1000, 6, 0.01, 0.01);
+        PlaneModel bestModel = PlaneModel::bestModel(models);
+
+        // check if any model found
+        BOOST_CHECK(models.size() > 0);
+
+        // check if the model is acceptably close to ground truth
+        rw::geometry::Plane referencePlane(
+                    Vector3D<>(-0.254345014605346, -0.962099998458569, 0.098347376738927),
+                    -0.25
+                    );
+        PlaneModel referenceModel(referencePlane);
+
+        BOOST_CHECK(bestModel.same(referenceModel, 0.01));
+    }
 }
-
-
 
 BOOST_AUTO_TEST_CASE(RANSACStablePose1DTest) {
-	/* Test whether RANSAC finds a stable pose with 1 dof model in provided data file.
-	 * 
-	 * Test file contains 10 points with orientations matching a stable pose with noise of sigma=5deg.
-	 * Test file also contains 5 random outliers.
-	 */
-	BOOST_TEST_MESSAGE("- Testing stable pose 1 dof fitting with RANSAC");
-	
-	std::srand(0);
-	
-	// read data file
-	string filePath = testFilePath() + "ransac/stablepose_data.csv";
-	ifstream inFile(filePath.c_str());
-	vector<Transform3D<> > data = readData(inFile);
-	inFile.close();
-	BOOST_CHECK(data.size() > 0);
-	
-	// extract orientations
-	vector<Rotation3D<> > rot;
-	BOOST_FOREACH (const Transform3D<>& t, data) {
-		rot.push_back(t.R());
-	}
-	
-	// find models
-	vector<StablePose1DModel> models = StablePose1DModel::findModels(rot, 100, 5, 0.1, 0.1);
-	StablePose1DModel bestModel = StablePose1DModel::bestModel(models);
-	
-	// check if any model found
-	BOOST_CHECK(models.size() > 0);
-	
-	// check if the model is acceptably close to ground truth
-	StablePose1DModel referenceModel(
-		Vector3D<>(0, 0, 1),
-		Vector3D<>(0, 0, 1)
-	);
-	
-	BOOST_CHECK(bestModel.same(referenceModel, 0.05));
-	
-	// check if stable pose defined by axes is the same as one defined by planes and a normal
-	StablePose1DModel modelPlanes(
-		Vector3D<>(0.181716811988644, 0.619803060100149, -0.763428560463371),
-		Vector3D<>(0, 0, 1)
-	);
-	
-	StablePose1DModel modelAxes = StablePose1DModel::fromAxes(
-		Vector3D<>(0, 0, 1),
-		Vector3D<>(0.181716811988644, 0.619803060100149, -0.763428560463371)
-	);
-	
-	BOOST_CHECK(modelPlanes.same(modelAxes, 0.01));
+    /* Test whether RANSAC finds a stable pose with 1 dof model in provided data file.
+     *
+     * Test file contains 10 points with orientations matching a stable pose with noise of sigma=5deg.
+     * Test file also contains 5 random outliers.
+     */
+    BOOST_TEST_MESSAGE("- Testing stable pose 1 dof fitting with RANSAC");
+
+
+    // read data file
+    string filePath = testFilePath() + "ransac/stablepose_data.csv";
+    ifstream inFile(filePath.c_str());
+    vector<Transform3D<> > data = readData(inFile);
+    inFile.close();
+    BOOST_CHECK(data.size() > 0);
+
+    // extract orientations
+    vector<Rotation3D<> > rot;
+    BOOST_FOREACH (const Transform3D<>& t, data) {
+        rot.push_back(t.R());
+    }
+
+    // Run the test with 1000 different seeds:
+    int seed = 0;
+    while (seed < 100)
+    {
+        std::srand(seed++);
+
+        // find models
+        vector<StablePose1DModel> models = StablePose1DModel::findModels(rot, 100, 5, 0.1, 0.01);
+        StablePose1DModel bestModel = StablePose1DModel::bestModel(models);
+
+        // check if any model found
+        BOOST_CHECK(models.size() > 0);
+
+        // check if the model is acceptably close to ground truth
+        StablePose1DModel referenceModel(
+                    Vector3D<>(0, 0, 1),
+                    Vector3D<>(0, 0, 1)
+                    );
+
+        BOOST_CHECK(bestModel.same(referenceModel, 0.05));
+
+        // check if stable pose defined by axes is the same as one defined by planes and a normal
+        StablePose1DModel modelPlanes(
+                    Vector3D<>(0.181716811988644, 0.619803060100149, -0.763428560463371),
+                    Vector3D<>(0, 0, 1)
+                    );
+
+        StablePose1DModel modelAxes = StablePose1DModel::fromAxes(
+                    Vector3D<>(0, 0, 1),
+                    Vector3D<>(0.181716811988644, 0.619803060100149, -0.763428560463371)
+                    );
+
+        BOOST_CHECK(modelPlanes.same(modelAxes, 0.01));
+    }
 }
-
-
 
 BOOST_AUTO_TEST_CASE(RANSACStablePose0DTest) {
-	/* Test whether RANSAC finds a stable pose with 0 dof model in provided data file.
-	 * 
-	 * Test file contains 10 points with orientations matching a stable pose with noise of sigma=5deg.
-	 * Test file also contains 5 random outliers.
-	 */
-	BOOST_TEST_MESSAGE("- Testing stable pose 0 dof fitting with RANSAC");
-	
-	std::srand(0);
-	
-	// read data file
-	string filePath = testFilePath() + "ransac/stablepose0_data.csv";
-	ifstream inFile(filePath.c_str());
-	vector<Transform3D<> > data = readData(inFile);
-	inFile.close();
-	BOOST_CHECK(data.size() > 0);
-	
-	// extract orientations
-	vector<Rotation3D<> > rot;
-	BOOST_FOREACH (const Transform3D<>& t, data) {
-		rot.push_back(t.R());
-	}
-	
-	// find models
-	vector<StablePose0DModel> models = StablePose0DModel::findModels(rot, 100, 5, 0.5, 0.5);
-	StablePose0DModel bestModel = StablePose0DModel::bestModel(models);
-	
-	// check if any model found
-	BOOST_CHECK(models.size() > 0);
-	
-	// check if the model is acceptably close to ground truth
-	StablePose0DModel referenceModel(RPY<>(0.1, 0.45, 0.0).toRotation3D());
-	
-	BOOST_CHECK(bestModel.same(referenceModel, 1.0));
-}
+    /* Test whether RANSAC finds a stable pose with 0 dof model in provided data file.
+     *
+     * Test file contains 10 points with orientations matching a stable pose with noise of sigma=5deg.
+     * Test file also contains 5 random outliers.
+     */
+    BOOST_TEST_MESSAGE("- Testing stable pose 0 dof fitting with RANSAC");
 
+
+
+    // read data file
+    string filePath = testFilePath() + "ransac/stablepose0_data.csv";
+    ifstream inFile(filePath.c_str());
+    vector<Transform3D<> > data = readData(inFile);
+    inFile.close();
+    BOOST_CHECK(data.size() > 0);
+
+    // extract orientations
+    vector<Rotation3D<> > rot;
+    BOOST_FOREACH (const Transform3D<>& t, data) {
+        rot.push_back(t.R());
+    }
+
+
+    // Run the test with 1000 different seeds:
+    int seed = 0;
+    while (seed < 100)
+    {
+        std::srand(seed++);
+
+        // find models
+        vector<StablePose0DModel> models = StablePose0DModel::findModels(rot, 100, 5, 0.5, 0.5);
+        StablePose0DModel bestModel = StablePose0DModel::bestModel(models);
+
+        // check if any model found
+        BOOST_CHECK(models.size() > 0);
+
+        // check if the model is acceptably close to ground truth
+        StablePose0DModel referenceModel(RPY<>(0.1, 0.45, 0.0).toRotation3D());
+
+        BOOST_CHECK(bestModel.same(referenceModel, 1.0));
+    }
+}
